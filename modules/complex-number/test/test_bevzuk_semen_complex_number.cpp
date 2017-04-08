87623fff3d554876053103752d4c2b373ac4cfbb
Semen Bevzuk <semen.bevzuk@gmail.com>
2017-04-08 23:01:58 +0300

    Fixed errors from code-check

--- modules/complex-number/test/test_bevzuk_semen_complex_number.cpp
+++ modules/complex-number/test/test_bevzuk_semen_complex_number.cpp
@@ -1,7 +1,8 @@
 // Copyright 2017 Korniakov Kirill
 
 #include <gtest/gtest.h>
-
+#include <limits>
+#include <string>
 #include "include/complex_number.h"
 
 TEST(Bevzuk_Semen_ComplexNumberTest, Can_assigning_yourself) {
@@ -11,7 +12,7 @@ TEST(Bevzuk_Semen_ComplexNumberTest, Can_assigning_yourself) {
 
     // Act
     ComplexNumber z(re, im);
-	z = z;
+    z = z;
 
     // Assert
     EXPECT_EQ(re, z.getRe());
@@ -41,18 +42,19 @@ TEST(Bevzuk_Semen_ComplexNumberTest, Can_division_by_number_more_eps) {
 
 TEST(Bevzuk_Semen_ComplexNumberTest, Sum_number_and_conjugate_is_real_number) {
     // Arrange
-    //z1 = a+ib; z2 = a-ib; => z1+z2 = 2a;
+    // z1 = a+ib; z2 = a-ib; => z1+z2 = 2a;
     ComplexNumber z1(5, 3);
     ComplexNumber z2(5, -3);
-    ComplexNumber result(10,0);
+    ComplexNumber result(10, 0);
 
     // Act & Assert
     EXPECT_EQ(z1+z2, result);
 }
 
-TEST(Bevzuk_Semen_ComplexNumberTest, Multiplication_by_conjugate_corresponds_property_of_complex_number) {
-    //Arrange
-    //z1 = a+ib; z2 = a-ib; => z1*z2 = a^2+b^2;
+TEST(Bevzuk_Semen_ComplexNumberTest, 
+    Multiplication_by_conjugate_corresponds_property_of_complex_number) {
+    // Arrange
+    // z1 = a+ib; z2 = a-ib; => z1*z2 = a^2+b^2;
     ComplexNumber z1(2, -1);
     ComplexNumber z2(2, 1);
     ComplexNumber result(5, 0);
