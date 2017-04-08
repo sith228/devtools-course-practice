bb283ed48143bcfd5e808d74db0b681517cd35f7
Maxim Doronin <doro156@me.com>
2017-04-08 17:21:45 +0300

    Delete extra spaces

--- modules/complex-number/test/test_Doronin_Maxim_complex_number.cpp
+++ modules/complex-number/test/test_Doronin_Maxim_complex_number.cpp
@@ -6,16 +6,16 @@
 
 TEST(Doronin_Maxim_ComplexNumberTest, Default_constructor_set_zeros) {
     ComplexNumber z;
-
+	
     EXPECT_EQ(0.0, z.getRe());
     EXPECT_EQ(0.0, z.getIm());
 }
 
 TEST(Doronin_Maxim_ComplexNumberTest, Can_create_via_copying_constructor) {
     ComplexNumber z(13.3, -17.8);
-    
+
     ComplexNumber zCopy(z);
-    
+
     EXPECT_EQ(z, zCopy);
 }
 
@@ -24,7 +24,7 @@ TEST(Doronin_Maxim_ComplexNumberTest, Setting_real_doesnt_change_imaginary) {
     double Im = -17.8;
     double newRe = 15.5;
     ComplexNumber z(Re, Im);
-    
+
     z.setRe(newRe);
 
     EXPECT_EQ(z.getIm(), Im);
@@ -35,20 +35,20 @@ TEST(Doronin_Maxim_ComplexNumberTest, Setting_imaginary_doesnt_change_real) {
     double Im = -17.8;
     double newIm = 21.3;
     ComplexNumber z(Re, Im);
-    
+
     z.setIm(newIm);
-    
+
     EXPECT_EQ(z.getRe(), Re);
 }
 
-TEST(Doronin_Maxim_ComplexNumberTest, Equal_operator_return_false_if_different) {
+TEST(Doronin_Maxim_ComplexNumberTest, Equal_oper_return_false_if_diffe) {
     ComplexNumber z1(13.3, -17.8);
     ComplexNumber z2(10.3, 17.8);
-    
+
     ASSERT_FALSE(z1 == z2);
 }
 
-TEST(Doronin_Maxim_ComplexNumberTest, NotEqual_operator_return_false_if_equal) {
+TEST(Doronin_Maxim_ComplexNumberTest, NotEqual_oper_return_false_if_equal) {
     ComplexNumber z1(13.3, -17.8);
     ComplexNumber z2(13.3, -17.8);
 
@@ -81,7 +81,7 @@ TEST(Doronin_Maxim_ComplexNumberTest, assignment_to_itself) {
     double Re = 13.3;
     double Im = -17.8;
     ComplexNumber z(Re, Im);
-    
+
     ASSERT_NO_FATAL_FAILURE(z = z);
 }
 
