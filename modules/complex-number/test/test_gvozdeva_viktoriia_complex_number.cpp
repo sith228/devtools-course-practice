9a4c091cfb75d2b6062341b9edde8de095e29d68
VikaGvozdeva <vikagvozdeva96@gmail.com>
2017-04-09 19:48:08 +0300

    Добавила тест на сравнение и деление

--- modules/complex-number/test/test_gvozdeva_viktoriia_complex_number.cpp
+++ modules/complex-number/test/test_gvozdeva_viktoriia_complex_number.cpp
@@ -16,3 +16,29 @@ TEST(Gvozdeva_Viktoriia_ComplexNumberTest, Multiplication_by_zero_is_zero) {
 	EXPECT_DOUBLE_EQ(result.getRe(), 0.0);
 	EXPECT_DOUBLE_EQ(result.getIm(), 0.0);
 }
+
+
+TEST(Gvozdeva_Viktoriia_ComplexNumberTest, Can_compare_objects) {
+	// Arrange
+	ComplexNumber z1(3.0, 6.0);
+	ComplexNumber z2(z1);
+
+	// Act
+	bool result = (z1 == z2);
+
+	// Assert
+	EXPECT_TRUE(result);
+}
+
+
+TEST(Gvozdeva_Viktoriia_ComplexNumberTest, Division_number_by_number_is_equal_to_one) {
+	// Arrange
+	ComplexNumber z1(3.0, 6.0);
+
+	// Act
+	ComplexNumber result = z1 / z1;
+
+	// Assert
+	EXPECT_DOUBLE_EQ(result.getRe(), 1.0);
+	EXPECT_DOUBLE_EQ(result.getIm(), 0.0);
+}
