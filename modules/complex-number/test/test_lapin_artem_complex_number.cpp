357a64309253e32e7ffd69216962093450b9a458
Regmir <arteom.lap@yandex.ru>
2017-04-09 18:42:00 +0400

    Added new tests for lab1

--- modules/complex-number/test/test_lapin_artem_complex_number.cpp
+++ modules/complex-number/test/test_lapin_artem_complex_number.cpp
@@ -4,15 +4,41 @@
 
 #include "include/complex_number.h"
 
-TEST(Korniakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
-	// Arrange
-	double re = 0.0;
-	double im = 0.0;
+TEST(Lapin_Artem_ComplexNumberTest, Can_Create_With_Negative_Real) {
+    // Arrange
+    double re = -20.0;
+    double im = 20.0;
 
-	// Act
-	ComplexNumber z(re, im);
+    // Act
+    ComplexNumber z(re, im);
 
-	// Assert
-	EXPECT_EQ(re, z.getRe());
-	EXPECT_EQ(im, z.getIm());
+    // Assert
+    EXPECT_EQ(re, z.getRe(), ComplexNumberTest::epsilon);
+    EXPECT_EQ(im, z.getIm(), ComplexNumberTest::epsilon);
+}
+
+TEST(Lapin_Artem_ComplexNumberTest, Can_Create_With_Negative_Imaginary) {
+    // Arrange
+    double re = 20.0;
+    double im = -20.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(re, z.getRe(), ComplexNumberTest::epsilon);
+    EXPECT_EQ(im, z.getIm(), ComplexNumberTest::epsilon);
+}
+
+TEST(Lapin_Artem_ComplexNumberTest, Zero_Number_Is_Equal_To_Zero) {
+    // Arrange
+    double re = 0.0;
+    double im = 0.0;
+
+    // Act
+    ComplexNumber z(re, im);
+
+    // Assert
+    EXPECT_EQ(re, 0, ComplexNumberTest::epsilon);
+    EXPECT_EQ(im, 0, ComplexNumberTest::epsilon);
 }
