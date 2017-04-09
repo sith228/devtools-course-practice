343e8fcae6ede32c42d1de88029bb04a5de4cb3f
Pletnev-N <sdgyjrj@yandex.ru>
2017-04-09 22:40:44 +0400

    Added 4 tests for lab1

--- modules/complex-number/test/test_Pletnev_Nikita_complex_number.cpp
+++ modules/complex-number/test/test_Pletnev_Nikita_complex_number.cpp
@@ -4,6 +4,16 @@
 
 #include "include/complex_number.h"
 
+
+TEST(Pletnev_Nikita_ComplexNumberTest, Assignment_Returns_The_Value) {
+    // Arrange
+    ComplexNumber z1(0.0, 0.0);
+    ComplexNumber z2(1.0, 1.0);
+
+    // Act & Assert
+    EXPECT_EQ(z2, z1=z2);
+}
+
 TEST(Pletnev_Nikita_ComplexNumberTest, Divide_Zero_By_A_Complex) {
     // Arrange
     ComplexNumber z1(0.0, 0.0);
@@ -27,4 +37,37 @@ TEST(Pletnev_Nikita_ComplexNumberTest, Subtract_A_Number_From_Zero) {
     // Assert
     ComplexNumber expected_z(-1.0, -1.0);
     EXPECT_EQ(expected_z, z);
+}
+
+TEST(Pletnev_Nikita_ComplexNumberTest, Copy_Constructor_Works) {
+    // Arrange
+    ComplexNumber z1(0.0, 0.0);
+
+    // Act
+    ComplexNumber z2(z1);
+
+    // Assert
+    EXPECT_EQ(z1, z2);
+}
+
+TEST(Pletnev_Nikita_ComplexNumberTest, Variables_Have_Different_Memory_After_Assignment) {
+    // Arrange
+    ComplexNumber z1(1.0, 1.0);
+
+    // Act
+    ComplexNumber z2=z1;
+
+    // Assert
+    EXPECT_NE(&z1, &z2);
+}
+
+TEST(Pletnev_Nikita_ComplexNumberTest, Variables_Have_Different_Memory_After_Copy_Constructor) {
+    // Arrange
+    ComplexNumber z1(1.0, 1.0);
+
+    // Act
+    ComplexNumber z2(z1);
+
+    // Assert
+    EXPECT_NE(&z1, &z2);
 }
