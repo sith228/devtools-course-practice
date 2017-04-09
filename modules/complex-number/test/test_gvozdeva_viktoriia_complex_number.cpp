bca9bedf8f700e4c32b46c1924fb463e4a3872e1
VikaGvozdeva <vikagvozdeva96@gmail.com>
2017-04-09 20:39:13 +0300

    Удалила пробелы

--- modules/complex-number/test/test_gvozdeva_viktoriia_complex_number.cpp
+++ modules/complex-number/test/test_gvozdeva_viktoriia_complex_number.cpp
@@ -1,9 +1,7 @@
 // Copyright 2017 Gvozdeva Viktoriia
 
 #include <gtest/gtest.h>
-
 #include <cmath>
-
 #include "include/complex_number.h"
 
 TEST(Gvozdeva_Viktoriia_ComplexNumberTest, Multiplication_by_zero_is_zero) {
