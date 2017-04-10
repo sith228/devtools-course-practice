f925c0c0752b1b405aedc4fc948c1ac9576529ff
allnes <nesoldr@mail.ru>
2017-04-10 13:50:18 +0300

    Added the test for use of a repeated enclosure of constructors

--- modules/complex-number/test/test_nesterov_alexander_complex_number.cpp
+++ modules/complex-number/test/test_nesterov_alexander_complex_number.cpp
@@ -1,1 +1,29 @@
 // Copyright 2016 Nesterov Alexander
+
+#include <gtest/gtest.h>
+#include <functional>
+
+#include "include/complex_number.h"
+
+TEST(Nesterov_Alexander_ComplexNumberTest,
+     Can_Use_Repeated_Enclosure_Constructors) {
+// Arrange
+    double re = 3.68;
+    double im = 2.09;
+
+// Act
+    ComplexNumber z(re, im);
+    std::function<ComplexNumber(int, ComplexNumber)> foo
+        = [&foo](int k, ComplexNumber y) {
+            if (k != 1) {
+                ComplexNumber temp(foo(k - 1, y));
+                y = temp;
+            }
+            return y;
+        };
+    ComplexNumber res(foo(10000, z));
+
+// Assert
+    EXPECT_EQ(re, res.getRe());
+    EXPECT_EQ(im, res.getIm());
+}
