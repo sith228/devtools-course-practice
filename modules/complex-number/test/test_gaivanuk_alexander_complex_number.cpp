ec7b456d1b7880f9c8ec90b2c64d3ead5751eae8
Alexander Gaivanuk <alexgaiv0748@gmail.com>
2017-04-10 14:24:01 +0400

    tab to spaces

--- modules/complex-number/test/test_gaivanuk_alexander_complex_number.cpp
+++ modules/complex-number/test/test_gaivanuk_alexander_complex_number.cpp
@@ -19,22 +19,22 @@ TEST(Gaivanuk_Alexander_ComplexNumberTest, Difference_on_equal_numbers) {
     double im = 324432.243;
 
     ComplexNumber z1(re, im);
-	ComplexNumber z2(re, im);
-	ComplexNumber z = z1 - z2;
+    ComplexNumber z2(re, im);
+    ComplexNumber z = z1 - z2;
 
     EXPECT_EQ(0.0, z.getRe());
     EXPECT_EQ(0.0, z.getIm());
 }
 
 TEST(Gaivanuk_Alexander_ComplexNumberTest, Mult_conjuctive_numbers) {
-	double re = 34.23;
-	double im = -12.44;
+    double re = 34.23;
+    double im = -12.44;
     ComplexNumber z1(re, im);
-	ComplexNumber z2(re, -im);
+    ComplexNumber z2(re, -im);
 
     ComplexNumber z = z1 * z2;
 
-	EXPECT_NEAR(re*re + im*im, z.getRe(), eps);
+    EXPECT_NEAR(re*re + im*im, z.getRe(), eps);
     EXPECT_NEAR(0.0, z.getIm(), eps);
 }
 
@@ -61,14 +61,14 @@ TEST(Gaivanuk_Alexander_ComplexNumberTest, Assign_to_itself) {
 TEST(Gaivanuk_Alexander_ComplexNumberTest, Division_by_itself) {
     ComplexNumber z(-3.23, 12.0);
 
-	ComplexNumber z2 = z / z;
+    ComplexNumber z2 = z / z;
 
     EXPECT_NEAR(1.0, z2.getRe(), eps);
-	EXPECT_NEAR(0.0, z2.getIm(), eps);
+    EXPECT_NEAR(0.0, z2.getIm(), eps);
 }
 
 TEST(Gaivanuk_Alexander_ComplexNumberTest, Number_is_not_not_equal_to_itself) {
-	ComplexNumber z(-3.23, 12.0);
+    ComplexNumber z(-3.23, 12.0);
 
-	EXPECT_FALSE(z != z);
+    EXPECT_FALSE(z != z);
 }
