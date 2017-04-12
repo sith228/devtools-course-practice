de841a6cb2e2ff26a5ed9c00b270b7653a35da14
Игорь Федоров <fedorovigor1155@gmail.com>
2017-04-12 20:18:55 +0300

    +3 test

--- modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
+++ modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
@@ -13,7 +13,7 @@ TEST(Igor_Fedorov_ComplexNumberTest, Cant_divide_by_Zero) {
 	ComplexNumber d;
 	EXPECT_ANY_THROW(d = c / d);
 }
-TEST(Igor_Fedorov_ComplexNumberTest, Cant_divide_by_Zero) {
+TEST(Igor_Fedorov_ComplexNumberTest, can_divide_by_1) {
 	double r = 123;
 	double i = 123;
 	ComplexNumber a(1, 0);
@@ -21,4 +21,15 @@ TEST(Igor_Fedorov_ComplexNumberTest, Cant_divide_by_Zero) {
 	ComplexNumber c = a / b;
 	ComplexNumber d(r / (r*r +i*i ),-i/(r*r+i*i));
 	EXPECT_EQ(c,d);
+}
+TEST(Igor_Fedorov_ComplexNumberTest, get_work_ok) {
+	double r1 = 1;
+	double i1 = 12;
+	double r2 = 123;
+	double i2 = 1234; 
+	ComplexNumber a(r1, i1);
+	ComplexNumber b(r2, i2);
+	ComplexNumber c = a * b;
+	ComplexNumber d(c.getRe, c.getIm);
+	EXPECT_EQ(c , d);
 }
