4d4f1685ba1c9bcabdd0bdd518550c3a3d453c77
Игорь Федоров <fedorovigor1155@gmail.com>
2017-04-12 20:45:09 +0300

    5 test done

--- modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
+++ modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
@@ -7,19 +7,23 @@
 TEST(Igor_Fedorov_ComplexNumberTest, Cant_divide_by_Zero) {
 	double r = 15.023;
 	double i = 97.193;
+	
 	ComplexNumber a(r, i);
 	ComplexNumber b(r, i);
 	ComplexNumber c = a - b;
 	ComplexNumber d;
+	
 	EXPECT_ANY_THROW(d = c / d);
 }
 TEST(Igor_Fedorov_ComplexNumberTest, can_divide_by_1) {
 	double r = 123;
 	double i = 123;
+	
 	ComplexNumber a(1, 0);
 	ComplexNumber b(r, i);
 	ComplexNumber c = a / b;
 	ComplexNumber d(r / (r*r +i*i ),-i/(r*r+i*i));
+	
 	EXPECT_EQ(c,d);
 }
 TEST(Igor_Fedorov_ComplexNumberTest, get_work_ok) {
@@ -30,15 +34,28 @@ TEST(Igor_Fedorov_ComplexNumberTest, get_work_ok) {
 	ComplexNumber a(r1, i1);
 	ComplexNumber b(r2, i2);
 	ComplexNumber c = a * b;
-	ComplexNumber d(c.getRe, c.getIm);
+	ComplexNumber d(c.getRe(), c.getIm());
 	EXPECT_EQ(c , d);
 }
 TEST(Igor_Fedorov_ComplexNumberTest, set_work_ok) {
 	double r = 1.234;
 	double i = 12.34;
+	
 	ComplexNumber a(r, i);
 	ComplexNumber d;
+	
 	d.setIm(r);
 	d.setIm(i);
+	
 	EXPECT_EQ(a, d);
+}
+TEST(Igor_Fedorov_ComplexNumberTest, correct_multilication) {
+	double r = 10.0;
+	double i = 0.0;
+
+	ComplexNumber c(i,r);
+	ComplexNumber result(-r, i);
+	ComplexNumber cc = c * c;
+	
+	EXPECT_EQ(result, cc);
 }
