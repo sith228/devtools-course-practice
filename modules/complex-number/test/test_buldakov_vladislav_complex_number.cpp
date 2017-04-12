61c940546ddbdc3eb07243bf8016ae1a0412a1cd
Wropwtr <buldakov.95@mail.ru>
2017-04-12 20:57:41 +0300

    Add some tests

--- modules/complex-number/test/test_buldakov_vladislav_complex_number.cpp
+++ modules/complex-number/test/test_buldakov_vladislav_complex_number.cpp
@@ -5,16 +5,32 @@
 #include "include/complex_number.h"
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Create_Default) {
-    // Arrange
-    double re = 0.0;
-    double im = 0.0;
-
-    // Act
+    // Arrange & Act
 	ComplexNumber z = ComplexNumber();
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+	EXPECT_EQ(0.0, z.getRe());
+	EXPECT_EQ(0.0, z.getIm());
+}
+
+TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Compare) {
+	// Arrange & Act
+	ComplexNumber z(1.0, -1.0);
+	ComplexNumber z_copy(1.0, -1.0);
+
+	// Assert
+	ASSERT_TRUE(z_copy == z);
+}
+
+TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Create_Object_Copy) {
+	// Arrange
+	ComplexNumber z(5.0, 16.0);
+
+	// Act
+	ComplexNumber z_copy(z);
+
+	// Assert
+	ASSERT_TRUE(z_copy == z);
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Sum) {
@@ -32,5 +48,28 @@ TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Sum) {
 	EXPECT_EQ(0.0, resut.getIm());
 }
 
+TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Multiply) {
+	// Arrange
+	ComplexNumber x(1.1, -1.4);
+	ComplexNumber y(1.4, -1.1);
 
+	// Act
+	ComplexNumber resut = x * y;
 
+	// Assert
+	EXPECT_EQ(-0.0, resut.getRe());
+	EXPECT_EQ(-3.17, resut.getIm());
+}
+
+TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Divide) {
+	// Arrange
+	ComplexNumber x(0.5, -1.0);
+	ComplexNumber y(-2.0, 4.0);
+
+	// Act
+	ComplexNumber resut = x / y;
+
+	// Assert
+	EXPECT_EQ(-0.25, resut.getRe());
+	EXPECT_EQ(0.0, resut.getIm());
+}
