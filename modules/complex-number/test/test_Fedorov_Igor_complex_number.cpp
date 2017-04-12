8b9ee760fee8da62dd6984655d349ececfb9e0f6
Игорь Федоров <fedorovigor1155@gmail.com>
2017-04-12 19:41:34 +0300

    +1 test

--- modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
+++ modules/complex-number/test/test_Fedorov_Igor_complex_number.cpp
@@ -4,7 +4,13 @@
 
 #include "include/complex_number.h"
 
-TEST(Igor_Fedorov_ComplexNumberTest, Can_Create_Zero) {
+TEST(Igor_Fedorov_ComplexNumberTest, Cant_divide_by_Zero) {
+	double re = 234.23;
+	double im = 324432.243;
 
-	
+	ComplexNumber z1(re, im);
+	ComplexNumber z2(re, im);
+	ComplexNumber z = z1 - z2;
+	ComplexNumber c;
+	EXPECT_ANY_THROW(c = z / c);
 }
