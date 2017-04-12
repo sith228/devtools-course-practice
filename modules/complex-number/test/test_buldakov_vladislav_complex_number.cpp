41698a8677750c57c2463f79aca664bc0b053951
Wropwtr <buldakov.95@mail.ru>
2017-04-12 21:14:23 +0300

    Replace tabs to spaces

--- modules/complex-number/test/test_buldakov_vladislav_complex_number.cpp
+++ modules/complex-number/test/test_buldakov_vladislav_complex_number.cpp
@@ -6,31 +6,31 @@
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Create_Default) {
     // Arrange & Act
-	ComplexNumber z = ComplexNumber();
+    ComplexNumber z = ComplexNumber();
 
     // Assert
-	EXPECT_EQ(0.0, z.getRe());
-	EXPECT_EQ(0.0, z.getIm());
+    EXPECT_EQ(0.0, z.getRe());
+    EXPECT_EQ(0.0, z.getIm());
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Compare) {
-	// Arrange & Act
-	ComplexNumber z(1.0, -1.0);
-	ComplexNumber z_copy(1.0, -1.0);
+    // Arrange & Act
+    ComplexNumber z(1.0, -1.0);
+    ComplexNumber z_copy(1.0, -1.0);
 
-	// Assert
-	ASSERT_TRUE(z_copy == z);
+    // Assert
+    ASSERT_TRUE(z_copy == z);
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Create_Object_Copy) {
-	// Arrange
-	ComplexNumber z(5.0, 16.0);
+    // Arrange
+    ComplexNumber z(5.0, 16.0);
 
-	// Act
-	ComplexNumber z_copy(z);
+    // Act
+    ComplexNumber z_copy(z);
 
-	// Assert
-	ASSERT_TRUE(z_copy == z);
+    // Assert
+    ASSERT_TRUE(z_copy == z);
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Sum) {
@@ -38,38 +38,38 @@ TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Sum) {
     double re = 1.0;
     double im = 2.0;
     ComplexNumber z(re, im);
-	ComplexNumber neg_z(-re, -im);
+    ComplexNumber neg_z(-re, -im);
 
     // Act
-	ComplexNumber resut = z + neg_z;
+    ComplexNumber resut = z + neg_z;
 
     // Assert
-	EXPECT_EQ(0.0, resut.getRe());
-	EXPECT_EQ(0.0, resut.getIm());
+    EXPECT_EQ(0.0, resut.getRe());
+    EXPECT_EQ(0.0, resut.getIm());
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Multiply) {
-	// Arrange
-	ComplexNumber x(1.1, -1.4);
-	ComplexNumber y(1.4, -1.1);
+    // Arrange
+    ComplexNumber x(1.1, -1.4);
+    ComplexNumber y(1.4, -1.1);
 
-	// Act
-	ComplexNumber resut = x * y;
+    // Act
+    ComplexNumber resut = x * y;
 
-	// Assert
-	EXPECT_EQ(-0.0, resut.getRe());
-	EXPECT_EQ(-3.17, resut.getIm());
+    // Assert
+    EXPECT_EQ(-0.0, resut.getRe());
+    EXPECT_EQ(-3.17, resut.getIm());
 }
 
 TEST(Buldakov_Vladislav_ComplexNumberTest, Can_Divide) {
-	// Arrange
-	ComplexNumber x(0.5, -1.0);
-	ComplexNumber y(-2.0, 4.0);
+    // Arrange
+    ComplexNumber x(0.5, -1.0);
+    ComplexNumber y(-2.0, 4.0);
 
-	// Act
-	ComplexNumber resut = x / y;
+    // Act
+    ComplexNumber resut = x / y;
 
-	// Assert
-	EXPECT_EQ(-0.25, resut.getRe());
-	EXPECT_EQ(0.0, resut.getIm());
+    // Assert
+    EXPECT_EQ(-0.25, resut.getRe());
+    EXPECT_EQ(0.0, resut.getIm());
 }
