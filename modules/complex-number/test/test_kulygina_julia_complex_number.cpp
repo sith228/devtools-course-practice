a9f34357f2f3aace82d9d7a4117d7bf03dbc336f
Julia Kulygina <Julia Kulygina>
2017-04-12 14:58:29 +0300

    Modifided tests

--- modules/complex-number/test/test_kulygina_julia_complex_number.cpp
+++ modules/complex-number/test/test_kulygina_julia_complex_number.cpp
@@ -13,37 +13,36 @@ TEST(Kulygina_Julia_ComplexNumberTest, Can_create_complex_number) {
 
 TEST(Kulygina_Julia_ComplexNumberTest, Can_set_complex_number) {
     ComplexNumber z(10.0, 10.0);
-    z.setRe(6.0);
+    ComplexNumber z2(1.0, 0.0);
+    
+    z.setRe(z2.getRe());
     z.setIm(-1.0);
-    EXPECT_EQ(6.0, z.getRe());
+    EXPECT_EQ(z2.getRe(), z.getRe());
     EXPECT_EQ(-1.0, z.getIm());
 }
 
 TEST(Kulygina_Julia_ComplexNumberTest, Correct_add_operation) {
     ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(1.0, 0.0);
-    EXPECT_EQ((z2+z1).getRe(), 2.0);
-    EXPECT_EQ((z2+z1).getIm(), 2.0);
+    
+    ASSERT_ANY_THROW((z2+z1).getRe(), 2.0);
 }
 
 TEST(Kulygina_Julia_ComplexNumberTest, Correct_definition_operation) {
     ComplexNumber z(1.0, 2.0);
     ComplexNumber zres(0.0, 0.0);
     zres = z;
-    EXPECT_EQ(zres.getRe(), 1.0);
-    EXPECT_EQ(zres.getIm(), 2.0);
+    EXPECT_TRUE(z == zres);
 }
 
 TEST(Kulygina_Julia_ComplexNumberTest, Correct_divided_zero) {
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2(0.0, 0.0);
-    EXPECT_ANY_THROW(z1/z2);
+    ASSERT_ANY_THROW(z1/z2);
 }
 
 TEST(Kulygina_Julia_ComplexNumberTest, Correct_divide_operation) {
     ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(1.0, 0.0);
+    ComplexNumber z2(1.0, -1.0);
     ASSERT_NO_THROW(z1/z2);
-    EXPECT_EQ((z1/z2).getRe(), 1.0);
-    EXPECT_EQ((z1/z2).getIm(), 2.0);
+    EXPECT_FALSE((z1/z2).getRe() == (z1.getRe() / z2.getRe()));
 }
