7e88522b2d8a61f6477c450a8765c20be6a3f883
lemurtaison <zona131313@gmail.com>
2017-04-14 01:07:33 +0300

    remove tab

--- modules/complex-number/test/test_nazarov_aleksandr_complex_number.cpp
+++ modules/complex-number/test/test_nazarov_aleksandr_complex_number.cpp
@@ -6,159 +6,157 @@
 // сначала стандартная проверка всего функционала
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Rigt_Re) {
-	
-	ComplexNumber a(11, 11);
 
-	EXPECT_EQ(a.getRe(), 11);
+        ComplexNumber a(11, 11);
 
+        EXPECT_EQ(a.getRe(), 11);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Set_Rigt_Im) {
 
-	ComplexNumber a(11, 11);
+        ComplexNumber a(11, 11);
 
-	a.setIm(12);
+        a.setIm(12);
 
-	EXPECT_EQ(a.getIm(), 12);
-
+        EXPECT_EQ(a.getIm(), 12);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Set_Rigt_Re) {
 
-	ComplexNumber a(11, 11);
-	
-	a.setRe(12);
+        ComplexNumber a(11, 11);
 
-	EXPECT_EQ(a.getRe(), 12);
+        a.setRe(12);
+
+        EXPECT_EQ(a.getRe(), 12);
 
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Rigt_Im) {
 
-	ComplexNumber a(11, 11);
+        ComplexNumber a(11, 11);
 
-	EXPECT_EQ(a.getIm(), 11);
+        EXPECT_EQ(a.getIm(), 11);
 
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Sum) {
 
-	double ar = 13.77, ai = 19.86;
-	double br = 4.20, bi = 14.00;
-	double cr = ar + br, ci = ai + bi;
+        double ar = 13.77, ai = 19.86;
+        double br = 4.20, bi = 14.00;
+        double cr = ar + br, ci = ai + bi;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a + b);
+        ASSERT_NO_THROW(c = a + b);
 
-	EXPECT_EQ(c.getRe(), cr);
-	EXPECT_EQ(c.getIm(), ci);
+        EXPECT_EQ(c.getRe(), cr);
+        EXPECT_EQ(c.getIm(), ci);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Subtract) {
 
-	double ar = 13.77, ai = 19.86;
-	double br = 4.20, bi = 14.00;
-	double cr = ar - br, ci = ai - bi;
+        double ar = 13.77, ai = 19.86;
+        double br = 4.20, bi = 14.00;
+        double cr = ar - br, ci = ai - bi;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a - b);
+        ASSERT_NO_THROW(c = a - b);
 
-	EXPECT_EQ(c.getRe(), cr);
-	EXPECT_EQ(c.getIm(), ci);
+        EXPECT_EQ(c.getRe(), cr);
+        EXPECT_EQ(c.getIm(), ci);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Multiplication) {
 
-	double ar = 10, ai = 10;
-	double br = 2, bi = 2;
-	double cr = 0, ci = 40;
+        double ar = 10, ai = 10;
+        double br = 2, bi = 2;
+        double cr = 0, ci = 40;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a * b);
+        ASSERT_NO_THROW(c = a * b);
 
-	EXPECT_EQ(c.getRe(), cr);
-	EXPECT_EQ(c.getIm(), ci);
+        EXPECT_EQ(c.getRe(), cr);
+        EXPECT_EQ(c.getIm(), ci);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Division) {
 
-	double ar = 10, ai = 10;
-	double br = 4, bi = 2;
-	double cr = 3, ci = 1;
+        double ar = 10, ai = 10;
+        double br = 4, bi = 2;
+        double cr = 3, ci = 1;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a / b);
+        ASSERT_NO_THROW(c = a / b);
 
-	EXPECT_EQ(c.getRe(), cr);
-	EXPECT_EQ(c.getIm(), ci);
+        EXPECT_EQ(c.getRe(), cr);
+        EXPECT_EQ(c.getIm(), ci);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Equal) {
 
-	double ar = 13.77, ai = 19.86;
-	double br = 4.20, bi = 14.00;
+        double ar = 13.77, ai = 19.86;
+        double br = 4.20, bi = 14.00;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a);
+        ASSERT_NO_THROW(c = a);
 
-	EXPECT_EQ(c, a);
-	EXPECT_NE(c, b);
-	EXPECT_EQ(1, c == a);
-	EXPECT_EQ(0, a == b);
+        EXPECT_EQ(c, a);
+        EXPECT_NE(c, b);
+        EXPECT_EQ(1, c == a);
+        EXPECT_EQ(0, a == b);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Get_Right_Not_Equal) {
 
-	double ar = 13.77, ai = 19.86;
-	double br = 4.20, bi = 14.00;
+        double ar = 13.77, ai = 19.86;
+        double br = 4.20, bi = 14.00;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(br, bi);
-	ComplexNumber c(0.00, 0.00);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(br, bi);
+        ComplexNumber c(0.00, 0.00);
 
-	ASSERT_NO_THROW(c = a);
+        ASSERT_NO_THROW(c = a);
 
-	EXPECT_EQ(c, a);
-	EXPECT_NE(c, b);
-	EXPECT_EQ(0, c != a);
-	EXPECT_EQ(1, a != b);
+        EXPECT_EQ(c, a);
+        EXPECT_NE(c, b);
+        EXPECT_EQ(0, c != a);
+        EXPECT_EQ(1, a != b);
 }
 
 // что нибудь необычное
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Can_Equate_To_Itself) {
 
-	double ar = 13.77, ai = 19.86;
+        double ar = 13.77, ai = 19.86;
 
-	ComplexNumber a(ar, ai);
+        ComplexNumber a(ar, ai);
 
-	ASSERT_NO_THROW(a = a);
+        ASSERT_NO_THROW(a = a);
 
-	EXPECT_EQ(a.getIm(), ai);
-	EXPECT_EQ(a.getRe(), ar);
+        EXPECT_EQ(a.getIm(), ai);
+        EXPECT_EQ(a.getRe(), ar);
 }
 
 TEST(Nazarov_Aleksandr_ComplexNumberTest, Cant_Division_To_Zero) {
 
-	double ar = 13.77, ai = 19.86;
+        double ar = 13.77, ai = 19.86;
 
-	ComplexNumber a(ar, ai);
-	ComplexNumber b(0, 0);
+        ComplexNumber a(ar, ai);
+        ComplexNumber b(0, 0);
 
-	ASSERT_ANY_THROW(a/b);
+        ASSERT_ANY_THROW(a/b);
 }
