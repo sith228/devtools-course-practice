b7c6a562f016d13c419b878f2d61b34a19b07e98
Maxim <thanatormax@gmail.com>
2017-04-13 13:21:50 +0300

    Fix

--- modules/complex-number/test/test_kildishev_maxim_complex_number.cpp
+++ modules/complex-number/test/test_kildishev_maxim_complex_number.cpp
@@ -10,29 +10,25 @@ TEST(Kildishev_Maxim_ComplexNumberTest, Cant_division_by_zero) {
     ASSERT_ANY_THROW(z = z / zero);
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Sum_of_zeros_is_zero)
-{
+TEST(Kildishev_Maxim_ComplexNumberTest, Sum_of_zeros_is_zero) {
     ComplexNumber z(0.0, 0.0);
     ComplexNumber expected(0.0, 0.0);
     EXPECT_EQ(expected, z = z + z);
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Multyply_by_zero_is_zero)
-{
+TEST(Kildishev_Maxim_ComplexNumberTest, Multyply_by_zero_is_zero) {
     ComplexNumber z(1.1, 1.1);
     ComplexNumber expected(0.0, 0.0);
     EXPECT_EQ(expected, z = z * expected);
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Copy_is_equal_orig)
-{
+TEST(Kildishev_Maxim_ComplexNumberTest, Copy_is_equal_orig) {
     ComplexNumber z(1.1, 1.1);
     ComplexNumber z1(z);
     EXPECT_EQ(z, z1);
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Sum_works)
-{
+TEST(Kildishev_Maxim_ComplexNumberTest, Sum_works) {
     ComplexNumber z(1.0, 1.0);
     ComplexNumber z1(2.0, 2.1);
     ComplexNumber z2 = z + z1;
