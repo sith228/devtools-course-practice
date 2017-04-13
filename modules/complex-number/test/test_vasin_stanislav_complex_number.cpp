9ca446cd55dbb2ed4abdbe3932aac163457cb9b3
VasinSS <stascnfcstascnfc@mail.ru>
2017-04-13 17:05:09 +0300

    Updated tests

--- modules/complex-number/test/test_vasin_stanislav_complex_number.cpp
+++ modules/complex-number/test/test_vasin_stanislav_complex_number.cpp
@@ -5,91 +5,75 @@
 #include "include/complex_number.h"
 
 
-TEST(Vasin_Stanislav_ComplexNumberTest, the_degree_of_the_imaginary_unit_1) {
+TEST(Vasin_Stanislav_ComplexNumberTest, imaginary_i_pow_2) {
     // Arrange
     ComplexNumber i(0.0, 1.0);
-    ComplexNumber result(-1.0, 0.0);
 
     // Act
     ComplexNumber i_pow_2 = i * i;
 
     // Assert
-    // i^2 = -1
-    EXPECT_TRUE(result == i_pow_2);
+    EXPECT_TRUE((i_pow_2.getRe() == -1.0) && (i_pow_2.getIm() == 0.0));
 }
 
-TEST(Vasin_Stanislav_ComplexNumberTest, the_degree_of_the_imaginary_unit_2) {
+TEST(Vasin_Stanislav_ComplexNumberTest, imaginary_i_pow_3) {
     // Arrange
     ComplexNumber i(0.0, 1.0);
-    ComplexNumber result(0.0, -1.0);
 
     // Act
     ComplexNumber i_pow_3 = i * i * i;
 
     // Assert
-    // i^3 = -i
-    EXPECT_TRUE(result == i_pow_3);
+    EXPECT_TRUE((i_pow_3.getRe() == 0.0) && (i_pow_3.getIm() == -1.0));
 }
 
-TEST(Vasin_Stanislav_ComplexNumberTest, the_degree_of_the_imaginary_unit_3) {
+TEST(Vasin_Stanislav_ComplexNumberTest, imaginary_i_pow_4) {
     // Arrange
     ComplexNumber i(0.0, 1.0);
-    ComplexNumber result(1.0, 0.0);
 
     // Act
     ComplexNumber i_pow_4 = i * i * i * i;
 
     // Assert
-    // i^4 = 1
-    EXPECT_TRUE(result == i_pow_4);
+    EXPECT_TRUE((i_pow_4.getRe() == 1.0) && (i_pow_4.getIm() == 0.0));
 }
 
-TEST(Vasin_Stanislav_ComplexNumberTest, complex_conjugate_numbers) {
+TEST(Vasin_Stanislav_ComplexNumberTest, mull_by_the_complex_conjugate) {
     // Arrange
     ComplexNumber A(2.3, 15.0);
     ComplexNumber B(2.3, -15.0);
-    ComplexNumber real_number(2.3 * 2.3 + 15.0 * 15.0, 0.0);
-
+    
     // Act
     ComplexNumber C = A * B;
-
+    
     // Assert
-    EXPECT_TRUE(real_number == C);
+    double res = 2.3 * 2.3 + 15.0 * 15.0;
+    EXPECT_TRUE((C.getRe() == res) && (C.getIm() == 0.0));
 }
 
 TEST(Vasin_Stanislav_ComplexNumberTest, error_add_small_and_large_numbers) {
     // Arrange
-    ComplexNumber A(10000000000.0, 10000000000.0);
-    ComplexNumber B(0.0000000001, 0.0000000001);
+    ComplexNumber A(1e+11, 1e+11);
+    ComplexNumber B(1e-11, 1e-11);
 
     // Act
     ComplexNumber C = A + B;
 
     // Assert
-    EXPECT_TRUE(A == C);
+    EXPECT_TRUE((A.getRe() == C.getRe()) && (A.getRe() == C.getRe()));
 }
 
-TEST(Vasin_Stanislav_ComplexNumberTest, default_constructor_1) {
+TEST(Vasin_Stanislav_ComplexNumberTest, default_constructor) {
     // Arrange
 
     // Act
     ComplexNumber A;
 
     // Assert
-    EXPECT_EQ(0.0, A.getRe());
+    EXPECT_TRUE((0.0 == A.getIm()) && (0.0 == A.getRe()));
 }
 
-TEST(Vasin_Stanislav_ComplexNumberTest, default_constructor_2) {
-    // Arrange
-
-    // Act
-    ComplexNumber A;
-
-    // Assert
-    EXPECT_EQ(0.0, A.getIm());
-}
-
-TEST(Vasin_Stanislav_ComplexNumberTest, copy_constructor_1) {
+TEST(Vasin_Stanislav_ComplexNumberTest, copy_constructor) {
     // Arrange
     ComplexNumber A(1.0, 23.1);
 
@@ -97,16 +81,5 @@ TEST(Vasin_Stanislav_ComplexNumberTest, copy_constructor_1) {
     ComplexNumber B(A);
 
     // Assert
-    EXPECT_EQ(A.getRe(), B.getRe());
-}
-
-TEST(Vasin_Stanislav_ComplexNumberTest, copy_constructor_2) {
-    // Arrange
-    ComplexNumber A(1.0, 23.1);
-
-    // Act
-    ComplexNumber B(A);
-
-    // Assert
-    EXPECT_EQ(A.getIm(), B.getIm());
+    EXPECT_TRUE((A.getIm() == B.getIm()) && (A.getRe() == B.getRe()));
 }
