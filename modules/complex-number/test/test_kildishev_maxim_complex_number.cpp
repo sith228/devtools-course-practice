40efa1713e52db675a23826a4b8056a93a21338e
Maxim <thanatormax@gmail.com>
2017-04-14 00:44:48 +0300

    Some test are replaced. Also fixed rouding problem.

--- modules/complex-number/test/test_kildishev_maxim_complex_number.cpp
+++ modules/complex-number/test/test_kildishev_maxim_complex_number.cpp
@@ -1,37 +1,45 @@
 // Copyright 2017 Kildishev Maxim
 
 #include <gtest/gtest.h>
+#include <math.h>
 
 #include "include/complex_number.h"
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Cant_division_by_zero) {
+TEST(Kildishev_Maxim_ComplexNumberTest, Multiply_by_selfajoint_is_only_realpart) {
     ComplexNumber z(1.0, 1.0);
-    ComplexNumber zero(0.0, 0.0);
-    ASSERT_ANY_THROW(z = z / zero);
+    ComplexNumber z1(1.0, -1.0);
+    ComplexNumber res = z * z1;
+    EXPECT_DOUBLE_EQ(2.0, res.getRe());
+    EXPECT_DOUBLE_EQ(0.0, res.getIm());
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Sum_of_zeros_is_zero) {
-    ComplexNumber z(0.0, 0.0);
-    ComplexNumber expected(0.0, 0.0);
-    EXPECT_EQ(expected, z = z + z);
+TEST(Kildishev_Maxim_ComplexNumberTest, Abs_of_selfajoint_digits_are_equal) {
+    ComplexNumber z(3.0, 4.0);
+    ComplexNumber z1(3.0, -4.0);
+    double abs_z = sqrt(z.getRe() * z.getRe() + z.getIm() * z.getIm());
+    double abs_z1 = sqrt(z1.getRe()*z1.getRe() + z1.getIm() * z1.getIm());
+    EXPECT_TRUE(abs_z == abs_z1);
 }
 
 TEST(Kildishev_Maxim_ComplexNumberTest, Multyply_by_zero_is_zero) {
     ComplexNumber z(1.1, 1.1);
     ComplexNumber expected(0.0, 0.0);
-    EXPECT_EQ(expected, z = z * expected);
+    EXPECT_DOUBLE_EQ(expected.getRe(), (z = z * expected).getRe());
+    EXPECT_DOUBLE_EQ(expected.getIm(), (z = z* expected).getIm());
+
 }
 
 TEST(Kildishev_Maxim_ComplexNumberTest, Copy_is_equal_orig) {
     ComplexNumber z(1.1, 1.1);
     ComplexNumber z1(z);
-    EXPECT_EQ(z, z1);
+    EXPECT_TRUE(z1.getRe() == z.getRe());
+    EXPECT_TRUE(z1.getIm() == z.getIm());
 }
 
-TEST(Kildishev_Maxim_ComplexNumberTest, Sum_works) {
-    ComplexNumber z(1.0, 1.0);
+TEST(Kildishev_Maxim_ComplexNumberTest, Sum_works_with_floatingpoint_numbers) {
+    ComplexNumber z(1.1, 1.0);
     ComplexNumber z1(2.0, 2.1);
     ComplexNumber z2 = z + z1;
-    ComplexNumber res(3, 3.1);
-    EXPECT_EQ(res, z2);
+    EXPECT_DOUBLE_EQ(3.1, z2.getRe());
+    EXPECT_DOUBLE_EQ(3.1, z2.getIm());
 }
