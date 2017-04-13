2ffa7258329e21fe34c9b0794b8694c975910206
Kate V <kate.chiffa@yandex.ru>
2017-04-14 01:49:10 +0400

    Try to fix again

--- modules/complex-number/test/test_vinogradova_ekaterinal_complex_number.cpp
+++ modules/complex-number/test/test_vinogradova_ekaterinal_complex_number.cpp
@@ -13,7 +13,7 @@ TEST(Vinogradova_Ekaterina_ComplexNumberTest, Commutativity_Of_Addition) {
     EXPECT_EQ(z1 + z2, z2 + z1);
 }
 
-TEST(Vinogradova_Ekaterina_ComplexNumberTest, Commutativity_Of_Multiplication) {
+TEST(Vinogradova_Ekaterina_ComplexNumberTest, Commutativity_Of_Mult) {
     // Arrange
     ComplexNumber z1(10.0, 15.0);
     ComplexNumber z2(14.0, 20.0);
@@ -36,7 +36,7 @@ TEST(Vinogradova_Ekaterina_ComplexNumberTest, Associativity_Of_Addition) {
     EXPECT_EQ(left_z, right_z);
 }
 
-TEST(Vinogradova_Ekaterina_ComplexNumberTest, Associativity_Of_Multiplication) {
+TEST(Vinogradova_Ekaterina_ComplexNumberTest, Associativity_Of_Mult) {
     // Arrange
     ComplexNumber z1(15.0, 10.0);
     ComplexNumber z2(14.0, 20.0);
@@ -76,7 +76,7 @@ TEST(Vinogradova_Ekaterina_ComplexNumberTest, Imaginary_Squared_Is_Minus_One) {
     EXPECT_EQ(exp_z, res_z);
 }
 
-TEST(Vinogradova_Ekaterina_ComplexNumberTest, Mult_Of_Conjugate_Numbers_Gives_Zero_Im) {
+TEST(Vinogradova_Ekaterina_ComplexNumberTest, Mult_Of_Conjugate_Give_Zero_Im) {
     // Arrange
     ComplexNumber z1(5.0, 12.0);
     ComplexNumber z2(5.0, -12.0);
