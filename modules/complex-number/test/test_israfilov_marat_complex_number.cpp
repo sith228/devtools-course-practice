f7282b5e236b97b554b587813d3c830c741a2847
Marat_Israfilov <Grendablez@gmail.com>
2017-04-14 00:18:14 +0300

    Tests update

--- modules/complex-number/test/test_israfilov_marat_complex_number.cpp
+++ modules/complex-number/test/test_israfilov_marat_complex_number.cpp
@@ -7,14 +7,12 @@
 TEST(Israfilov_Marat_ComplexNumberTest, Difference_Equal_Numbers_Gives_Zero) {
     // Arrange
     double re = 12.4;
-    double im = -7.7;
-    double re1 = 0.0;
-    double im1 = 0.0;
+    double im = -7.7;    
+    ComplexNumber z2(0.0, 0.0);
 
     // Act
     ComplexNumber z(re, im);
     ComplexNumber z1(re, im);
-    ComplexNumber z2(re1, im1);
 
     // Assert
     EXPECT_EQ(z - z1, z2);
@@ -24,12 +22,10 @@ TEST(Israfilov_Marat_ComplexNumberTest, Add_With_Zero_Does_Not_Change_Number) {
     // Arrange
     double re = 1.2;
     double im = 3.4;
-    double re1 = 0.0;
-    double im1 = 0.0;
+    ComplexNumber z1(0.0, 0.0);
 
     // Act
     ComplexNumber z(re, im);
-    ComplexNumber z1(re1, im1);
 
     // Assert
     EXPECT_EQ(z + z1, z);
@@ -39,12 +35,10 @@ TEST(Israfilov_Marat_ComplexNumberTest, Multiplication_With_Zero_Gives_Zero) {
     // Arrange
     double re = 1.2;
     double im = 3.4;
-    double re1 = 0.0;
-    double im1 = 0.0;
+    ComplexNumber z1(0.0, 0.0);
 
     // Act
     ComplexNumber z(re, im);
-    ComplexNumber z1(re1, im1);
 
     // Assert
     EXPECT_EQ(z * z1, z1);
@@ -54,12 +48,10 @@ TEST(Israfilov_Marat_ComplexNumberTest, Diff_With_Zero_Does_Not_Change_Number) {
     // Arrange
     double re = 1.2;
     double im = 3.4;
-    double re1 = 0.0;
-    double im1 = 0.0;
+    ComplexNumber z1(0.0, 0.0);
 
     // Act
     ComplexNumber z(re, im);
-    ComplexNumber z1(re1, im1);
 
     // Assert
     EXPECT_EQ(z - z1, z);
@@ -69,12 +61,10 @@ TEST(Israfilov_Marat_ComplexNumberTest, Division_Zero_By_Number_Gives_Zero) {
     // Arrange
     double re = 1.2;
     double im = 3.4;
-    double re1 = 0.0;
-    double im1 = 0.0;
+    ComplexNumber z1(0.0, 0.0);
 
     // Act
     ComplexNumber z(re, im);
-    ComplexNumber z1(re1, im1);
 
     // Assert
     EXPECT_EQ(z1 / z, z1);
