9b5bce3d70b8944d8cd5e3e5d7989740aa4a3df6
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-04-14 13:09:21 +0300

    I am sure it was last changes in tests

--- modules/complex-number/test/test_zemlyanskiy.cpp
+++ modules/complex-number/test/test_zemlyanskiy.cpp
@@ -1,3 +1,4 @@
+// Copyright 2017 Korniakov Kirill
 #include <gtest/gtest.h>
 
 #include <string>
@@ -8,59 +9,58 @@
 
 TEST(Zemlyanskiy_TEST,Can_Subtract_Numbers)
 {
-	ComplexNumber s1(5.2, 3.9);
-	ComplexNumber s2(3.8,4.5);
-	ComplexNumber s = s1 - s2;
+    ComplexNumber s1(5.2, 3.9);
+    ComplexNumber s2(3.8,4.5);
+    ComplexNumber s = s1 - s2;
 
-	EXPECT_EQ(s1 - s2, s);
+    EXPECT_EQ(s1 - s2, s);
 }
 
-TEST(Zemlyanskiy_TEST,Can_Get_Real_Value )
+TEST(Zemlyanskiy_TEST,Can_Get_Real_Value)
 {
-	double a = rand() % 100;
-	ComplexNumber z(a,0.0);
+    double a = rand() % 100;
+    ComplexNumber z(a,0.0);
 
-	ASSERT_NO_THROW(z.getRe());
+    ASSERT_NO_THROW(z.getRe());
 }
 
 TEST(Zemlyanskiy_TEST, Can_Get_Imaginary_Value)
 {
-	double b = rand() % 100;
-	ComplexNumber z(0.0, b);
+    double b = rand() % 100;
+    ComplexNumber z(0.0, b);
 
-	ASSERT_NO_THROW(z.getIm());
+    ASSERT_NO_THROW(z.getIm());
 }
 
 TEST(Zemlyanskiy_TEST, Can_Not_Divide_By_Zero)
 {
-	ComplexNumber m1(6.0, 9.0);
-	ComplexNumber m2(0.0, 0.0);
+    ComplexNumber r1(6.0, 9.0);
+    ComplexNumber r2(0.0, 0.0);
 
-	EXPECT_THROW(m1 / m2, std::string);
+    EXPECT_THROW(r1 / r2, std::string);
 }
 
 TEST(Zemlyanskiy_TEST,Can_Multiply_Numbers)
 {
-	ComplexNumber x1(2.2, 2.2);
-	ComplexNumber x2(4.4,4.4);
-	ComplexNumber x = x1*x2;
+    ComplexNumber x1(2.2, 2.2);
+    ComplexNumber x2(4.4,4.4);
+    ComplexNumber x = x1*x2;
 
-	EXPECT_EQ(x1*x2, x);
+    EXPECT_EQ(x1*x2, x);
 }
 
 TEST(Zemlyanskiy_TEST, Can_Set_Real_Value)
 {
-	ComplexNumber z;
-	double re = rand() % 100;
+    ComplexNumber z;
+    double re = rand() % 100;
 
-	ASSERT_NO_THROW(z.setRe(re));
+    ASSERT_NO_THROW(z.setRe(re));
 }
-
 TEST(Zemlyanskiy_TEST, Can_Set_Imaginary_Value)
 {
-	ComplexNumber z;
-	double re = rand() % 100;
+    ComplexNumber z;
+    double re = rand() % 100;
 
-	ASSERT_NO_THROW(z.setRe(re));
+    ASSERT_NO_THROW(z.setRe(re));
 }
 
