e810b3181d0b47a51b28b85ab8050c7bd4999fbb
katrinezor <katrinezor@yandex.ru>
2017-04-14 19:25:41 +0300

    fixed. just some spaces

--- modules/complex-number/test/test_zorina_ekaterina_complex_number.cpp
+++ modules/complex-number/test/test_zorina_ekaterina_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Assign_Complex_Number_To_Itself){
+TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Assign_Complex_Number_To_Itself) {
     // Arrange
     double re = 1.0, im = -1.0;
     // Act
@@ -15,7 +15,7 @@ TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Assign_Complex_Number_To_Itself){
     EXPECT_EQ(z1, z2);
 }
 
-TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Substract_Many_Complex_Numbers){
+TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Substract_Many_Complex_Numbers) {
     // Arrange
     double re = 2.0, im = -1.0;
     // Act
@@ -30,7 +30,7 @@ TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Substract_Many_Complex_Numbers){
     EXPECT_EQ(z, zt);
 }
 
-TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Add_Many_Complex_Numbers){
+TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Add_Many_Complex_Numbers) {
     // Arrange
     double re = 1.0, im = -1.0;
     // Act
@@ -44,7 +44,7 @@ TEST(Zorina_Ekaterina_ComplexNumberTest, Can_Add_Many_Complex_Numbers){
     EXPECT_EQ(z, z1 + z2 + z3 + z4 + z5);
 }
 
-TEST(Zorina_Ekaterina_ComplexNumberTest, Conjugate_Complex_Numbers_Multiply){
+TEST(Zorina_Ekaterina_ComplexNumberTest, Conjugate_Complex_Numbers_Multiply) {
     // Arrange
     double re = 6.0, im = 4.22;
     // Act
@@ -55,7 +55,7 @@ TEST(Zorina_Ekaterina_ComplexNumberTest, Conjugate_Complex_Numbers_Multiply){
     EXPECT_EQ(z, z1 * z2);
 }
 
-TEST(Zorina_Ekaterina_ComplexNumberTest, Copied_Number_Has_Its_Own_Memory){
+TEST(Zorina_Ekaterina_ComplexNumberTest, Copied_Number_Has_Its_Own_Memory) {
     // Arrange
     double re = 1.0, im = -1.0;
     // Act
