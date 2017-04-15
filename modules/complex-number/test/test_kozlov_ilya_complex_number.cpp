def8acf8aedb17124403ff9ce9fc2667a240d7c1
Ilya Kozlov <ilya140297@gmail.com>
2017-04-15 04:16:11 +0300

    Updated tests

--- modules/complex-number/test/test_kozlov_ilya_complex_number.cpp
+++ modules/complex-number/test/test_kozlov_ilya_complex_number.cpp
@@ -1,3 +1,5 @@
+// Copyright 2017 Kozlov Ilya
+
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
@@ -5,21 +7,17 @@
 
 TEST(Kozlov_Ilya_ComplexNumberTest, Existence_Of_Neutral_Element_For_Multiplication)
 {
-    // (a + bi)(1 + 0i) =  a + bi - Существование нейтрального элемента для умножения (свойство)
-
 	const int a = 10;
-    const int b = 15;
-    
-    ComplexNumber C1(a, b);
-    ComplexNumber C2(1, 0);
- 
-    EXPECT_EQ((C1*C2), C1);
+	const int b = 15;
+
+	ComplexNumber C1(a, b);
+	ComplexNumber C2(1, 0);
+
+	EXPECT_EQ((C1*C2), C1);
 }
 
 TEST(Kozlov_Ilya_ComplexNumberTest, DISABLED_Existence_Of_Private_Number)
 {
-    // z1 * z = z2 ==> z = z2 / z1 , (где  z - любое константное косплексное) - существование частного
-    
 	const double az = 5.0;
 	const double bz = 6.0;
 
@@ -27,16 +25,15 @@ TEST(Kozlov_Ilya_ComplexNumberTest, DISABLED_Existence_Of_Private_Number)
 	ComplexNumber z2(11.0, 6.0);
 	ComplexNumber z(az, bz);
 
-    bool res = ((z1 * z == z2) && (z == z2 / z1));
+	bool res = ((z1 * z == z2) && (z == z2 / z1));
 
-    ASSERT_TRUE(res);
+	ASSERT_TRUE(res);
 }
 
 TEST(Kozlov_Ilya_ComplexNumberTest, Divide_And_Multiplicate_With_Z_Is_Equal_To_Z)
 {
-    // z*z/z = z
-    ComplexNumber z(10, 10);
-    
+	ComplexNumber z(10, 10);
+
 	EXPECT_EQ(z*z / z, z);
 }
 
@@ -44,10 +41,10 @@ TEST(Kozlov_Ilya_ComplexNumberTest, SetRe_SetIm_Is_Working_Correctly)
 {
 	const int az = 10;
 	const int bz = 15;
-    
-    ComplexNumber z1(az, bz);
+
+	ComplexNumber z1(az, bz);
 	ComplexNumber z2(1, 1);
-    
+
 	z2.setRe(az);
 	z2.setIm(bz);
 
@@ -65,10 +62,10 @@ TEST(Kozlov_Ilya_ComplexNumberTest, Try_To_GetRe_In_SetRe)
 
 TEST(Kozlov_Ilya_ComplexNumberTest, Compare_Get_In_Set_With_Avarage)
 {
-    const int az = 10;
+	const int az = 10;
 	const int bz = 15;
 
-    ComplexNumber z(az, bz);
+	ComplexNumber z(az, bz);
 	ComplexNumber zCheck(az, bz);
 
 	z.setRe(z.getRe());
@@ -76,4 +73,3 @@ TEST(Kozlov_Ilya_ComplexNumberTest, Compare_Get_In_Set_With_Avarage)
 
 	EXPECT_EQ(z, zCheck);
 }
-
