7235fa404a06bb6ec4c0eef4a74378d86efe7c3f
Ilya Kozlov <ilya140297@gmail.com>
2017-04-15 04:22:38 +0300

    Updated tests

--- modules/complex-number/test/test_kozlov_ilya_complex_number.cpp
+++ modules/complex-number/test/test_kozlov_ilya_complex_number.cpp
@@ -5,71 +5,65 @@
 #include "include/complex_number.h"
 
 
-TEST(Kozlov_Ilya_ComplexNumberTest, Existence_Of_Neutral_Element_For_Multiplication)
-{
-	const int a = 10;
-	const int b = 15;
+TEST(Kozlov_Ilya_ComplexNumberTest, Existence_Of_Neutral_Element_For_Multiplication){
+    const int a = 10;
+    const int b = 15;
 
-	ComplexNumber C1(a, b);
-	ComplexNumber C2(1, 0);
+    ComplexNumber C1(a, b);
+    ComplexNumber C2(1, 0);
 
-	EXPECT_EQ((C1*C2), C1);
+    EXPECT_EQ((C1*C2), C1);
 }
 
-TEST(Kozlov_Ilya_ComplexNumberTest, DISABLED_Existence_Of_Private_Number)
-{
-	const double az = 5.0;
-	const double bz = 6.0;
+TEST(Kozlov_Ilya_ComplexNumberTest, DISABLED_Existence_Of_Private_Number){
+    const double az = 5.0;
+    const double bz = 6.0;
 
-	ComplexNumber z1(10.0, 3.0);
-	ComplexNumber z2(11.0, 6.0);
-	ComplexNumber z(az, bz);
+    ComplexNumber z1(10.0, 3.0);
+    ComplexNumber z2(11.0, 6.0);
+    ComplexNumber z(az, bz);
 
-	bool res = ((z1 * z == z2) && (z == z2 / z1));
+    bool res = ((z1 * z == z2) && (z == z2 / z1));
 
-	ASSERT_TRUE(res);
+    ASSERT_TRUE(res);
 }
 
-TEST(Kozlov_Ilya_ComplexNumberTest, Divide_And_Multiplicate_With_Z_Is_Equal_To_Z)
-{
-	ComplexNumber z(10, 10);
+TEST(Kozlov_Ilya_ComplexNumberTest, Divide_And_Multiplicate_With_Z_Is_Equal_To_Z){
+    ComplexNumber z(10, 10);
 
-	EXPECT_EQ(z*z / z, z);
+    EXPECT_EQ(z*z / z, z);
 }
 
-TEST(Kozlov_Ilya_ComplexNumberTest, SetRe_SetIm_Is_Working_Correctly)
-{
-	const int az = 10;
-	const int bz = 15;
+TEST(Kozlov_Ilya_ComplexNumberTest, SetRe_SetIm_Is_Working_Correctly){
+    const int az = 10;
+    const int bz = 15;
 
-	ComplexNumber z1(az, bz);
+    ComplexNumber z1(az, bz);
 	ComplexNumber z2(1, 1);
 
-	z2.setRe(az);
-	z2.setIm(bz);
+    z2.setRe(az);
+    z2.setIm(bz);
 
-	EXPECT_EQ(z1, z2);
+    EXPECT_EQ(z1, z2);
 }
 
-TEST(Kozlov_Ilya_ComplexNumberTest, Try_To_GetRe_In_SetRe)
-{
-	const int az = 10;
+TEST(Kozlov_Ilya_ComplexNumberTest, Try_To_GetRe_In_SetRe){
+    const int az = 10;
 
-	ComplexNumber z(az, 1);
+    ComplexNumber z(az, 1);
 
-	ASSERT_NO_THROW(z.setRe(z.getRe()));
+    ASSERT_NO_THROW(z.setRe(z.getRe()));
 }
 
-TEST(Kozlov_Ilya_ComplexNumberTest, Compare_Get_In_Set_With_Avarage)
-{
-	const int az = 10;
-	const int bz = 15;
+TEST(Kozlov_Ilya_ComplexNumberTest, Compare_Get_In_Set_With_Avarage){
+    const int az = 10;
+    const int bz = 15;
 
-	ComplexNumber z(az, bz);
-	ComplexNumber zCheck(az, bz);
+    ComplexNumber z(az, bz);
+    ComplexNumber zCheck(az, bz);
 
-	z.setRe(z.getRe());
-	z.setIm(z.getIm());
+    z.setRe(z.getRe());
+    z.setIm(z.getIm());
 
-	EXPECT_EQ(z, zCheck);
+    EXPECT_EQ(z, zCheck);
 }
