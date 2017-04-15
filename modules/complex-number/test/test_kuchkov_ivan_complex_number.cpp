16a0ee8ae9361efc3f90dcdcc9bb3629d2e35c95
Kuchkov Ivan <iheap.ya.ru>
2017-04-15 17:36:08 +0300

    replace tabs with spaces and add little test

--- modules/complex-number/test/test_kuchkov_ivan_complex_number.cpp
+++ modules/complex-number/test/test_kuchkov_ivan_complex_number.cpp
@@ -6,19 +6,30 @@
 
 TEST(Kuchkov_Ivan_ComplexNumberTest, check_create_by_default)
 {
-	ComplexNumber z;
+    ComplexNumber z;
 
-	EXPECT_DOUBLE_EQ(0.0, z.getRe());
-	EXPECT_DOUBLE_EQ(0.0, z.getIm());
+    EXPECT_DOUBLE_EQ(0.0, z.getRe());
+    EXPECT_DOUBLE_EQ(0.0, z.getIm());
 }
 
 TEST(Kuchkov_Ivan_ComplexNumberTest, can_create_non_zero_number) {
-	
-	double re = 3.1415;
-	double im = 2.71828;
 
-	ComplexNumber z(re, im);
+    double re = 3.1415;
+    double im = 2.71828;
 
-	EXPECT_DOUBLE_EQ(re, z.getRe());
-	EXPECT_DOUBLE_EQ(im, z.getIm());
+    ComplexNumber z(re, im);
+
+    EXPECT_DOUBLE_EQ(re, z.getRe());
+    EXPECT_DOUBLE_EQ(im, z.getIm());
+}
+
+TEST(Kuchkov_Ivan_ComplexNumberTest, check_copy_constructor) {
+
+    double re = 3.1415;
+    double im = 2.71828;
+
+    ComplexNumber z1(re, im);
+    ComplexNumber z2(z1);
+
+    ASSERT_TRUE(z1 == z2);
 }
