d307dbdf21e2393e9df54fdcec13c11320289cc2
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-04-23 18:40:59 +0300

    Lead the code to the style standard

--- modules/komarov-nikolay-programmer-calculator/src/programmer_calculator.cxx
+++ modules/komarov-nikolay-programmer-calculator/src/programmer_calculator.cxx
@@ -1,11 +1,57 @@
 // Copyright 2017 Komarov Nikolay
 
+#include <string>
+
 #include "include/programmer_calculator.h"
 
-ProgrammerCalculator::ProgrammerCalculator() {};
+ProgrammerCalculator::ProgrammerCalculator() {}
 
-ProgrammerCalculator::~ProgrammerCalculator() {};
+ProgrammerCalculator::~ProgrammerCalculator() {}
 
 std::string ProgrammerCalculator::convertDecToBin(int dec) {
     return "";
+}
+
+std::string ProgrammerCalculator::convertDecToOct(int dec) {
+    return "";
+}
+
+std::string ProgrammerCalculator::convertDecToHex(int dec) {
+    return "";
+}
+
+std::string ProgrammerCalculator::convertBinToOct(std::string bin) {
+    return "";
+}
+
+int ProgrammerCalculator::convertBinToDec(std::string bin) {
+    return 0;
+}
+
+std::string ProgrammerCalculator::convertBinToHex(std::string bin) {
+    return "";
+}
+
+std::string ProgrammerCalculator::convertOctToBin(std::string oct) {
+    return "";
+}
+
+int ProgrammerCalculator::convertOctToDec(std::string oct) {
+    return 0;
+}
+
+std::string ProgrammerCalculator::convertOctToHex(std::string oct) {
+    return "";
+}
+
+std::string ProgrammerCalculator::convertHexToBin(std::string hex) {
+    return "";
+}
+
+std::string ProgrammerCalculator::convertHexToOct(std::string hex) {
+    return "";
+}
+
+int ProgrammerCalculator::convertHexToDec(std::string hex) {
+    return 0;
 }
