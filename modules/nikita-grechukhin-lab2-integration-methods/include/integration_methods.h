7c31ee9aadadb4e308928423e1a8de21a74ca53a
Nikita Grechuhin <NikitaGrechukhin@gmail.com>
2017-04-23 20:19:41 +0300

    new module

--- modules/nikita-grechukhin-lab2-integration-methods/include/integration_methods.h
+++ modules/nikita-grechukhin-lab2-integration-methods/include/integration_methods.h
@@ -3,14 +3,6 @@
 #ifndef MODULES_NIKITA_GRECHUKHIN_LAB2_INTEGRATION_METHODS_INCLUDE_INTEGRATION_METHODS_H_
 #define MODULES_NIKITA_GRECHUKHIN_LAB2_INTEGRATION_METHODS_INCLUDE_INTEGRATION_METHODS_H_
 
-#include <string>
-
-enum method {
-    Rectangles,
-    Trapezium,
-    Simpson
-};
-
 class IntegrationMethod {
 
 private:
--- modules/nikita-grechukhin-lab2-integration-methods/test/test_integration_method.cpp
+++ modules/nikita-grechukhin-lab2-integration-methods/test/test_integration_method.cpp
@@ -2,9 +2,6 @@
 
 #include <gtest/gtest.h>
 
-#include <string>
-#include <stdexcept>
-
 #include "include/integration_methods.h"
 
 TEST(INTEGRATION, canCreateIntegrationMethod) {
