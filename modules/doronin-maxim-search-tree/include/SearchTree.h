894089849d0e7fae2f8d618676afcddc50bfbfb7
Maxim Doronin <doro156@me.com>
2017-04-25 21:19:38 +0300

    Add SearchTree class declaration

--- modules/doronin-maxim-search-tree/include/SearchTree.h
+++ modules/doronin-maxim-search-tree/include/SearchTree.h
@@ -5,10 +5,33 @@
 
 class Node {
  public:
-     float key;
+     double key;
      Node* left;
      Node* right;
      Node* parent;
+};
+
+class SearchTree {
+ public:
+     SearchTree();
+     ~SearchTree();
+
+     double findMin() const;
+     double findMax() const;
+     double findPrev(const double key) const;
+     double findNext(const double key) const;
+     bool search(const double key) const;
+
+     void insert(const float key);
+     void erase(const float key);
+
+     bool isEmpty() const;
+     bool size() const;
+
+ protected:
+     Node* root;
+     void recursiveDeletion(Node* node);
+     int recursiveSize(const Node* const node) const;
 };
 
 #endif MODULES_SEARCH_TREE_INCLUDE_SEARCH_TREE_H_
