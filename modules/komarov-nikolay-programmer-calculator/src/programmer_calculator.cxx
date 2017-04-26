a2da12c5084b6dac80d6e59b95aa11ac16c91c8c
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-04-26 18:08:38 +0300

    Test for checking wrong input data

--- modules/komarov-nikolay-programmer-calculator/src/programmer_calculator.cxx
+++ modules/komarov-nikolay-programmer-calculator/src/programmer_calculator.cxx
@@ -7,65 +7,119 @@
 ProgrammerCalculator::ProgrammerCalculator() {}
 
 std::string ProgrammerCalculator::convertDecToBin(int dec) {
-    if (dec < 0) return "";
+    if (dec < 0) {
+        return "-1";
+    }
     std::string result = "";
     int quotient = dec;
     int remainder = 0;
+    
     while (quotient >= 2) {
         remainder = quotient % 2;
         quotient = quotient / 2;
         result = std::to_string(remainder) + result;
     }
+
     result = std::to_string(quotient) + result;
+    
     return result;
 }
 
 std::string ProgrammerCalculator::convertDecToOct(int dec) {
-    if (dec < 0) return "";
+    if (dec < 0) {
+        return "-1";
+    }
     std::string result = "";
 
     return result;
 }
 
 std::string ProgrammerCalculator::convertDecToHex(int dec) {
-    if (dec < 0) return "";
+    if (dec < 0) {
+        return "-1";
+    }
     std::string result = "";
 
     return result;
 }
 
 std::string ProgrammerCalculator::convertBinToOct(std::string bin) {
-    return "";
+    if (bin.find_first_not_of("01") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
+    return result;
 }
 
 int ProgrammerCalculator::convertBinToDec(std::string bin) {
-    return 0;
+    if (bin.find_first_not_of("01") != std::string::npos) {
+        return -1;
+    }
+    int result = 0;
+
+    return result;
 }
 
 std::string ProgrammerCalculator::convertBinToHex(std::string bin) {
-    return "";
+    if (bin.find_first_not_of("01") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
+    return result;
 }
 
 std::string ProgrammerCalculator::convertOctToBin(std::string oct) {
+    if (oct.find_first_not_of("01234567") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
     return "";
 }
 
 int ProgrammerCalculator::convertOctToDec(std::string oct) {
-    return 0;
+    if (oct.find_first_not_of("01234567") != std::string::npos) {
+        return -1;
+    }
+    int result = 0;
+
+    return result;
 }
 
 std::string ProgrammerCalculator::convertOctToHex(std::string oct) {
-    return "";
+    if (oct.find_first_not_of("01234567") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
+    return result;
 }
 
 std::string ProgrammerCalculator::convertHexToBin(std::string hex) {
-    return "";
+    if (hex.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
+    return result;
 }
 
 std::string ProgrammerCalculator::convertHexToOct(std::string hex) {
-    return "";
+    if (hex.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) {
+        return "-1";
+    }
+    std::string result = "";
+
+    return result;
 }
 
 int ProgrammerCalculator::convertHexToDec(std::string hex) {
-    return 0;
+    if (hex.find_first_not_of("0123456789abcdefABCDEF") != std::string::npos) {
+        return -1;
+    }
+    int result = 0;
+
+    return result;
 }
--- modules/komarov-nikolay-programmer-calculator/test/test_programmer_calculator.cpp
+++ modules/komarov-nikolay-programmer-calculator/test/test_programmer_calculator.cpp
@@ -15,3 +15,135 @@ TEST(Komarov_Nikolay_Programmer_Calculator_Test, Can_Convert_Dec_To_Bin) {
 
     EXPECT_EQ(binResult, bin);
 }
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Dec_To_Bin) {
+    std::string binResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string bin = "-1";
+    int dec = -83904;
+
+    binResult = programmer_calculator.convertDecToBin(dec);
+
+    EXPECT_EQ(binResult, bin);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Dec_To_Oct) {
+    std::string octResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string oct = "-1";
+    int dec = -49;
+
+    octResult = programmer_calculator.convertDecToOct(dec);
+
+    EXPECT_EQ(octResult, oct);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Dec_To_Hex) {
+    std::string hexResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string hex = "-1";
+    int dec = -8657;
+
+    hexResult = programmer_calculator.convertDecToHex(dec);
+
+    EXPECT_EQ(hexResult, hex);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Bin_To_Oct) {
+    std::string octResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string bin = "69838gjik";
+    std::string oct = "-1";
+
+    octResult = programmer_calculator.convertBinToOct(bin);
+
+    EXPECT_EQ(octResult, oct);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Bin_To_Dec) {
+    int decResult = 0;
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string bin = "433572";
+    int dec = -1;
+
+    decResult = programmer_calculator.convertBinToDec(bin);
+
+    EXPECT_EQ(decResult, dec);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Bin_To_Hex) {
+    std::string hexResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string bin = "9jg348";
+    std::string hex = "-1";
+
+    hexResult = programmer_calculator.convertBinToHex(bin);
+
+    EXPECT_EQ(hexResult, hex);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Oct_To_Bin) {
+    std::string binResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string oct = "86205";
+    std::string bin = "-1";
+
+    binResult = programmer_calculator.convertOctToBin(oct);
+
+    EXPECT_EQ(binResult, bin);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Oct_To_Dec) {
+    int decResult = 0;
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string oct = "jg930#&";
+    int dec = -1;
+
+    decResult = programmer_calculator.convertOctToDec(oct);
+
+    EXPECT_EQ(decResult, dec);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Oct_To_Hex) {
+    std::string hexResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string oct = "289360";
+    std::string hex = "-1";
+
+    hexResult = programmer_calculator.convertOctToHex(oct);
+
+    EXPECT_EQ(hexResult, hex);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Hex_To_Bin) {
+    std::string binResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string hex = "49a7fe";
+    std::string bin = "-1";
+
+    binResult = programmer_calculator.convertHexToBin(hex);
+
+    EXPECT_EQ(binResult, bin);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Hex_To_Oct) {
+    std::string octResult = "";
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string hex = "ig5095uj";
+    std::string oct = "-1";
+
+    octResult = programmer_calculator.convertHexToOct(hex);
+
+    EXPECT_EQ(octResult, oct);
+}
+
+TEST(Komarov_Nikolay_Programmer_Calculator_Test, Wrong_Result_Of_Converting_Hex_To_Dec) {
+    int decResult = 0;
+    ProgrammerCalculator programmer_calculator = *new ProgrammerCalculator();
+    std::string hex = "8592^";
+    int dec = -1;
+
+    decResult = programmer_calculator.convertOctToDec(hex);
+
+    EXPECT_EQ(decResult, dec);
+}
