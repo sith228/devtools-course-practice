3c30ea2473474ef84255c71db6365df66b29e575
Nick Dmitrichev <dmitrichev.nick@gmail.com>
2017-04-30 15:54:12 +0300

    Hah, srtsly? Well, style.

--- modules/converter-currency/test/test_converter_currency.cpp
+++ modules/converter-currency/test/test_converter_currency.cpp
@@ -3,9 +3,9 @@
 #include <gtest/gtest.h>
 
 #include "include/converter_currency.h"
-     
+
 class ConverterCurrenciesTest : public ::testing::Test {
-protected:
+ protected:
     virtual void SetUp() {
         Converter = new converter_currency();
     }
@@ -13,29 +13,24 @@ protected:
 };
 
 TEST_F(ConverterCurrenciesTest, can_not_convert_if_money_size_less_then_zero) {
-
     EXPECT_ANY_THROW(Converter->Convert(-1, RUR, EUR));
 }
 
 TEST_F(ConverterCurrenciesTest, can_convert_if_money_size_equal_to_zero) {
-
     EXPECT_NO_THROW(Converter->Convert(0, RUR, EUR));
 }
 
 TEST_F(ConverterCurrenciesTest, if_zero_money_size_was_converted_its_zero) {
-
     float expected = 0.0f;
     EXPECT_EQ(expected, Converter->Convert(0, RUR, EUR));
 }
 
 TEST_F(ConverterCurrenciesTest, can_convert_if_target_equal_to_old) {
-
     float expected = 111.1f;
     EXPECT_EQ(expected, Converter->Convert(expected, EUR, EUR));
 }
 
 TEST_F(ConverterCurrenciesTest, can_convert_from_RUR_to_RUR) {
-
     float expected = 111.1f;
     EXPECT_EQ(expected, Converter->Convert(expected, RUR, RUR));
 }
@@ -125,6 +120,5 @@ TEST_F(ConverterCurrenciesTest, can_convert_from_CNY_to_USD) {
 }
 
 TEST_F(ConverterCurrenciesTest, there_are_no_problems_with_constructor) {
-
     EXPECT_NO_THROW(new converter_currency());
 }
