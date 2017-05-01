c00a7b781613bd2753ee22d5a784c5cd38bca8f8
VikaGvozdeva <vikagvozdeva96@gmail.com>
2017-05-01 16:36:57 +0300

    Add header file

---
+++ modules/queue/include/queue.h
@@ -0,0 +1,27 @@
+// Copyright 2017 Gvozdeva Viktoria
+
+#ifndef MODULES_GVOZDEVA_VIKTORIA_QUEUE_INCLUDE_QUEUE_H_
+#define MODULES_GVOZDEVA_VIKTORIA_QUEUE_INCLUDE_QUEUE_H_
+
+#include <iostream>
+
+template <class valType>
+
+class Queue {
+private:
+    int head, tail, length, maxSize; ;
+    valType *queuePtr;
+
+public:
+    Queue();
+    Queue(int _maxSize); 
+    ~Queue();
+    Queue (const TQueue & ); 
+    TQueue &operator =(const Queue & ); 
+    bool IsFull() const; 
+    bool IsEmpty() const; 
+    void Push(const valType & ); 
+    valType Pop(); 
+};
+
+#endif  // MODULES_GVOZDEVA_VIKTORIA_QUEUE_INCLUDE_QUEUE_H_
