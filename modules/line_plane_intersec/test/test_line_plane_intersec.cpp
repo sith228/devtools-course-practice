4efd0856554f60396f9725d5ca39dceff3ade6f2
Regmir <arteom.lap@yandex.ru>
2017-05-02 18:05:48 +0300

    More Fixes

--- modules/line_plane_intersec/test/test_line_plane_intersec.cpp
+++ modules/line_plane_intersec/test/test_line_plane_intersec.cpp
@@ -15,7 +15,7 @@ TEST(Lapin_Artem_Line_Plane_Intersec_Test, Do_Throw_When_Line_Parallel_Plane) {
     EXPECT_THROW(Intersec.CalculateIntersection(), std::string);
 }
 
-TEST(Lapin_Artem_Line_Plane_Intersec_Test, CalculateIntersection_Returns_Correct_Result) {
+TEST(Lapin_Artem_Line_Plane_Intersec_Test, Calculate_Return_Is_Correct) {
     // Arrange
     Intersection Intersec(-1, -2, 3, -3, 2, -2, 1, 3, -5, 9);
     std::vector<double> expected(3);
@@ -37,12 +37,12 @@ TEST(Lapin_Artem_Line_Plane_Intersec_Test, GetLine_And_SetLine_Works_Correct) {
     expected[4] = 2;
     expected[5] = 1;
     // Act
-    Intersec.SetLine(6,5,4,3,2,1);
+    Intersec.SetLine(6, 5, 4, 3, 2, 1);
     // Assert
     EXPECT_EQ(Intersec.GetLine(), expected);
 }
 
-TEST(Lapin_Artem_Line_Plane_Intersec_Test, GetPlane_And_SetPlane_Works_Correct) {
+TEST(Lapin_Artem_Line_Plane_Intersec_Test, GetPlane_And_SetPlane_Correct) {
     // Arrange
     Intersection Intersec;
     std::vector<double> expected(4);
@@ -56,7 +56,7 @@ TEST(Lapin_Artem_Line_Plane_Intersec_Test, GetPlane_And_SetPlane_Works_Correct) 
     EXPECT_EQ(Intersec.GetPlane(), expected);
 }
 
-TEST(Lapin_Artem_Line_Plane_Intersec_Test, More_Test_For_CalculateIntersection) {
+TEST(Lapin_Artem_Line_Plane_Intersec_Test, More_Test_For_Calculate) {
     // Arrange
     Intersection Intersec(-3, 0, -2, -1, -3, 3, 1, -2, -1, 3);
     std::vector<double> expected(3);
