2a3108311aa0b2292a7c09edb16bdbf1f61d4b45
allnes <nesoldr@mail.ru>
2017-05-03 00:38:56 +0300

    Added constructor, getter and setter

--- modules/color-spaces/include/rgb_space.h
+++ modules/color-spaces/include/rgb_space.h
@@ -1,6 +1,24 @@
 // Copyright 2017 Nesterov Alexander
-
 #ifndef MODULES_COLOR_SPACES_INCLUDE_RGB_SPACE_H_
 #define MODULES_COLOR_SPACES_INCLUDE_RGB_SPACE_H_
+
+class RGBSpace {
+ private:
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+
+ public:
+    RGBSpace(): red(0), green(0), blue(0) {}
+    RGBSpace(const uint8_t red_, const uint8_t green_, const uint8_t blue_);
+    RGBSpace(const RGBSpace &rgb_space);
+
+    uint8_t GetRed() const;
+    uint8_t GetGreen() const;
+    uint8_t GetBlue() const;
+    void SetRed(const uint8_t red_);
+    void SetGreen(const uint8_t green_);
+    void SetBlue(const uint8_t blue_);
+};
 
 #endif  // MODULES_COLOR_SPACES_INCLUDE_RGB_SPACE_H_
--- modules/color-spaces/src/rgb_space.cpp
+++ modules/color-spaces/src/rgb_space.cpp
@@ -1,3 +1,54 @@
 // Copyright 2017 Nesterov Alexander
 
 #include "../include/rgb_space.h"
+#include <string>
+
+RGBSpace::RGBSpace(const uint8_t red_,
+                   const uint8_t green_,
+                   const uint8_t blue_) {
+    if ((red_ < 0) && (red_ > 255)) {
+        throw std::string("The index of red color isn't in the range 0-255");
+    } else if ((green_ < 0) && (green_ > 255)) {
+        throw std::string("The index of red color isn't in the range 0-255");
+    } else if ((blue_ < 0) && (blue_ > 255)) {
+        throw std::string("The index of red color isn't in the range 0-255");
+    } else {
+        red = red_;
+        green = green_;
+        blue = blue_;
+    }
+}
+
+RGBSpace::RGBSpace(const RGBSpace &rgb_space) {
+    if (&rgb_space == nullptr) {
+        throw std::string("Copying object is nullptr");
+    } else {
+        red = rgb_space.red;
+        green = rgb_space.green;
+        blue = rgb_space.blue;
+    }
+}
+
+uint8_t RGBSpace::GetRed() const {
+    return red;
+}
+
+uint8_t RGBSpace::GetGreen() const {
+    return green;
+}
+
+uint8_t RGBSpace::GetBlue() const {
+    return blue;
+}
+
+void RGBSpace::SetRed(const uint8_t red_) {
+    red = red_;
+}
+
+void RGBSpace::SetGreen(const uint8_t green_) {
+    green = green_;
+}
+
+void RGBSpace::SetBlue(const uint8_t blue_) {
+    blue = blue_;
+}
