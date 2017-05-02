307dc6528c0ceca5f635207410ccae28d3557085
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-02 14:53:43 +0300

    added API for module

--- modules/statistical-moments/include/statistical_moments.h
+++ modules/statistical-moments/include/statistical_moments.h
@@ -1,1 +1,19 @@
+// Copyright 2017 Olga Kiseleva
 
+#ifndef MODULES_STATISTICAL_MOMENTS_INCLUDE_STATISTICAL_MOMENTS_H_
+#define MODULES_STATISTICAL_MOMENTS_INCLUDE_STATISTICAL_MOMENTS_H_
+
+#include <vector>
+#include <string>
+
+class statisticalMoments {
+private:
+    std::vector<double> values;
+    std::vector<double> chanced;
+public:
+    statisticalMoments();
+    statisticalMoments(std::vector<double> values, std::vector<double> chanced);
+    ~statisticalMoments();
+    double getMoment(unsigned int order);
+};
+#endif  // MODULES_STATISTICAL_MOMENTS_INCLUDE_STATISTICAL_MOMENTS_H_
