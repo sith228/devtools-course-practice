18b636776d7dee06630756dfb5d2366749aa94ae
allnes <nesoldr@mail.ru>
2017-05-05 01:29:07 +0300

    unsignet char -> uint8_t

--- modules/color-spaces/src/rgb_space.cpp
+++ modules/color-spaces/src/rgb_space.cpp
@@ -3,6 +3,7 @@
 #include "../include/rgb_space.h"
 #include <string>
 #include <algorithm>
+#include <cstdint>
 
 void RGBSpace::swap(RGBSpace &rgb_space) {
     std::swap(red, rgb_space.red);
@@ -20,9 +21,9 @@ RGBSpace::RGBSpace(const int red_,
     } else if ((blue_ < 0) || (blue_ > 255)) {
         throw std::string("The index of blue color isn't in the range 0-255");
     } else {
-        red = (unsigned char) red_;
-        green = (unsigned char) green_;
-        blue = (unsigned char) blue_;
+        red = (uint8_t) red_;
+        green = (uint8_t) green_;
+        blue = (uint8_t) blue_;
     }
 }
 
@@ -43,15 +44,15 @@ RGBSpace& RGBSpace::operator=(const RGBSpace &rgb_space) {
     return *this;
 }
 
-unsigned char RGBSpace::GetRed() const {
+uint8_t RGBSpace::GetRed() const {
     return red;
 }
 
-unsigned char RGBSpace::GetGreen() const {
+uint8_t RGBSpace::GetGreen() const {
     return green;
 }
 
-unsigned char RGBSpace::GetBlue() const {
+uint8_t RGBSpace::GetBlue() const {
     return blue;
 }
 
@@ -59,7 +60,7 @@ void RGBSpace::SetRed(const int red_) {
     if ((red_ < 0) || (red_ > 255)) {
         throw std::string("The index of red color isn't in the range 0-255");
     } else {
-        red = (unsigned char) red_;
+        red = (uint8_t) red_;
     }
 }
 
@@ -67,7 +68,7 @@ void RGBSpace::SetGreen(const int green_) {
     if ((green_ < 0) || (green_ > 255)) {
         throw std::string("The index of green color isn't in the range 0-255");
     }  else {
-        green = (unsigned char) green_;
+        green = (uint8_t) green_;
     }
 }
 
@@ -75,7 +76,7 @@ void RGBSpace::SetBlue(const int blue_) {
     if ((blue_ < 0) || (blue_ > 255)) {
         throw std::string("The index of blue color isn't in the range 0-255");
     } else {
-        blue = (unsigned char) blue_;
+        blue = (uint8_t) blue_;
     }
 }
 
