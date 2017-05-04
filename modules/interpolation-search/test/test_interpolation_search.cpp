64c768ee3658357547222a5764c58ebdcc03f162
vladjiss <danreegly@gmail.com>
2017-05-04 11:54:19 +0300

    compilation test

--- modules/interpolation-search/test/test_interpolation_search.cpp
+++ modules/interpolation-search/test/test_interpolation_search.cpp
@@ -3,3 +3,9 @@
 #include <gtest/gtest.h>
 
 #include "include/interpolation_search.h"
+
+TEST(InterpolationSearch, Can_Create_Search) {
+  const int length = 3;
+  int * sortedArray = new int[length] {1, 2, 3};
+  ASSERT_NO_THROW(InterpolationSearch interpolationSearch(sortedArray, length));
+}
