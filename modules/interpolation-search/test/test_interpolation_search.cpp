df5391e4ccd893eee98365d47778bacfa5a58d05
vladjiss <danreegly@gmail.com>
2017-05-04 13:49:04 +0300

    Tests fix

--- modules/interpolation-search/test/test_interpolation_search.cpp
+++ modules/interpolation-search/test/test_interpolation_search.cpp
@@ -33,6 +33,12 @@ TEST(InterpolationSearch, Cant_Create_Search_With_Negative_Array_Lenght) {
   ASSERT_ANY_THROW(InterpolationSearch interpolationSearch(sortedArray, -1));
 }
 
+TEST(InterpolationSearch, Cant_Create_Search_With_Zero_Array_Lenght) {
+  const int length = 0;
+  int * sortedArray = new int[length];
+  ASSERT_ANY_THROW(InterpolationSearch interpolationSearch(sortedArray, length));
+}
+
 TEST(InterpolationSearch, Can_Set_Array) {
   const int length = 3;
   int * sortedArray1 = new int[length] {1, 2, 3};
@@ -46,13 +52,6 @@ TEST(InterpolationSearch, Search_Works_Correctly) {
   int * sortedArray = new int[length] {1, 2, 3};
   InterpolationSearch interpolationSearch(sortedArray, length);
   EXPECT_EQ(2, interpolationSearch.Search(3));
-}
-
-TEST(InterpolationSearch, Search_Works_In_One_Elem_Array) {
-  const int length = 1;
-  int * sortedArray = new int[length] {1};
-  InterpolationSearch interpolationSearch(sortedArray, length);
-  EXPECT_EQ(0, interpolationSearch.Search(1));
 }
 
 TEST(InterpolationSearch, Cant_Search_With_Unsorted_Array) {
