fde4217b89fbede834b876b24ab8fbb194a562ab
vladjiss <danreegly@gmail.com>
2017-05-04 11:34:55 +0300

    compilation test

--- modules/interpolation-search/include/interpolation_search.h
+++ modules/interpolation-search/include/interpolation_search.h
@@ -1,18 +1,1 @@
 // Copyright 2017 Yashkov Vladislav
-
-#ifndef MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
-#define MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
-
-class InterpolationSearch {
- public:
-    InterpolationSearch(int * sortedArray, int length);
-    int Search(int toFind);
-    void SetArray(int * sortedArray, int length);
-
- private:
-    int * sortedArray_;
-    int length_;
-    bool isArraySorted(int * sortedArray, int length);
-};
-
-#endif  // MODULES_INTERPOLATION_SEARCH_INCLUDE_INTERPOLATION_SEARCH_H_
