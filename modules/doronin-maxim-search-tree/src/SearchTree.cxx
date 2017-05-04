6eba9893e2f469ef7b51cef3380c3fe09bd78bbb
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2017-05-04 14:04:50 +0300

    Add braces for operators: if-else, while and for

--- modules/doronin-maxim-search-tree/src/SearchTree.cxx
+++ modules/doronin-maxim-search-tree/src/SearchTree.cxx
@@ -10,14 +10,17 @@ SearchTree::~SearchTree() {
 
 const Node* SearchTree::search(const double key) const {
     Node* currentNode = root;
-    if (currentNode == 0)
+    if (currentNode == 0) {
         throw "search: Tree is empty!";
+    }
 
-    while (currentNode != 0 && currentNode->key != key)
-        if (key < currentNode->key)
+    while (currentNode != 0 && currentNode->key != key) {
+        if (key < currentNode->key) {
             currentNode = currentNode->left;
-        else
+        } else {
             currentNode = currentNode->right;
+        }
+    }
 
     return currentNode;
 }
@@ -32,39 +35,46 @@ const Node* SearchTree::findMax() const {
 
 const Node* SearchTree::findMin(const Node* const node) const {
     const Node* currentNode = node;
-    if (currentNode == 0)
+    if (currentNode == 0) {
         throw "searchMin: Tree is empty!";
-    while (currentNode->left != 0)
+    }
+    while (currentNode->left != 0) {
         currentNode = currentNode->left;
+    }
     return currentNode;
 }
 
 const Node* SearchTree::findMax(const Node* const node) const {
     const Node* currentNode = node;
-    if (currentNode == 0)
+    if (currentNode == 0) {
         throw "searchMax: Tree is empty!";
-    while (currentNode->right != 0)
+    }
+    while (currentNode->right != 0) {
         currentNode = currentNode->right;
+    }
     return currentNode;
 }
 
 const Node* SearchTree::findPrev(const double key) const {
     const Node* currentNode = search(key);
-    if (currentNode == 0)
+    if (currentNode == 0) {
         throw "findPrev: there is no element with such key!";
+    }
     return findPrev(currentNode);
 }
 
 const Node* SearchTree::findNext(const double key) const {
     const Node* currentNode = search(key);
-    if (currentNode == 0)
+    if (currentNode == 0) {
         throw "findNext: there is no element with such key!";
+    }
     return findNext(currentNode);
 }
 
 const Node* SearchTree::findPrev(const Node* const node) const {
-    if (node->left != 0)
+    if (node->left != 0) {
         return findMax(node->left);
+    }
 
     const Node* currentNode = node;
     const Node* currentParent = node->parent;
@@ -76,8 +86,9 @@ const Node* SearchTree::findPrev(const Node* const node) const {
 }
 
 const Node* SearchTree::findNext(const Node* const node) const {
-    if (node->right != 0)
+    if (node->right != 0) {
         return findMin(node->right);
+    }
 
     const Node* currentNode = node;
     const Node* currentParent = node->parent;
@@ -99,51 +110,59 @@ void SearchTree::insert(const double key) {
         root = node;
         return;
     }
-    if (search(key) != 0)
+    if (search(key) != 0) {
         throw "insert: Already have such key!";
+    }
 
     Node* currentNode = root;
     Node* tmpParentNode;
     while (currentNode != 0) {
         tmpParentNode = currentNode;
-        if (currentNode->key < node->key)
+        if (currentNode->key < node->key) {
             currentNode = currentNode->right;
-        else
+        } else {
             currentNode = currentNode->left;
+        }
     }
 
     node->parent = tmpParentNode;
-    if (tmpParentNode->key <= node->key)
+    if (tmpParentNode->key <= node->key) {
         tmpParentNode->right = node;
-    else
+    } else {
         tmpParentNode->left = node;
+    }
 }
 
 void SearchTree::erase(const double key) {
     Node* dyingNode = const_cast<Node*>(search(key));
-    if (dyingNode == 0)
+    if (dyingNode == 0) {
         throw "erase: no such element in the tree";
+    }
 
     Node* parent = dyingNode->parent;
     if (dyingNode->left == 0 && dyingNode->right == 0) {
-        if (parent->left == dyingNode)
+        if (parent->left == dyingNode) {
             parent->left = 0;
-        if (parent->right == dyingNode)
+        }
+        if (parent->right == dyingNode) {
             parent->right = 0;
+        }
         delete dyingNode;
         return;
     } else if (dyingNode->left == 0 || dyingNode->right == 0) {
         if (dyingNode->left == 0) {
-            if (parent->left == dyingNode)
+            if (parent->left == dyingNode) {
                 parent->left = dyingNode->right;
-            else
+            } else {
                 parent->right = dyingNode->right;
+            }
             dyingNode->right->parent = parent;
         } else {
-            if (parent->left == dyingNode)
+            if (parent->left == dyingNode) {
                 parent->left = dyingNode->left;
-            else
+            } else {
                 parent->right = dyingNode->left;
+            }
             dyingNode->left->parent = parent;
         }
         delete dyingNode;
@@ -161,10 +180,11 @@ void SearchTree::clear() {
 }
 
 bool SearchTree::isEmpty() const {
-    if (root != 0)
+    if (root != 0) {
         return 0;
-    else
+    } else {
         return 1;
+    }
 }
 
 const int SearchTree::size() const {
@@ -172,8 +192,9 @@ const int SearchTree::size() const {
 }
 
 void SearchTree::recursiveDeletion(Node** const node) {
-    if (*node == 0)
+    if (*node == 0) {
         return;
+    }
     recursiveDeletion(&((*node)->left));
     recursiveDeletion(&((*node)->left));
     delete *node;
@@ -181,8 +202,9 @@ void SearchTree::recursiveDeletion(Node** const node) {
 }
 
 const int SearchTree::recursiveSize(const Node* const node) const {
-    if (node == 0)
+    if (node == 0) {
         return 0;
+    }
     int size;
     size = recursiveSize(node->left) + recursiveSize(node->right);
     return size + 1;
