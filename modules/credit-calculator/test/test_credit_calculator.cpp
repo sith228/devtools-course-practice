7723fe0816076e382bb6b0f4544acb7f0c1bb1c8
Vagik <simonyan.vagik@yandex.ru>
2017-05-05 11:45:05 +0300

    fixed for review #9

--- modules/credit-calculator/test/test_credit_calculator.cpp
+++ modules/credit-calculator/test/test_credit_calculator.cpp
@@ -8,13 +8,13 @@ TEST(Simonyan_Vaginak_CreditCalculator, get_monthly_payment) {
     // Arrange
     int amount = 300000;
     int period = 12;
-    double interest = 22;
+    double inter = 22;
     double result;
     double moth_pay = 28078;
     double month_error = 0.2;
 
     // Act
-    result = CreditCalculator::monthly_payment(amount, period, interest);
+    result = CreditCalculator::monthly_payment(amount, period, inter);
 
     // Assert
     EXPECT_NEAR(moth_pay, result, period*month_error);
@@ -25,34 +25,34 @@ TEST(Simonyan_Vaginak_CreditCalculator,
     // Arrange
     int amount = -300000;
     int period = 12;
-    double interest = 150;
+    double inter = 150;
 
     // Act+Assert
-    ASSERT_ANY_THROW(CreditCalculator::monthly_payment(amount, period, interest));
+    ASSERT_ANY_THROW(CreditCalculator::monthly_payment(amount, period, inter));
 }
 
 TEST(Simonyan_Vaginak_CreditCalculator,
-    get_month_payment_with_low_interest) {
+    get_month_payment_with_low_inter) {
     // Arrange
     int amount = 300000;
     int period = 12;
-    double interest = 1e-15;
+    double inter = 1e-15;
 
     // Act+Assert
-    ASSERT_ANY_THROW(CreditCalculator::monthly_payment(amount, period, interest));
+    ASSERT_ANY_THROW(CreditCalculator::monthly_payment(amount, period, inter));
 }
 
 TEST(Simonyan_Vaginak_CreditCalculator, get_total_payout) {
     // Arrange
     int amount = 300000;
     int period = 12;
-    double interest = 22;
+    double inter = 22;
     double result;
     double payout = 336940;
     double month_error = 0.2;
 
     // Act
-    result = CreditCalculator::total_payout(amount, period, interest);
+    result = CreditCalculator::total_payout(amount, period, inter);
 
     // Assert
     EXPECT_NEAR(payout, result, period*month_error);
@@ -63,23 +63,23 @@ TEST(Simonyan_Vaginak_CreditCalculator,
     // Arrange
     int amount = 300000;
     int period = -12;
-    double interest = 22;
+    double inter = 22;
 
     // Act+Assert
-    ASSERT_ANY_THROW(CreditCalculator::total_payout(amount, period, interest));
+    ASSERT_ANY_THROW(CreditCalculator::total_payout(amount, period, inter));
 }
 
 TEST(Simonyan_Vaginak_CreditCalculator, get_overpayment_amount) {
     // Arrange
     int amount = 300000;
     int period = 12;
-    double interest = 22;
+    double inter = 22;
     double result;
     double overpay = 36940;
     double month_error = 0.2;
 
     // Act
-    result = CreditCalculator::overpayment_amount(amount, period, interest);
+    result = CreditCalculator::overpayment_amount(amount, period, inter);
 
     // Assert
     EXPECT_NEAR(overpay, result, period*month_error);
@@ -90,24 +90,25 @@ TEST(Simonyan_Vaginak_CreditCalculator,
     // Arrange
     int amount = 300000;
     int period = -12;
-    double interest = 22;
+    double inter = 22;
 
     // Act+Assert
-    ASSERT_ANY_THROW(CreditCalculator::overpayment_amount(amount, period, interest));
+    ASSERT_ANY_THROW(CreditCalculator::overpayment_amount(amount,
+        period, inter));
 }
 
 TEST(Simonyan_Vaginak_CreditCalculator, check_balance) {
     // Arrange
     int amount = 300000;
     int period = 12;
-    int month_passed = 5;
-    double interest = 22;
+    int month_pas = 5;
+    double inter = 22;
     double result;
     double balance = 196550;
     double month_error = 0.2;
 
     // Act
-    result = CreditCalculator::check_balance(amount, period, interest, month_passed);
+    result = CreditCalculator::check_balance(amount, period, inter, month_pas);
 
     // Assert
     EXPECT_NEAR(balance, result, period*month_error);
@@ -118,9 +119,10 @@ TEST(Simonyan_Vaginak_CreditCalculator,
     // Arrange
     int amount = -300000;
     int period = 12;
-    int month_passed = 5;
-    double interest = 22;
+    int month_pas = 5;
+    double inter = 22;
 
     // Act+Assert
-    ASSERT_ANY_THROW(CreditCalculator::check_balance(amount, period, interest, month_passed));
+    ASSERT_ANY_THROW(CreditCalculator::check_balance(amount, period,
+        inter, month_pas));
 }
