bd14da655d73e94c5ac727bc6e107ab4e60cca0f
Kate V <kate.chiffa@yandex.ru>
2017-05-05 23:09:46 +0400

    Style changes

--- modules/deque/include/deque.h
+++ modules/deque/include/deque.h
@@ -1,30 +1,27 @@
 // Copyright 2017 Vinogradova Ekaterina
 
-#ifndef DEQUE_H_
-#define DEQUE_H_
+#ifndef MODULES_DEQUE_INCLUDE_DEQUE_H_
+#define MODULES_DEQUE_INCLUDE_DEQUE_H_
 
 #include <iostream>
 
-using namespace std;
+#define DefMemSize   25
 
-#define DefMemSize   25     // размер памяти по умолчанию
-
-typedef double    TElem;    // тип элемента
+typedef double    TElem;
 typedef TElem*    PTElem;
-typedef double    TData;    // тип значений
+typedef double    TData;
 
-class Deque
-{
-protected:
-    PTElem pMem;      // память для СД
+class Deque {
+ protected:
+    PTElem pMem;
     int Front;
     int Back;
-    int MemSize;      // размер памяти для СД
-    int DataCount;    // количество элементов в СД
+    int MemSize;
+    int DataCount;
 
     int GetNextFrontIndex(int index);
     int GetNextBackIndex(int index);
-public:
+ public:
     Deque(int size = DefMemSize);
     ~Deque();
     void InsFront(TElem element);
@@ -35,4 +32,4 @@ public:
     bool IsFull(void) const;
 };
 
-#endif  // DEQUE_H_
+#endif  // MODULES_DEQUE_INCLUDE_DEQUE_H_
