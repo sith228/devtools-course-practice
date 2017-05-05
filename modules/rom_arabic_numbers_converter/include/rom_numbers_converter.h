30b2c909daa3542812e4608a780dafad0821a265
Ivan Kochin <ivan.kochin@inbox.ru>
2017-05-06 02:08:44 +0300

    CodeCheck1

--- modules/rom_arabic_numbers_converter/include/rom_numbers_converter.h
+++ modules/rom_arabic_numbers_converter/include/rom_numbers_converter.h
@@ -2,8 +2,8 @@
 
 
 
-#ifndef MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_ 
-#define MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_ 
+#ifndef MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_
+#define MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_
 
 #include <string>
 #include <iostream>
@@ -11,8 +11,8 @@
 using std::string;
 
 class RomNumConverter {
-    public:
+    public: 
     string convert_arabic_to_rom(int temp);
     int convert_rom_to_arabic(string bufstr);
 };
-#endif  // MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_ 
+#endif  // MODULES_ROM_ARABIC_NUMBERS_CONVERTER_INCLUDE_ROM_NUMBERS_CONVERTER_H_
