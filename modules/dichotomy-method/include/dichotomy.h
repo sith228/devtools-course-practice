c0d33e0bb148cb0443f2c6119f728bd493d097c1
Maxim <thanatormax@gmail.com>
2017-05-06 14:48:13 +0300

    Moar style fixes

--- modules/dichotomy-method/include/dichotomy.h
+++ modules/dichotomy-method/include/dichotomy.h
@@ -4,11 +4,10 @@
 #define MODULES_DICHOTOMY_METHOD_INCLUDE_DICHOTOMY_H_
 
 //За унимодальную функцию примем y = afactor*(x+bfactor)^2 + cfactor
-class Dichotomy
-{
+class Dichotomy{
  public:
     double findMin(int afactor,int bfactor, int cfactor,
-         double leftBorder, double rightBorder, double eps);
+                   double leftBorder, double rightBorder, double eps);
 };
 
 #endif  // MODULES_DICHOTOMY_METHOD_INCLUDE_DICHOTOMY_H_
