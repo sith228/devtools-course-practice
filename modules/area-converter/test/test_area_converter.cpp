1701c85a7812718b625aad1049cee5194020ea3e
Kuchkov Ivan <iheap.ya.ru>
2017-05-06 18:33:46 +0300

    fix whitespace in end of lines

--- modules/area-converter/test/test_area_converter.cpp
+++ modules/area-converter/test/test_area_converter.cpp
@@ -7,7 +7,7 @@
 static const double Neighbourhood = 0.0000001;
 
 TEST(AreaConverterTest, check_create_converter_unit) {
-    AreaConverter c = AreaConverter(AreaConverter::centimeter, 
+    AreaConverter c = AreaConverter(AreaConverter::centimeter,
                                     AreaConverter::meter);
 
     double r1 = c(123);
@@ -17,7 +17,7 @@ TEST(AreaConverterTest, check_create_converter_unit) {
 }
 
 TEST(AreaConverterTest, check_acr_to_yard_convertion) {
-    double r1 = AreaConverter(AreaConverter::acr, 
+    double r1 = AreaConverter(AreaConverter::acr,
                                 AreaConverter::yard)(3.1415);
     double r2 = 15204.86;
 
@@ -25,7 +25,7 @@ TEST(AreaConverterTest, check_acr_to_yard_convertion) {
 }
 
 TEST(AreaConverterTest, can_operate_with_zero_size_areas) {
-    double r1 = AreaConverter(AreaConverter::meter, 
+    double r1 = AreaConverter(AreaConverter::meter,
                                 AreaConverter::inch)(0.);
     double r2 = 0.;
 
@@ -33,14 +33,14 @@ TEST(AreaConverterTest, can_operate_with_zero_size_areas) {
 }
 
 TEST(AreaConverterTest, is_negative_size_area_works_correct) {
-    AreaConverter c = AreaConverter(AreaConverter::meter, 
+    AreaConverter c = AreaConverter(AreaConverter::meter,
                                     AreaConverter::inch);
 
     EXPECT_ANY_THROW(c(-2.1718));
 }
 
 TEST(AreaConverterTest, check_convert_multiple_times_with_different_arguments) {
-    AreaConverter c = AreaConverter(AreaConverter::centimeter, 
+    AreaConverter c = AreaConverter(AreaConverter::centimeter,
                                     AreaConverter::acr);
     int size = 10;
     double delta = 0.25;
@@ -49,7 +49,7 @@ TEST(AreaConverterTest, check_convert_multiple_times_with_different_arguments) {
 
     for (int i = 0; i < size; ++i) {
         r1.push_back(c(i * delta));
-        r2.push_back(AreaConverter(AreaConverter::centimeter, 
+        r2.push_back(AreaConverter(AreaConverter::centimeter,
                                     AreaConverter::acr)(i * delta));
     }
 
