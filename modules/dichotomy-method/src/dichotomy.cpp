93dc0b03ddf9a1666e04f62e3152f1dadf3f7ba6
Maxim <thanatormax@gmail.com>
2017-05-06 17:26:54 +0300

    And again...

--- modules/dichotomy-method/src/dichotomy.cpp
+++ modules/dichotomy-method/src/dichotomy.cpp
@@ -5,12 +5,10 @@
 //За унимодальную функцию примем y = afactor*(x+bfactor)^2 + cfactor
 
 double Dichotomy::findMin(int afactor, int bfactor, int cfactor,
-     double leftBorder, double rightBorder, double eps){
-
+     double leftBorder, double rightBorder, double eps) {
     double c = (leftBorder + rightBorder)/2;
     double funcValueC = afactor*(c+bfactor)*(c+bfactor) + cfactor;
-    while( rightBorder - leftBorder > eps )
-    {
+    while (rightBorder - leftBorder > eps) {
         double x = (leftBorder + c)/2;
         double funcValueX = afactor*(x+bfactor)*(x+bfactor) + cfactor;
         double y = (c + rightBorder)/2;
@@ -20,21 +18,16 @@ double Dichotomy::findMin(int afactor, int bfactor, int cfactor,
                 rightBorder = c;
                 c = x;
                 funcValueC = funcValueX;
-            }
-        else if ( (funcValueC < funcValueX) && (funcValueC < funcValueY) ){
-
+            } else if ( (funcValueC < funcValueX) && (funcValueC < funcValueY) ) {
                 leftBorder = x;
                 rightBorder = y;
-            }
-        else{
-
+            } else {
                 leftBorder = c;
 				c = y;
                 funcValueC = funcValueY;
             }
-
         //Поскольку метод дихотомии должен вернуть отрезок, на котором
         //должен находиться минимум, возьмём за оценку решения его середину
     }
-     return (leftBorder + rightBorder) / 2;
+    return (leftBorder + rightBorder) / 2;
 }
