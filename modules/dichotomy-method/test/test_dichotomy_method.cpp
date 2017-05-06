7001e84c70c8a8980ff484fac2c1e335531b3dbd
Maxim <thanatormax@gmail.com>
2017-05-06 20:32:01 +0300

    shhhhh

--- modules/dichotomy-method/test/test_dichotomy_method.cpp
+++ modules/dichotomy-method/test/test_dichotomy_method.cpp
@@ -3,27 +3,27 @@
 #include <gtest/gtest.h>
 #include "include/dichotomy.h"
 
-TEST(Kildishev_Maxim_Dichotomy_Method_Tests, function_work) {
+TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Function_work) {
     Dichotomy obj;
     EXPECT_NO_THROW(obj.findMin(1, 0, 0, -1, 1, 0.01));
 }
 
-TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_Incorrect_Left_Border) {
+TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_incorrect_left_border) {
     Dichotomy obj;
     EXPECT_ANY_THROW(obj.findMin(1, 0, 0, -0.5, -1, 0.01));
 }
 
-TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_Incorrect_Right_Border) {
+TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_incorrect_right_border) {
     Dichotomy obj;
     EXPECT_ANY_THROW(obj.findMin(1, 0, 0, 1, 0.5, 0.01));
 }
 
-TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_Close_Values) {
+TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Choose_close_values) {
     Dichotomy obj;
     EXPECT_ANY_THROW(obj.findMin(1, 0, 0, 0.5, 0.5555555, 0.01));
 }
 
-TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Throw_When_Function_Is_Zero) {
+TEST(Kildishev_Maxim_Dichotomy_Method_Tests, Throw_when_function_is_zero) {
     Dichotomy obj;
     EXPECT_ANY_THROW(obj.findMin(0, 1, 0, -1, 1, 0.01));
 }
