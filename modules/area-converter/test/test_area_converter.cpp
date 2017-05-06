2c6b16b2ca560f75e6bb88824491c55effa0b81f
Kuchkov Ivan <iheap.ya.ru>
2017-05-06 18:22:11 +0300

    fix braces and spaces in tests

--- modules/area-converter/test/test_area_converter.cpp
+++ modules/area-converter/test/test_area_converter.cpp
@@ -6,8 +6,7 @@
 
 static const double Neighbourhood = 0.0000001;
 
-TEST(AreaConverterTest, check_create_converter_unit)
-{
+TEST(AreaConverterTest, check_create_converter_unit) {
     AreaConverter c = AreaConverter(AreaConverter::centimeter, AreaConverter::meter);
 
     double r1 = c(123);
@@ -16,33 +15,27 @@ TEST(AreaConverterTest, check_create_converter_unit)
     ASSERT_NEAR(r1, r2, Neighbourhood);
 }
 
-TEST(AreaConverterTest, check_acr_to_yard_convertion)
-{
-   
+TEST(AreaConverterTest, check_acr_to_yard_convertion) {
     double r1 = AreaConverter(AreaConverter::acr, AreaConverter::yard)(3.1415);
     double r2 = 15204.86;
 
     ASSERT_NEAR(r1, r2, Neighbourhood);
 }
 
-TEST(AreaConverterTest, can_operate_with_zero_size_areas)
-{
-
+TEST(AreaConverterTest, can_operate_with_zero_size_areas) {
     double r1 = AreaConverter(AreaConverter::meter, AreaConverter::inch)(0.);
     double r2 = 0.;
 
     EXPECT_DOUBLE_EQ(r1, r2);
 }
 
-TEST(AreaConverterTest, is_negative_size_area_works_correct)
-{
+TEST(AreaConverterTest, is_negative_size_area_works_correct) {
     AreaConverter c = AreaConverter(AreaConverter::meter, AreaConverter::inch);
-    
+
     EXPECT_ANY_THROW(c(-2.1718));
 }
 
-TEST(AreaConverterTest, check_convert_multiple_times_with_different_arguments)
-{
+TEST(AreaConverterTest, check_convert_multiple_times_with_different_arguments) {
     AreaConverter c = AreaConverter(AreaConverter::centimeter, AreaConverter::acr);
     int size = 10;
     double delta = 0.25;
