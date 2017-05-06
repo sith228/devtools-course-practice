8e5869ab983216bc7bfa37e262de85520149a04b
Maxim <thanatormax@gmail.com>
2017-05-06 17:37:49 +0300

    ....

--- modules/dichotomy-method/test/test_dichotomy_method.cpp
+++ modules/dichotomy-method/test/test_dichotomy_method.cpp
@@ -3,8 +3,7 @@
 #include <gtest/gtest.h>
 #include "include/dichotomy.h"
 
-TEST(Kildishev_Maxim_DichotomyMethod_Tests, First)
-{
+TEST(Kildishev_Maxim_DichotomyMethod_Tests, First){
     Dichotomy obj;
-    EXPECT_NO_THROW(obj.findMin(1,0,0,-1,-1,0.01));
+    EXPECT_NO_THROW(obj.findMin(1, 0, 0, -1, -1, 0.01));
 }
