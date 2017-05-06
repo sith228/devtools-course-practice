e9ee1db5664b94ff87f834447c48e6be026b3ddc
Kuchkov Ivan <iheap.ya.ru>
2017-05-06 16:56:08 +0300

    fix negative input parameters

--- modules/area-converter/src/area_converter.cpp
+++ modules/area-converter/src/area_converter.cpp
@@ -5,5 +5,9 @@ AreaConverter::AreaConverter(double from, double to)
     : ratio_(Square(from / to)) {}
 
 double AreaConverter::operator()(double x) {
+    if (x < 0) {
+        throw "It is impossible to work with negative area";
+    }
+
     return x * ratio_;
 }
