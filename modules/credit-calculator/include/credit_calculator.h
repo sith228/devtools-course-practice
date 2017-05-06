e9290599b8bcf2d8e2355ef0edf00f546fa01c3f
Vagik <simonyan.vagik@yandex.ru>
2017-05-06 17:18:18 +0300

    fixed for review #13

--- modules/credit-calculator/include/credit_calculator.h
+++ modules/credit-calculator/include/credit_calculator.h
@@ -14,7 +14,7 @@ class CreditCalculator {
  private:
   static bool check_data(int amount, int period, double interest);
   static bool check_data(int amount, int period,
-      double interest, unsigned int month_pas);
+      double interest, int month_pas);
 };
 
 #endif  // MODULES_CREDIT_CALCULATOR_INCLUDE_CREDIT_CALCULATOR_H_
