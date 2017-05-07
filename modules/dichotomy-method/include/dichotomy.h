b5a7396a281848cebe368d311a72cd3a551ce8e7
Maxim <thanatormax@gmail.com>
2017-05-07 15:08:37 +0300

    Method place fix

--- modules/dichotomy-method/include/dichotomy.h
+++ modules/dichotomy-method/include/dichotomy.h
@@ -5,12 +5,12 @@
 
 //За унимодальную функцию примем y = a_factor*(x+b_factor)^2 + c_factor
 class Dichotomy{
+ public:
+   double FindMin(int a_factor, int b_factor, int c_factor,
+       double left_border, double right_border, double eps);
  private:
     void EnteredCorrectly(int a_factor, double left_border,
          double right_border, double eps);
- public:
-    double FindMin(int a_factor, int b_factor, int c_factor,
-        double left_border, double right_border, double eps);
 };
 
 #endif  // MODULES_DICHOTOMY_METHOD_INCLUDE_DICHOTOMY_H_
