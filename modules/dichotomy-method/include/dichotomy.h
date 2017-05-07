a6dcb9456cec200b577629352fdbe10320bd7bc7
Maxim <thanatormax@gmail.com>
2017-05-07 15:13:56 +0300

    Spaces

--- modules/dichotomy-method/include/dichotomy.h
+++ modules/dichotomy-method/include/dichotomy.h
@@ -6,10 +6,10 @@
 //За унимодальную функцию примем y = a_factor*(x+b_factor)^2 + c_factor
 class Dichotomy{
  public:
-   double FindMin(int a_factor, int b_factor, int c_factor,
+     double FindMin(int a_factor, int b_factor, int c_factor,
        double left_border, double right_border, double eps);
  private:
-    void EnteredCorrectly(int a_factor, double left_border,
+     void EnteredCorrectly(int a_factor, double left_border,
          double right_border, double eps);
 };
 
