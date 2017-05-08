06ea4a6ba83e59fcfb54dda2683177ab6cefae36
Игорь Федоров <fedorovigor1155@gmail.com>
2017-05-08 22:25:22 +0300

    fix1

--- modules/root-finder/src/newton-method.cpp
+++ modules/root-finder/src/newton-method.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 bool newton_method::IsMonotone(float(*fun)(float)) {
     if (A > B) {
         std::swap(A, B);
@@ -44,7 +46,7 @@ float newton_method::FindRoot(float(*fun)(float), float(*deriv_fun)(float), floa
         float xn = fun(A)*deriv2_fun(A) > 0 ? A : B;
         float x1 = xn - fun(xn) / deriv_fun(xn);
         float x0 = xn;
-        while (std::abs(x0 - x1) > Acc) {
+        while (abs(x0 - x1) > Acc) {
             if (x1 < A || x1 > B)
                 throw("segment have not desidion");
             x0 = x1;
