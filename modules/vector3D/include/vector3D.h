0f26d332a70deab1ce8dba1e95f6a8ba334df10f
katrinezor <katrinezor@yandex.ru>
2017-05-08 09:18:36 +0300

    Зорина - Лабораторная работа №2

---
+++ modules/vector3D/include/vector3D.h
@@ -0,0 +1,48 @@
+#ifndef __VECTOR3D_H__
+#define __VECTOR3D_H__
+
+#include <iostream>
+using namespace std;
+
+// Вектор в трёхмерном пространстве
+class Vector3D {
+protected:
+    double x;
+    double y;
+    double z;
+public:
+    Vector3D(double _x = 0.0, double _y = 0.0, double _z = 0.0);
+    Vector3D(const Vector3D &v);                // конструктор копирования
+    double GetX() const { return x; };            // доступ
+    double GetY() const { return y; };
+    double GetZ() const { return z; };
+
+    bool operator==(const Vector3D &v) const;  // сравнение
+    bool operator!=(const Vector3D &v) const;  // сравнение
+    bool IsCloseTo(const Vector3D &v) const;    // приближённое сравнение
+                                                // для проверки вычислений
+    Vector3D& operator=(const Vector3D &v);     // присваивание
+
+                                              // векторные операции
+    Vector3D  operator+(const Vector3D &v);     // сложение
+    Vector3D  operator-(const Vector3D &v);     // вычитание
+    double Norm();                        // норма вектора
+    Vector3D Normalize();                // нормализация вектора
+    double  operator*(const Vector3D &v);     // скалярное произведение
+    Vector3D  operator^(const Vector3D &v);    // векторное произв-е v(v1, v2)
+
+                                              // ввод-вывод
+    friend istream& operator>>(istream &in, Vector3D &v) {
+        in >> v.x >> v.y >> v.z;
+
+        return in;
+    }
+    friend ostream& operator<<(ostream &out, const Vector3D &v) {
+        out << "(" << v.x << ", " << v.y << ", " << v.z << ")" << endl;
+
+        return out;
+    }
+};
+
+
+#endif
---
+++ modules/vector3D/src/vector3D.cpp
@@ -0,0 +1,97 @@
+#include "vector3D.h"
+
+
+Vector3D::Vector3D(double _x, double _y, double _z) {
+    x = _x;
+    y = _y;
+    z = _z;
+}
+
+Vector3D::Vector3D(const Vector3D &v) {
+    x = v.x;
+    y = v.y;
+    z = v.z;
+}
+
+bool Vector3D::operator==(const Vector3D &v) const {
+    if (x == v.x && y == v.y && z == v.z)
+        return true;
+    else
+        return false;
+}
+
+bool Vector3D::operator!=(const Vector3D &v) const {
+    return !(*this == v);
+}
+
+bool Vector3D::IsCloseTo(const Vector3D & v) const {
+    double epsilon = 0.00000000000001;    // 14 знаков после запятой
+
+    if ((abs(x - v.x) < epsilon) &&
+        (abs(y - v.y) < epsilon) &&
+        (abs(z - v.z) < epsilon))
+        return true;
+    else
+        return false;
+}
+
+Vector3D& Vector3D::operator=(const Vector3D &v) {
+    if (*this != v)
+    {
+        x = v.x;
+        y = v.y;
+        z = v.z;
+    }
+    return *this;
+}
+
+Vector3D Vector3D::operator+(const Vector3D &v) {
+    Vector3D temp;
+    temp.x = x + v.x;
+    temp.y = y + v.y;
+    temp.z = z + v.z;
+
+    return temp;
+}
+
+Vector3D Vector3D::operator-(const Vector3D &v) {
+    Vector3D temp;
+    temp.x = x - v.x;
+    temp.y = y - v.y;
+    temp.z = z - v.z;
+
+    return temp;
+}
+
+double Vector3D::Norm() {
+    return sqrt(x*x + y*y + z*z);
+}
+
+Vector3D Vector3D::Normalize() {
+    double norm = Norm();
+    if (norm != 0.0)
+    {
+        x = x / norm;
+        y = y / norm;
+        z = z / norm;
+    }
+
+    return *this;
+}
+
+double Vector3D::operator*(const Vector3D & v) {
+    return x*v.x + y*v.y + z*v.z;
+}
+
+Vector3D Vector3D::operator^(const Vector3D & v) {
+    Vector3D vt(*this);
+    if (vt.x != 0 || vt.y != 0 || vt.z != 0)
+        if (v.x != 0 || v.y != 0 || v.z != 0)
+        {
+            x = vt.y*v.z - vt.z*v.y;
+            y = vt.z*v.x - vt.x*v.z;
+            z = vt.x*v.y - vt.y*v.x;
+        }
+
+    return *this;
+}
