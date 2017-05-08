578008ce4785e900defd41ea02075f3176cd4fa7
Splav555 <olegka35@gmail.com>
2017-05-09 01:41:59 +0300

    code fix

--- modules/length_converter/include/length_converter.h
+++ modules/length_converter/include/length_converter.h
@@ -44,8 +44,25 @@ class Length {
     double ConvertTo(LENGTH_TYPE type) const;
 
  private:
-    double length_cm;  // Длина в сантиметрах
+    double length_cm_;  // Длина в сантиметрах
     double GetConvertCoef(LENGTH_TYPE type) const;
+
+    static constexpr const double micrometre_to_cm_ = 0.0001;
+    static constexpr const double millimetre_to_cm_ = 0.1;
+    static constexpr const double centimetre_to_cm_ = 1.0;
+    static constexpr const double decimetre_to_cm_ = 10.0;
+    static constexpr const double metre_to_cm_ = 100.0;
+    static constexpr const double kilometre_to_cm_ = 100000.0;
+    static constexpr const double inch_to_cm_ = 2.54;
+    static constexpr const double hand_to_cm_ = 10.16;
+    static constexpr const double link_to_cm_ = 20.1168;
+    static constexpr const double foot_to_cm_ = 30.48;
+    static constexpr const double yard_to_cm_ = 91.44;
+    static constexpr const double rod_to_cm_ = 502.92;
+    static constexpr const double chain_to_cm_ = 2011.68;
+    static constexpr const double furhlong_to_cm_ = 20116.8;
+    static constexpr const double mile_to_cm_ = 160934.4;
+    static constexpr const double league_to_cm_ = 482803.2;
 };
 
 #endif  // MODULES_LENGTH_CONVERTER_INCLUDE_LENGTH_CONVERTER_H_
--- modules/length_converter/src/length_converter.cpp
+++ modules/length_converter/src/length_converter.cpp
@@ -3,68 +3,53 @@
 #include "include/length_converter.h"
 
 /* Коэффициенты перевода единицы измерения в см */
-#define MICROMETRE_TO_CM    0.0001
-#define MILLIMETRE_TO_CM    0.1
-#define CENTIMETRE_TO_CM    1.0
-#define DECIMETRE_TO_CM     10.0
-#define METRE_TO_CM         100.0
-#define KILOMETRE_TO_CM     100000.0
-#define INCH_TO_CM          2.54
-#define HAND_TO_CM          10.16
-#define LINK_TO_CM          20.1168
-#define FOOT_TO_CM          30.48
-#define YARD_TO_CM          91.44
-#define ROD_TO_CM           502.92
-#define CHAIN_TO_CM         2011.68
-#define FURHLONG_TO_CM      20116.8
-#define MILE_TO_CM          160934.4
-#define LEAGUE_TO_CM        482803.2
+
 
 Length::Length(LENGTH_TYPE type, double value) {
     SetValue(type, value);
 }
 
 void Length::SetValue(LENGTH_TYPE type, double value) {
-    length_cm = value * GetConvertCoef(type);
+    length_cm_ = value * GetConvertCoef(type);
 }
 
 double Length::GetConvertCoef(LENGTH_TYPE type) const {
     switch (type) {
     case MICROMETRE:
-        return MICROMETRE_TO_CM;
+        return micrometre_to_cm_;
     case MILLIMETRE:
-        return MILLIMETRE_TO_CM;
+        return millimetre_to_cm_;
     case DECIMETRE:
-        return DECIMETRE_TO_CM;
+        return decimetre_to_cm_;
     case METRE:
-        return METRE_TO_CM;
+        return metre_to_cm_;
     case KILOMETRE:
-        return KILOMETRE_TO_CM;
+        return kilometre_to_cm_;
     case INCH:
-        return INCH_TO_CM;
+        return inch_to_cm_;
     case HAND:
-        return HAND_TO_CM;
+        return hand_to_cm_;
     case LINK:
-        return LINK_TO_CM;
+        return link_to_cm_;
     case FOOT:
-        return FOOT_TO_CM;
+        return foot_to_cm_;
     case YARD:
-        return YARD_TO_CM;
+        return yard_to_cm_;
     case ROD:
-        return ROD_TO_CM;
+        return rod_to_cm_;
     case CHAIN:
-        return CHAIN_TO_CM;
+        return chain_to_cm_;
     case FURHLONG:
-        return FURHLONG_TO_CM;
+        return furhlong_to_cm_;
     case MILE:
-        return MILE_TO_CM;
+        return mile_to_cm_;
     case LEAGUE:
-        return LEAGUE_TO_CM;
+        return league_to_cm_;
     default:
-        return CENTIMETRE_TO_CM;
+        return centimetre_to_cm_;
     }
 }
 
 double Length::ConvertTo(LENGTH_TYPE type) const {
-    return length_cm / GetConvertCoef(type);
+    return length_cm_ / GetConvertCoef(type);
 }
