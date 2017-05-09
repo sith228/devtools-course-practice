a43b2061b5934ce50b515d1ba6aeae41170e7c12
Игорь Федоров <fedorovigor1155@gmail.com>
2017-05-09 14:22:11 +0300

    fix1

--- modules/root-finder/include/newton-method.h
+++ modules/root-finder/include/newton-method.h
@@ -4,22 +4,23 @@
 #define MODULES_ROOT_FINDER_INCLUDE_NEWTON_METHOD_H_
 
 class newton_method {
-public:
-    void setAB(float a,float b) {
+ public:
+    void setAB(float a, float b) {
         A = a;
         B = b;
-    };
+    }
     void setAccuracy(float acc) {
         Acc = acc;
-    };
+    }
 
-    float FindRoot(float(*fun)(float), float(*deriv_fun)(float), float(*deriv2_fun)(float));
+    float FindRoot(float(*fun)(float),
+        float(*deriv_fun)(float), float(*deriv2_fun)(float));
 
-private:
+ private:
     float A;
     float B;
     float Acc;
     bool IsMonotone(float(*fun)(float));
 };
-#endif
+#endif  // MODULES_ROOT_FINDER_INCLUDE_NEWTON_METHOD_H_
 
