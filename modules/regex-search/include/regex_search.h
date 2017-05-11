c14ddc972638408bbc89a83aea01cb9292d4f247
Andrew Kazakov <tosk-nn@yandex.ru>
2017-05-12 00:18:49 +0300

    Implemented RegexSearch class

--- modules/regex-search/include/regex_search.h
+++ modules/regex-search/include/regex_search.h
@@ -1,1 +1,28 @@
 // Copyright 2017 Kazakov Andrew
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+typedef std::vector<unsigned int> RegexSearchResult;
+
+class RegexSearch {
+public:
+    RegexSearch(const std::string);
+    RegexSearch(const RegexSearch&);
+    RegexSearch& operator=(const RegexSearch&);
+    ~RegexSearch(void);
+
+    void SetRegex(const std::string);
+    std::regex GetRegex(void) const;
+
+    RegexSearchResult Find(std::string str);
+    RegexSearchResult FindInFile(std::string filename);
+
+protected:
+    std::regex regex_;
+};
--- modules/regex-search/src/regex_search.cpp
+++ modules/regex-search/src/regex_search.cpp
@@ -1,1 +1,57 @@
 // Copyright 2017 Kazakov Andrew
+
+#include "regex_search.h"
+
+RegexSearch::RegexSearch(const std::string regex) {
+    regex_ = std::regex(regex);
+}
+
+RegexSearch::RegexSearch(const RegexSearch& regex) {
+    regex_ = regex.regex_;
+}
+
+RegexSearch& RegexSearch::operator=(const RegexSearch& regex) {
+    regex_ = regex.regex_;
+
+    return *this;
+}
+
+void RegexSearch::SetRegex(const std::string regex) {
+    regex_ = regex;
+}
+
+std::regex RegexSearch::GetRegex() const {
+    return regex_;
+}
+
+RegexSearchResult RegexSearch::Find(std::string str) {
+    std::smatch matches;
+    RegexSearchResult res;
+
+    while (std::regex_search(str, matches, regex_)) {
+        res.push_back(matches.length());
+        res.push_back(matches.position());
+        str = matches.suffix().str();
+    }
+
+    return res;
+}
+
+RegexSearchResult RegexSearch::FindInFile(std::string filename) {
+    std::smatch matches;
+    RegexSearchResult res;
+    std::string str;
+    std::ifstream file;
+
+    file.open(filename);
+    std::getline(file, str);
+    file.close();
+
+    while (std::regex_search(str, matches, regex_)) {
+        res.push_back(matches.length());
+        res.push_back(matches.position());
+        str = matches.suffix().str();
+    }
+
+    return res;
+}
