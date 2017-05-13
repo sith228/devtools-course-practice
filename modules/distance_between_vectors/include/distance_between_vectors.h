6ede0caec83d7c04e68063e249eab70457762dd0
GorozhaninMihail <gorozhaninmihail@yandex.ru>
2017-05-13 23:49:29 +0300

    Added something3 for lab2

--- modules/distance_between_vectors/include/distance_between_vectors.h
+++ modules/distance_between_vectors/include/distance_between_vectors.h
@@ -1,18 +1,15 @@
 // Copyright 2017 Gorozhanin Mihail
-#ifndef GOROZHANIN_MIHAIL_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
-#define GOROZHANIN_MIHAIL_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
+#ifndef MODULES_DISTANCE_BETWEEN_VECTORS_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
+#define MODULES_DISTANCE_BETWEEN_VECTORS_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
 
-#include <cmath>
-#include <iostream> 
-#include <vector>
-
-using namespace std;
+#include <math.h>
+#include <vector.h>
 
 enum TypeMetric { L1 = 1, L2, L3, L4, LInf };
 
 // Metric or the distance measure is non-negative, symmetric.
-// The Minkowski metric (L3, L4)
-// The Euclidean metric (L2). 
+// The Minkowski metric (L3, L4).
+// The Euclidean metric (L2).
 // Manhattan distance (L1).
 // The metric Chebyshev (LInf).
 
@@ -21,9 +18,9 @@ TypeMetric Type_Metric;
 int Size_Vector;
 
 class DistanceBetweenVectors {
-public:
-  static float CalculateDistance(float *vector1, 
-  float *vector2, TypeMetric TypeM, int SizeV);
+ public:
+   static float CalculateDistance(float *vector1,
+   float *vector2, TypeMetric TypeM, int SizeV);
 };
 
-#endif GOROZHANIN_MIHAIL_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
+// #endif MODULES_DISTANCE_BETWEEN_VECTORS_INCLUDE_DISTANCE_BETWEEN_VECTORS_H_
