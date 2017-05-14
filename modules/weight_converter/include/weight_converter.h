2bc6085aaeb3ab8308bfe25d3883f0806378e8bd
Wropwtr <buldakov.95@mail.ru>
2017-05-14 17:14:18 +0300

    Fix codestyle

--- modules/weight_converter/include/weight_converter.h
+++ modules/weight_converter/include/weight_converter.h
@@ -1,14 +1,15 @@
-#pragma once
+// Copyright 2017 Buldakov Vladislav
 
-using namespace std;
+#ifndef MODULES_WEIGHT_CONVERTER_INCLUDE_WEIGHT_CONVERTER_H_
+#define MODULES_WEIGHT_CONVERTER_INCLUDE_WEIGHT_CONVERTER_H_
 
 enum Weights{
     KG,    // kilogram
     MG,    // milligram
-    G,     // gram 
+    G,     // gram
     GR,    // grain
     DR,    // dram
-    OZ,    // ounce 
+    OZ,    // ounce
     LB,    // pound
     CR,    // carat
 
@@ -16,14 +17,16 @@ enum Weights{
 };
 
 class WeightConverter{
-public:
+ public:
     WeightConverter(double weight = 0.0, Weights type = KG);
     void setWeight(double weight, Weights type);
     double getWeight();
 
     double Convert(Weights to);
     double Convert(Weights from, Weights to, double val);
-private:
-    double Weight; // kg   
+ private:
+    double Weight;    // kg
     static const double ratio[END];
 };
+
+#endif  // MODULES_WEIGHT_CONVERTER_INCLUDE_WEIGHT_CONVERTER_H_
