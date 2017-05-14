68d8aca9f503ca2f9b6d59fc1eb9c5364a3e11cb
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-05-14 15:43:28 +0300

    Fixed miss spaces8.0

--- modules/elasticity/src/Demand.cxx
+++ modules/elasticity/src/Demand.cxx
@@ -15,15 +15,16 @@ Demand::Demand(double _oldprice, double _newprice,
     coeffofdemand = deltademand/deltaprice;
     revenue = 0;
     elasticity = 0;
-  } else
+  } else {
       throw "Error";
+    }
 }
 
 int Demand::checkforelasticity() {
   if (coeffofdemand == 1.0) {
     // Coefficient of unit elasticity
     elasticity = UnitElastic;
-  } else if(coeffofdemand > 1.0) {
+  } else if (coeffofdemand > 1.0) {
       // Demand is elastic
       elasticity = Elastic;
     } else {
@@ -33,10 +34,10 @@ int Demand::checkforelasticity() {
   return elasticity;
 }
 
-double Demand::revenuechange(double revenue) { 
+double Demand::revenuechange(double revenue) {
   // Old revenue
-  if (coeffofdemand >= 1.0) { 
-    for (int time = 0;time < 30; time++) { 
+  if (coeffofdemand >= 1.0) {
+    for (int time = 0; time < 30; time++) {
     // 30 days of work
       if (newprice > oldprice) {
         revenue--;
@@ -47,8 +48,8 @@ double Demand::revenuechange(double revenue) {
           }
     }
   }
-  if (coeffofdemand < 1.0) { 
-    for (int time=0;time < 30;time++) { 
+  if (coeffofdemand < 1.0) {
+    for (int time=0; time < 30; time++) {
       if (newprice > oldprice) {
         revenue++;
       } else if (newprice < oldprice) {
