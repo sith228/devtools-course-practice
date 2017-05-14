2c5c1fe5d2d3be7c79d6b0cebf922e19e28fb8b2
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-05-14 15:34:50 +0300

    Fixed miss spaces7.0

--- modules/elasticity/src/Demand.cxx
+++ modules/elasticity/src/Demand.cxx
@@ -4,7 +4,7 @@
 
 Demand::Demand(double _oldprice, double _newprice,
                double _olddemand, double _newdemand) {
-  if((_oldprice >= 0) && (_newprice >= 0) && 
+  if ((_oldprice >= 0) && (_newprice >= 0) &&
      (_olddemand >= 0) && (_newdemand >= 0)) {
     oldprice = _oldprice;
     olddemand = _olddemand;
@@ -15,50 +15,49 @@ Demand::Demand(double _oldprice, double _newprice,
     coeffofdemand = deltademand/deltaprice;
     revenue = 0;
     elasticity = 0;
-  }
-  else
-    throw "Error";
+  } else
+      throw "Error";
 }
 
 int Demand::checkforelasticity() {
-  if(coeffofdemand == 1.0) {
-    //Coefficient of unit elasticity
+  if (coeffofdemand == 1.0) {
+    // Coefficient of unit elasticity
     elasticity = UnitElastic;
-  }
-  else if(coeffofdemand > 1.0) {
-    //Demand is elastic
-    elasticity = Elastic;
-  }
-  else {
-    //Demand is not elastic
-    elasticity = NoElastic;
-  }
+  } else if(coeffofdemand > 1.0) {
+      // Demand is elastic
+      elasticity = Elastic;
+    } else {
+        // Demand is not elastic
+        elasticity = NoElastic;
+      }
   return elasticity;
 }
 
 double Demand::revenuechange(double revenue) { 
-  //Old revenue
-  if(coeffofdemand >= 1.0) { 
+  // Old revenue
+  if (coeffofdemand >= 1.0) { 
     for (int time = 0;time < 30; time++) { 
-    //30 days of work
-      if(newprice > oldprice)
+    // 30 days of work
+      if (newprice > oldprice) {
         revenue--;
-      else if(newprice < oldprice)
-        revenue++;
-      else
-        return 0;
+      } else if (newprice < oldprice) {
+          revenue++;
+        } else {
+            return 0;
+          }
     }
   }
-  if(coeffofdemand < 1.0) { 
-    for(int time=0;time < 30;time++) { 
-      if (newprice > oldprice)
+  if (coeffofdemand < 1.0) { 
+    for (int time=0;time < 30;time++) { 
+      if (newprice > oldprice) {
         revenue++;
-      else if (newprice < oldprice)
-        revenue--;
-      else
-        return 0;
+      } else if (newprice < oldprice) {
+          revenue--;
+        } else {
+            return 0;
+          }
     }
   }
-  //New revenue
+  // New revenue
   return revenue;
 }
