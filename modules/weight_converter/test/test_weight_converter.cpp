5ccba19daf752cae813a0fa262820a78cb0074ef
Wropwtr <buldakov.95@mail.ru>
2017-05-14 14:06:17 +0300

    Initial commit

---
+++ modules/weight_converter/test/test_weight_converter.cpp
@@ -0,0 +1,13 @@
+// Copyright 2017 Buldakov Vladislav
+
+#include <gtest/gtest.h>
+
+#include "include/weight_converter.h"
+
+TEST(WeightConverterTest, TestName) {
+    // Arrange
+
+    // Act
+
+    // Assert
+}
