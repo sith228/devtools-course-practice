2459504bbe0919f539982bdbd4bcdd56512b91c5
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-05-14 15:11:49 +0300

    Fixed miss spaces5.0

--- modules/elasticity/src/Demand.cxx
+++ modules/elasticity/src/Demand.cxx
@@ -2,61 +2,61 @@
 
 #include "../include/Demand.h"
 
-Demand:: Demand(double _oldprice, double _newprice, double _olddemand, double _newdemand) {
-   if((_oldprice>=0)&&(_newprice>=0)&&(_olddemand>=0)&&(_newdemand>=0)) {
-      oldprice=_oldprice;
-      olddemand=_olddemand;
-      newprice=_newprice;
-      newdemand=_newdemand;
-      deltaprice = newprice - oldprice;
-      deltademand = newdemand - olddemand;
-      coeffofdemand = deltademand/deltaprice;
-      revenue = 0;
-      elasticity = 0;
-    }
-    else
-      throw "Error";
+Demand::Demand(double _oldprice, double _newprice, double _olddemand, double _newdemand) {
+  if((_oldprice>=0)&&(_newprice>=0)&&(_olddemand>=0)&&(_newdemand>=0)) {
+    oldprice=_oldprice;
+    olddemand=_olddemand;
+    newprice=_newprice;
+    newdemand=_newdemand;
+    deltaprice = newprice - oldprice;
+    deltademand = newdemand - olddemand;
+    coeffofdemand = deltademand/deltaprice;
+    revenue = 0;
+    elasticity = 0;
+  }
+  else
+    throw "Error";
 }
 
-int Demand:: checkforelasticity() {
-    if(coeffofdemand==1.0) {
-        //Coefficient of unit elasticity
-        elasticity = UnitElastic;
-     }
-    else if(coeffofdemand>1.0) {
-          //Demand is elastic
-          elasticity = Elastic;
-     }
-    else {
-          //Demand is not elastic
-          elasticity = NoElastic;
-    }
-    return elasticity;
+int Demand::checkforelasticity() {
+  if(coeffofdemand==1.0) {
+    //Coefficient of unit elasticity
+    elasticity = UnitElastic;
+  }
+  else if(coeffofdemand>1.0) {
+    //Demand is elastic
+    elasticity = Elastic;
+  }
+  else {
+    //Demand is not elastic
+    elasticity = NoElastic;
+  }
+  return elasticity;
 }
 
-double Demand:: revenuechange(double revenue) { 
-    //Old revenue
-    if(coeffofdemand>=1.0) { 
-        for (int time = 0;time < 30; time++) { 
-        //30 days of work
-            if(newprice>oldprice)
-               revenue--;
-            else if(newprice<oldprice)
-               revenue++;
-            else
-              return 0;
-        }
+double Demand::revenuechange(double revenue) { 
+  //Old revenue
+  if(coeffofdemand>=1.0) { 
+    for (int time = 0;time < 30; time++) { 
+    //30 days of work
+      if(newprice>oldprice)
+        revenue--;
+      else if(newprice<oldprice)
+        revenue++;
+      else
+        return 0;
     }
-    if(coeffofdemand<1.0) { 
-        for(int time=0;time<30;time++) { 
-            if (newprice>oldprice)
-                revenue++;
-            else if (newprice<oldprice)
-                revenue--;
-            else
-              return 0;
-        }
+  }
+  if(coeffofdemand<1.0) { 
+    for(int time=0;time<30;time++) { 
+      if (newprice>oldprice)
+        revenue++;
+      else if (newprice<oldprice)
+        revenue--;
+      else
+        return 0;
     }
-    //New revenue
-    return revenue;
+  }
+  //New revenue
+  return revenue;
 }
