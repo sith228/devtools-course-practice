b96ae47a3073b19dbb3849f86f9c75e8bb2cc306
GorozhaninMihail <gorozhaninmihail@yandex.ru>
2017-05-16 00:35:50 +0300

    Added something4.1 for lab2

--- modules/distance_between_vectors/src/distance_between_vectors.cpp
+++ modules/distance_between_vectors/src/distance_between_vectors.cpp
@@ -3,31 +3,29 @@
 
 float DistanceBetweenVectors::CalculateDistance(std::vector<float> vector1,
 std::vector<float> vector2, Metric::TypeMetric TypeM) {
-	float distance;
-	int size;
-	if (vector1.size() == vector2.size()) {
-		std::vector<float>vect1 = vector1;
-		std::vector<float>vect2 = vector2;
-		size = vector1.size();
-		if (TypeM == Metric::LInf) {
-			float max = 0;
-			for (int i = 0; i < size; i++)
-				if (max < fabs(vect1[i] - vect2[i]))
-					max = static_cast<float>(fabs(vect1[i] - vect2[i]));
-			distance = max;
-		}
-		else {
-			float Sum_Of_Components = 0;
-			for (int i = 0; i < size; i++)
-				Sum_Of_Components += powf(static_cast<float>
-				(fabs(vect1[i] - vect2[i])),
-					1.0f * static_cast<float>(TypeM));
-			distance = powf(Sum_Of_Components,
-				1.0f / static_cast<float>(TypeM));
-		}
-		return distance;
-	}
-	else {
-		return 0;
-	}
+  float distance;
+  int size;
+  if (vector1.size() == vector2.size()) {
+	  std::vector<float>vect1 = vector1;
+	  std::vector<float>vect2 = vector2;
+	  size = vector1.size();
+	  if (TypeM == Metric::LInf) {
+		  float max = 0;
+		  for (int i = 0; i < size; i++)
+			  if (max < fabs(vect1[i] - vect2[i]))
+				  max = static_cast<float>(fabs(vect1[i] - vect2[i]));
+		  distance = max;
+	  } else {
+		  float Sum_Of_Components = 0;
+		  for (int i = 0; i < size; i++)
+			  Sum_Of_Components += powf(static_cast<float>
+			  (fabs(vect1[i] - vect2[i])),
+				  1.0f * static_cast<float>(TypeM));
+		  distance = powf(Sum_Of_Components,
+      1.0f / static_cast<float>(TypeM));
+	  }
+	  return distance;
+  } else {
+	  return 0;
+  }  
 }
