70fc12378aa335d3142457479b20679e19576522
Nikita Grechuhin <NikitaGrechukhin@gmail.com>
2017-05-15 22:00:50 +0300

    fix

--- modules/integration-methods/test/test_function_parser.cpp
+++ modules/integration-methods/test/test_function_parser.cpp
@@ -58,7 +58,7 @@ TEST(PARSER, throw_exception_when_input_empty) {
 }
 
 TEST(PARSER, throw_exception_when_input_invalid) {
-    std::string func = "10*(1115+3";
+    std::string func = "10*(115+3";
 
     Parser parser;
     ASSERT_ANY_THROW(parser.Parse(func.c_str()).eval());
