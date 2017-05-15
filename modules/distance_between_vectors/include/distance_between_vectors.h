de9082a5cb61ae43ac6c20ce2d45c1d3bcc7bf18
GorozhaninMihail <gorozhaninmihail@yandex.ru>
2017-05-16 00:17:14 +0300

    Added something4 for lab2

--- modules/distance_between_vectors/include/distance_between_vectors.h
+++ modules/distance_between_vectors/include/distance_between_vectors.h
@@ -6,21 +6,19 @@
 #include <vector>
 #include <iostream>
 
-
-
 namespace Metric {
-	enum TypeMetric { L1 = 1, L2, L3, L4, LInf };
+  enum TypeMetric { L1 = 1, L2, L3, L4, LInf };
 }
-	// Metric or the distance measure is non-negative, symmetric.
-	// The Minkowski metric (L3, L4).
-	// The Euclidean metric (L2).
-	// Manhattan distance (L1).
-	// The metric Chebyshev (LInf).
+  // Metric or the distance measure is non-negative, symmetric.
+  // The Minkowski metric (L3, L4).
+  // The Euclidean metric (L2).
+  // Manhattan distance (L1).
+  // The metric Chebyshev (LInf).
 
 
 class DistanceBetweenVectors {
-public:
-	static float CalculateDistance(std::vector<float> vector1,
+ public:
+  static float CalculateDistance(std::vector<float> vector1,
   std::vector<float> vector2, Metric::TypeMetric TypeM);
 };
 
