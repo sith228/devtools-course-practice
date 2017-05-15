649f55622a6ed39e36216f20631ffa1ec74b1d6d
Wropwtr <buldakov.95@mail.ru>
2017-05-15 22:00:13 +0300

    Fix line lenght

--- modules/weight_converter/src/weight_converter.cpp
+++ modules/weight_converter/src/weight_converter.cpp
@@ -2,7 +2,8 @@
 
 #include "include/weight_converter.h"
 
-const std::map<Weights, double> WeightsMap::kWeights_ratio = WeightsMap::CreateMap();
+const std::map<Weights, double> WeightsMap::kWeights_ratio = 
+    WeightsMap::CreateMap();
 
 WeightConverter::WeightConverter(double weight, Weights type) {
     SetWeight(weight, type);
@@ -21,5 +22,6 @@ double WeightConverter::Convert(Weights to) {
 }
 
 double WeightConverter::Convert(double weight, Weights from, Weights to) {
-    return WeightsMap::kWeights_ratio.at(from) / WeightsMap::kWeights_ratio.at(to) * weight;
+    return WeightsMap::kWeights_ratio.at(from) / 
+        WeightsMap::kWeights_ratio.at(to) * weight;
 }
