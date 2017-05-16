5cd01af8e465ff908cb4e18fe8900ac2cd892c36
GorozhaninMihail <gorozhaninmihail@yandex.ru>
2017-05-16 17:50:37 +0300

    Fix codestyle №2

--- modules/distance_between_vectors/test/test_distance_between_vectors.cpp
+++ modules/distance_between_vectors/test/test_distance_between_vectors.cpp
@@ -16,14 +16,13 @@ TEST(DistanceBetweenVectors, Different_Size_Vectors) {
     V2[i] = static_cast<float>(i+2);
   }
 
-  EXPECT_ANY_THROW(DistanceBetweenVectors::CalculateDistance(V1, V2, Type););
+  EXPECT_ANY_THROW(DistanceBetweenVectors::CalculateDistance(V1, V2, Type));
 }
 
 TEST(DistanceBetweenVectors, Different_Size_Vectors_With_Different_Metric) {
   std::vector<float> V1(4);
   std::vector<float> V2(5);
-  Metric::TypeMetric Type1 = Metric::L2;
-  Metric::TypeMetric Type2 = Metric::LInf;
+  Metric::TypeMetric Type = Metric::LInf;
 
   for (int i = 0; i < 4; i++) {
     V1[i] = static_cast<float>(i*(0.3+0.01*i));
@@ -31,10 +30,9 @@ TEST(DistanceBetweenVectors, Different_Size_Vectors_With_Different_Metric) {
   for (int i = 0; i < 5; i++) {
     V2[i] = static_cast<float>(i+2);
   }
-  float result1 = DistanceBetweenVectors::CalculateDistance(V1, V2, Type1);
-  float result2 = DistanceBetweenVectors::CalculateDistance(V1, V2, Type2);
+  
+  EXPECT_ANY_THROW(DistanceBetweenVectors::CalculateDistance(V1, V2, Type));
 
-  EXPECT_FLOAT_EQ(result1, result2);
 }
 
 TEST(DistanceBetweenVectors, Equal_Size_Vectors_Final) {
