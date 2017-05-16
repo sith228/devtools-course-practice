ed6a09af71cfd1ce70659b8c2ba8a37570080e48
Marat_Israfilov <Grendablez@gmail.com>
2017-05-16 22:15:49 +0300

    Add realization without determinant

--- modules/matrix-calculator/include/matrix_calculator.h
+++ modules/matrix-calculator/include/matrix_calculator.h
@@ -1,25 +1,39 @@
+// Copyright 2017 Israfilov Marat
+
 #pragma once
 #ifndef MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
 #define MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
 
+#include <vector>
+#define INVALID_ARGS -1
+#define INVALID_SIZES -2
+#define INVALID_MATRIX -3
+#define DIV_BY_ZERO -4
+
 class MatrixCalculator {
+	
+public:
+
+	MatrixCalculator(int, int);
+	MatrixCalculator(const MatrixCalculator&);
+	~MatrixCalculator();
+
+	MatrixCalculator& operator =(const MatrixCalculator&);
+	MatrixCalculator operator +(const MatrixCalculator&) const;
+	MatrixCalculator operator -(const MatrixCalculator&) const;
+	MatrixCalculator operator *(const MatrixCalculator&) const;
+
+	double Determinant() const;
+	void SetMatrix(std::vector<std::vector<double>> &);
+
+protected:
+
+	bool operator ==(const MatrixCalculator&) const;
+	bool size_comp(const MatrixCalculator&) const;
+
 private:
-    double **matrix;
-    int rows, columns;
 
-public:    
-    MatrixCalculator(int _rows = 0, int _columns = 0);
-    ~MatrixCalculator();
-
-    MatrixCalculator& operator =(const MatrixCalculator&);
-    MatrixCalculator operator +(const MatrixCalculator&) const;
-    MatrixCalculator operator -(const MatrixCalculator&) const;
-    MatrixCalculator operator *(const MatrixCalculator&) const;
-
-    double determinant() const;
-    void set_array(double, double);
-
-    bool operator ==(const Matrix&) const;
+	std::vector<std::vector<double > > matrix_;
 };
 
 #endif // MODULES_MATRIX_CALCULATOR_INCLUDE_MATRIX_CALCULATOR_H_
---
+++ modules/matrix-calculator/src/matrix_calculator.cpp
@@ -0,0 +1,114 @@
+#include "matrix.h"
+#include <math.h>
+#include <iostream>
+
+bool MatrixCalculator::operator ==(const MatrixCalculator& p) const {
+    return this->matrix_ == p.matrix_;
+}
+
+bool MatrixCalculator::size_comp(const MatrixCalculator& p) const {
+    bool result = false;
+
+    if (this->matrix_.size() == p.matrix_.size()) {
+        result = true;
+
+        for (int i = 0; matrix_.size(); ++i) {
+            if (this->matrix_[i].size() != p.matrix_[i].size()) {
+                result = false;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+MatrixCalculator::MatrixCalculator(int rows = 0, int columns = 0) {
+    if (rows < 0 || columns < 0) {
+        throw INVALID_ARGS;
+    }
+
+    matrix_ = std::vector<std::vector<double > >(rows, std::vector<double>(columns, 0.0));
+}
+
+MatrixCalculator::MatrixCalculator(const MatrixCalculator& p) {
+    matrix_ = p.matrix_;
+}
+
+MatrixCalculator::~MatrixCalculator() {
+    matrix_.clear();
+}
+
+MatrixCalculator& MatrixCalculator::operator =(const MatrixCalculator& p) {
+    if (!(*this == p)) {        
+        matrix_ = p.matrix_;
+    }
+    
+    return *this;
+}
+
+MatrixCalculator MatrixCalculator::operator +(const MatrixCalculator& p) const {
+    MatrixCalculator result(*this);
+
+    if (!size_comp(p)) {
+        throw INVALID_SIZES;
+    }
+    
+    for (int i = 0; i < this->matrix_.size(); ++i) {
+        for (int j = 0; j < this->matrix_[i].size(); ++j) {
+            result.matrix_[i][j] = this->matrix_[i][j] + p.matrix_[i][j];
+        }
+    }
+
+    return result;
+}
+
+MatrixCalculator MatrixCalculator::operator -(const MatrixCalculator& p) const {
+    MatrixCalculator result(*this);
+
+    if (!size_comp(p)) {
+        throw INVALID_SIZES;
+    }
+
+    for (int i = 0; i < this->matrix_.size(); ++i) {
+        for (int j = 0; j < this->matrix_[i].size(); ++j) {
+            result.matrix_[i][j] = this->matrix_[i][j] - p.matrix_[i][j];
+        }
+    }
+
+    return result;
+}
+
+MatrixCalculator MatrixCalculator::operator *(const MatrixCalculator& p) const {
+    if (this->matrix_[0].size() != p.matrix_.size()) {
+        throw INVALID_SIZES;
+    }
+    
+    MatrixCalculator result(this->matrix_.size(), p.matrix_[0].size());
+    
+    for (int i = 0; i < result.matrix_.size(); ++i) {
+        for (int j = 0; j < result.matrix_[i].size(); ++j) {
+            for(int k = 0; k < p.matrix_.size(); ++k) {
+                result.matrix_[i][j] += this->matrix_[i][k] * p.matrix_[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+double MatrixCalculator::Determinant() const {    
+    return 0.0;
+}
+
+void MatrixCalculator::SetMatrix(std::vector<std::vector<double>> &input_matrix) {
+    for (int i = 1; i < input_matrix.size(); ++i) {
+        if (input_matrix[i - 1].size() != input_matrix[i].size()) {
+            throw INVALID_MATRIX;
+        }
+    }
+
+    if (this->matrix_ != input_matrix) {
+        matrix_ = input_matrix;    
+    }
+}
