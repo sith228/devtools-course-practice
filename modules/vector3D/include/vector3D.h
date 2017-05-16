f2c31492cff32ec15f23a71c8b154763c8706aab
katrinezor <katrinezor@yandex.ru>
2017-05-16 20:22:07 +0300

    Style fixes

--- modules/vector3D/include/vector3D.h
+++ modules/vector3D/include/vector3D.h
@@ -9,34 +9,35 @@
 
 // Vector in 3-dimensional space
 class Vector3D {
- protected:
-    double x;
-    double y;
-    double z;
 
- public:
-    explicit Vector3D(double _x = 0.0, double _y = 0.0, double _z = 0.0);
-    Vector3D(const Vector3D &v);                // copy constructor
-    double GetX() const { return x; }            // access
-    double GetY() const { return y; }
-    double GetZ() const { return z; }
+public:
+	explicit Vector3D(double _x = 0.0, double _y = 0.0, double _z = 0.0);
+	Vector3D(const Vector3D &v);                // copy constructor
+	double GetX() const { return x_; }            // access
+	double GetY() const { return y_; }
+	double GetZ() const { return z_; }
 
-    void SetX(double _x) { x = _x; }
-    void SetY(double _y) { y = _y; }
-    void SetZ(double _z) { z = _z; }
+	void SetX(double _x) { x_ = _x; }
+	void SetY(double _y) { y_ = _y; }
+	void SetZ(double _z) { z_ = _z; }
 
-    bool operator==(const Vector3D &v) const;  // comparison
-    bool operator!=(const Vector3D &v) const;  // comparison
+	bool operator==(const Vector3D &v) const;  // comparison
+	bool operator!=(const Vector3D &v) const;  // comparison
 
-    Vector3D& operator=(const Vector3D &v);     // assigment
+	Vector3D& operator=(const Vector3D &v);     // assigment
 
-                                              // vector operations
-    Vector3D  operator+(const Vector3D &v) const;     // addition
-    Vector3D  operator-(const Vector3D &v) const;     // subtraction
-    double Norm();                        // vector norm
-    Vector3D Normalize();                // normalization vector
-    double  operator*(const Vector3D &v) const;     // scalar product
-    Vector3D  operator^(const Vector3D &v) const;    // vector product v1 ^ v2
+												                                                          // vector operations
+	Vector3D  operator+(const Vector3D &v) const;     // addition
+	Vector3D  operator-(const Vector3D &v) const;     // subtraction
+	double Norm();                        // vector norm
+	Vector3D Normalize();                // normalization vector
+	double  operator*(const Vector3D &v) const;     // scalar product
+	Vector3D  operator^(const Vector3D &v) const;    // vector product v1 ^ v2
+
+protected:
+	double x_;
+	double y_;
+	double z_;
 };
 
 
--- modules/vector3D/src/vector3D.cpp
+++ modules/vector3D/src/vector3D.cpp
@@ -4,82 +4,82 @@
 
 
 Vector3D::Vector3D(double _x, double _y, double _z) {
-    x = _x;
-    y = _y;
-    z = _z;
+	x_ = _x;
+	y_ = _y;
+	z_ = _z;
 }
 
 Vector3D::Vector3D(const Vector3D &v) {
-    x = v.x;
-    y = v.y;
-    z = v.z;
+	x_ = v.x_;
+	y_ = v.y_;
+	z_ = v.z_;
 }
 
 bool Vector3D::operator==(const Vector3D &v) const {
-    if (x == v.x && y == v.y && z == v.z)
-        return true;
-    else
-        return false;
+	if (x_ == v.x_ && y_ == v.y_ && z_ == v.z_)
+		return true;
+	else
+		return false;
 }
 
 bool Vector3D::operator!=(const Vector3D &v) const {
-    return !(*this == v);
+	return !(*this == v);
 }
 
 Vector3D& Vector3D::operator=(const Vector3D &v) {
-    if (*this != v) {
-        x = v.x;
-        y = v.y;
-        z = v.z;
-    }
-    return *this;
+	if (*this != v) {
+		x_ = v.x_;
+		y_ = v.y_;
+		z_ = v.z_;
+	}
+	return *this;
 }
 
 Vector3D Vector3D::operator+(const Vector3D &v) const {
-    Vector3D temp;
-    temp.x = x + v.x;
-    temp.y = y + v.y;
-    temp.z = z + v.z;
+	Vector3D temp;
+	temp.x_ = x_ + v.x_;
+	temp.y_ = y_ + v.y_;
+	temp.z_ = z_ + v.z_;
 
-    return temp;
+	return temp;
 }
 
 Vector3D Vector3D::operator-(const Vector3D &v) const {
-    Vector3D temp;
-    temp.x = x - v.x;
-    temp.y = y - v.y;
-    temp.z = z - v.z;
+	Vector3D temp;
+	temp.x_ = x_ - v.x_;
+	temp.y_ = y_ - v.y_;
+	temp.z_ = z_ - v.z_;
 
-    return temp;
+	return temp;
 }
 
 double Vector3D::Norm() {
-    return sqrt(x*x + y*y + z*z);
+	return sqrt(x_*x_ + y_*y_ + z_*z_);
 }
 
 Vector3D Vector3D::Normalize() {
-    double norm = Norm();
-    if (norm != 0.0) {
-        x = x / norm;
-        y = y / norm;
-        z = z / norm;
-    }
+	double norm = Norm();
+	if (norm != 0.0) {
+		x_ = x_ / norm;
+		y_ = y_ / norm;
+		z_ = z_ / norm;
+	}
 
-    return *this;
+	return *this;
 }
 
 double Vector3D::operator*(const Vector3D & v) const {
-    return x*v.x + y*v.y + z*v.z;
+	return x_*v.x_ + y_*v.y_ + z_*v.z_;
 }
 
 Vector3D Vector3D::operator^(const Vector3D & v) const {
-    Vector3D vt(*this), temp;
-    if (vt.x != 0 || vt.y != 0 || vt.z != 0) {
-        if (v.x != 0 || v.y != 0 || v.z != 0) {
-            temp.x = vt.y*v.z - vt.z*v.y;
-            temp.y = vt.z*v.x - vt.x*v.z;
-            temp.z = vt.x*v.y - vt.y*v.x;
-        }
-    }
-    return temp;
+	Vector3D vt(*this), temp;
+	if (vt.x_ != 0 || vt.y_ != 0 || vt.z_ != 0) {
+		if (v.x_ != 0 || v.y_ != 0 || v.z_ != 0) {
+			temp.x_ = vt.y_*v.z_ - vt.z_*v.y_;
+			temp.y_ = vt.z_*v.x_ - vt.x_*v.z_;
+			temp.z_ = vt.x_*v.y_ - vt.y_*v.x_;
+		}
+	}
+	return temp;
 }
