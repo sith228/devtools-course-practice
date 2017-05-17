5aa87a3f95b7f5d913840f82818bd9af0eda3e69
Marat_Israfilov <Grendablez@gmail.com>
2017-05-17 22:18:28 +0300

    100% codecov

--- modules/matrix-calculator/test/test_matrix_calculator.cpp
+++ modules/matrix-calculator/test/test_matrix_calculator.cpp
@@ -121,6 +121,30 @@ TEST(MatrixCalculatorTest, determinant_is_calculated_correctry) {
     EXPECT_EQ(m1.Determinant(), expected);
 }
 
+TEST(MatrixCalculatorTest, first_elem_is_zero) {
+    std::vector<std::vector<double>> v1 = {
+        { 0.0, 2.0, 5.0 },
+        { 3.0, 2.0, 5.0 },
+        { 5.0, 3.0, 1.0 } };    
+
+    MatrixCalculator m1;
+    m1.SetMat(v1);
+
+    EXPECT_ANY_THROW(m1.Determinant());
+}
+
+TEST(MatrixCalculatorTest, cant_calc_determinant_in_rect_matrix) {
+    std::vector<std::vector<double>> v1 = {
+        { 1.0, 2.0, 5.0, 3.0 },
+        { 3.0, 2.0, 5.0, 2.0 },
+        { 5.0, 3.0, 1.0, 4.0 } };    
+
+    MatrixCalculator m1;
+    m1.SetMat(v1);
+
+    EXPECT_ANY_THROW(m1.Determinant());
+}
+
 TEST(MatrixCalculatorTest, size_comp_is_correctly) {
     std::vector<std::vector<double>> v1(3, std::vector<double>(3, 2));
     std::vector<std::vector<double>> v2(4, std::vector<double>(3, 2));
