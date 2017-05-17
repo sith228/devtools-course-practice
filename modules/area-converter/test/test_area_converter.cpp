5ae9c0c027792c8e7fda75bc0440bd05b3995255
Kuchkov Ivan <iheap@ya.ru>
2017-05-18 02:55:33 +0300

    fix line_length

--- modules/area-converter/test/test_area_converter.cpp
+++ modules/area-converter/test/test_area_converter.cpp
@@ -8,7 +8,8 @@
 
 #include "include/area_converter.h"
 
-static const double kNeighbourhood = std::numeric_limits<double>::epsilon() * 100000;
+static const double kNeighbourhood = 
+    std::numeric_limits<double>::epsilon() * 100000;
 
 TEST(AreaConverterTest, can_create_converter_unit) {
     EXPECT_NO_THROW(AreaConverter(AreaConverter::kCentimeter,
@@ -49,7 +50,8 @@ TEST(AreaConverterTest, is_negative_size_area_works_correct) {
     EXPECT_ANY_THROW(c(-2.1718));
 }
 
-TEST(AreaConverterTest, check_convert_multiple_times_with_different_arguments) {
+TEST(AreaConverterTest, 
+    check_convert_multiple_times_with_different_arguments) {
     AreaConverter c = AreaConverter(AreaConverter::kCentimeter,
         AreaConverter::kAcr);
     int size = 10;
