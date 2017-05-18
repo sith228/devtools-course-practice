4ac5895615c91885f062922a53e5c4bc13eed04d
Игорь Федоров <fedorovigor97@mail.ru>
2017-05-18 18:38:41 +0300

    Update newton-method.cpp
    change coding style

--- modules/root-finder/src/newton-method.cpp
+++ modules/root-finder/src/newton-method.cpp
@@ -7,46 +7,46 @@
 #include <cmath>
 
 bool newton_method::IsMonotone(float(*fun)(float)) {
-    if (A > B) {
-        std::swap(A, B);
+    if (A_ > B_) {
+        std::swap(A_, B_);
     }
-    if (fun(A) > fun(B)) {
-        float i = A;
+    if (fun(A_) > fun(B_)) {
+        float i = A_;
         float temp_i = i;
-        while (temp_i < B) {
+        while (temp_i < B_) {
             if (fun(i) < fun(temp_i))
                 return false;
             i = temp_i;
-            temp_i += Acc;
+            temp_i += Acc_;
             }
-    } else if (fun(A) < fun(B)) {
-        float i = A;
+    } else if (fun(A_) < fun(B_)) {
+        float i = A_;
         float temp_i = i;
-        while (temp_i < B) {
+        while (temp_i < B_) {
             if (fun(i) > fun(temp_i))
                 return false;
             i = temp_i;
-            temp_i += Acc;
+            temp_i += Acc_;
         }
     }
-    if (A == B) {
+    if (A_ == B_) {
         return false;
     }
-    if (fun(A) == fun(B)) {
+    if (fun(A_) == fun(B_)) {
         return false;
     }
     return true;
 }
 
-float newton_method::FindRoot(float(*fun)(float),
+float NewtonMethod::FindRoot(float(*fun)(float),
     float(*deriv_fun)(float), float(*deriv2_fun)(float)) {
     if (IsMonotone(fun)) {
-        float xn = fun(A)*deriv2_fun(A) > 0 ? A : B;
+        float xn = fun(A_)*deriv2_fun(A_) > 0 ? A_ : B_;
         float x1 = xn - fun(xn) / deriv_fun(xn);
         float x0 = xn;
         float xsum = std::abs(x0 - x1);
-        while (xsum > Acc) {
-            if (x1 < A || x1 > B)
+        while (xsum > Acc_) {
+            if (x1 < A_ || x1 > B_)
                 throw("segment have not desidion");
             x0 = x1;
             x1 = x1 - fun(x1) / deriv_fun(x1);
