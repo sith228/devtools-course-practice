0fff3c2d189aa240129877d17dd8bd0f978cf163
Andrew Kazakov <tosk-nn@yandex.ru>
2017-05-18 22:37:07 +0300

    RegexSearch: huge changes (implemented custom search engine)

--- modules/regex-search/include/regex_search.h
+++ modules/regex-search/include/regex_search.h
@@ -1,4 +1,4 @@
-﻿// Copyright 2017 Kazakov Andrew
+// Copyright 2017 Kazakov Andrew
 
 #ifndef MODULES_REGEX_SEARCH_INCLUDE_REGEX_SEARCH_H_
 #define MODULES_REGEX_SEARCH_INCLUDE_REGEX_SEARCH_H_
@@ -6,26 +6,65 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stack>
 #include <vector>
-#include <regex>
 
-typedef std::vector<unsigned int> RegexSearchResult;
+typedef std::vector<int> RegexSearchResult;
 
+// Basic regular expressions search.
+// - Supported quanifications: *, ?, +, {n} (1<=n<=9)
+// - Parentheses and quanitifications within another parenthesis are not allowed
+// - \n, \r support
+// - There can be empty regular expressions
+// - Search is implemented according to DFA principle
 class RegexSearch {
  public:
-    RegexSearch();
-    explicit RegexSearch(const std::string);
-    RegexSearch(const RegexSearch&);
-    RegexSearch& operator=(const RegexSearch&);
+  static const int kNotFound = -1;
+  static const int errorTooLongRegex = -1;
+  static const int errorRegExpIncorrect = -2;
+  static const int errorTooLongString = -3;
 
-    void SetRegex(const std::string);
-    std::string GetRegex(void) const;
+  explicit RegexSearch(const std::string = "");
+  RegexSearch(const RegexSearch&);
+  RegexSearch& operator=(const RegexSearch&);
+  ~RegexSearch(void) {}
 
-    RegexSearchResult Find(std::string str);
-    RegexSearchResult FindInFile(std::string filename);
+  void SetRegex(const std::string&);
+  std::string GetRegex(void) const;
 
- protected:
-    std::string regex_;
+  // Returns kNotFound if there are no matches
+  RegexSearchResult Find(const std::string&);
+
+ private:
+  // Token type. TOK_Q_CUSTOM is {n} quantifier
+  enum TokenType {
+    TOK_WORD,
+    TOK_Q_ASTERISK,
+    TOK_Q_QUESTION,
+    TOK_Q_PLUS,
+    TOK_Q_CUSTOM,
+    TOK_PAR_OPEN,
+    TOK_PAR_CLOSE,
+    _ABSENT
+  };
+
+  struct Lexeme {
+    std::string str;
+    int min, max;
+  };
+
+  static const int kMaxStringLength = 5000;
+  static const int kMaxRegexLength = 150;
+  static const int kMaxTokenSize = 3;
+  static const int kInfinityRepeats = -1;
+
+  std::vector<Lexeme> regex_;
+  std::string regex_str_;
+
+  RegexSearchResult FindFirst(const std::string&, const size_t pos = 0);
+
+  static void MakeStringLonger(std::string& out_str, const std::string& str);
+  static void Parse(const std::string&, std::vector<Lexeme>&);
 };
 
 #endif
--- modules/regex-search/src/regex_search.cpp
+++ modules/regex-search/src/regex_search.cpp
@@ -1,66 +1,304 @@
-﻿// Copyright 2017 Kazakov Andrew
+// Copyright 2017 Kazakov Andrew
 
 #include "include/regex_search.h"
 
-RegexSearch::RegexSearch() {
-    regex_ = std::string("");
-}
+//
+// Constructors/operator=
+//
 
 RegexSearch::RegexSearch(const std::string regex) {
-    regex_ = regex;
+  SetRegex(regex);
 }
 
 RegexSearch::RegexSearch(const RegexSearch& regex) {
-    regex_ = regex.regex_;
+  regex_ = regex.regex_;
+  regex_str_ = regex.regex_str_;
 }
 
 RegexSearch& RegexSearch::operator=(const RegexSearch& regex) {
+  if (&regex != this) {
     regex_ = regex.regex_;
+    regex_str_ = regex.regex_str_;
+  }
 
-    return *this;
+  return *this;
 }
 
-void RegexSearch::SetRegex(const std::string regex) {
-    regex_ = regex;
+//
+// Set & get
+//
+
+void RegexSearch::SetRegex(const std::string& regex) {
+  if (regex.length() > kMaxRegexLength)
+    throw RegexSearch::errorTooLongRegex;
+
+  std::vector<Lexeme> lexemes;
+
+  try {
+    RegexSearch::Parse(regex, lexemes);
+  } catch (const int e) {
+    throw RegexSearch::errorRegExpIncorrect;
+  }
+
+  regex_ = lexemes;
+  regex_str_ = regex;
 }
 
 std::string RegexSearch::GetRegex() const {
-    return regex_;
+  return regex_str_;
 }
 
-RegexSearchResult RegexSearch::Find(std::string str) {
-    RegexSearchResult result;
-    std::smatch matches;
-    std::regex regex(regex_);
+//
+// Find
+//
 
-    while (std::regex_search(str, matches, regex)) {
-        result.push_back(matches.length());
-        result.push_back(matches.position());
-        str = matches.suffix().str();
-    }
+RegexSearchResult RegexSearch::Find(const std::string& str) {
+  if (str.length() > kMaxStringLength)
+    throw RegexSearch::errorTooLongString;
 
-    return result;
+  RegexSearchResult res, temp(2);
+  size_t pos = 0;
+
+  // If regex is empty, the whole string matches
+  if (regex_.size() == 0) {
+    res.push_back(0);
+    res.push_back(str.size());
+    return res;
+  }
+
+  do {
+    temp = FindFirst(str, pos);
+    res.push_back(temp[0]);
+    res.push_back(temp[1]);
+    pos = temp[0] + temp[1];
+  } while (temp[0] != RegexSearch::kNotFound);
+
+  // At least there is one match, last 2 elements is useless
+  if (res.size() > 2) {
+    res.pop_back();
+    res.pop_back();
+  }
+
+  return res;
 }
 
-RegexSearchResult RegexSearch::FindInFile(std::string filename) {
-    RegexSearchResult result;
-    std::smatch matches;
-    std::regex regex(regex_);
-    int current_position = 0;
+//
+// Private methods
+//
 
-    std::string str, temp_str;
-    std::ifstream file;
-    file.open(filename);
-    while (std::getline(file, temp_str)) {
-        str += temp_str + "\n";
+RegexSearchResult RegexSearch::FindFirst(const std::string& str, const size_t pos) {
+  RegexSearchResult res(2);
+
+  Lexeme lexeme;
+  std::string l_str, temp;
+
+  // if true, we go to the next part of the regex
+  bool curr_lexeme_ended = false;
+
+  int match_start = RegexSearch::kNotFound, match_end = 0;
+
+  for (size_t i = pos, j = 0; i < str.size() && j < regex_.size(); ++i) {
+    lexeme = regex_[j];
+    l_str = lexeme.str;
+
+    // There are only 3 options: min=0 max=1, min=0 max=<infinity>, min=max=<const>
+    if (lexeme.min == 0) {
+      if (l_str.compare(str.substr(i, l_str.size())) == 0) {
+        if (match_start == RegexSearch::kNotFound)
+          match_start = i;
+        match_end = (i += (l_str.size()));
+
+        if (lexeme.max = RegexSearch::kInfinityRepeats)
+          while (l_str.compare(str.substr(i, l_str.size())) == 0)
+            match_end = (i += (l_str.size()));
+
+        --i;
+        curr_lexeme_ended = true;
+      }
+    } else {
+      temp = "";
+      for (int j = 0; j < lexeme.min; ++j)
+        temp += l_str;
+
+      if (temp.compare(str.substr(i, temp.size())) == 0) {
+        if (match_start == RegexSearch::kNotFound)
+          match_start = i;
+        match_end = (i += (temp.size()));
+        --i;
+        curr_lexeme_ended = true;
+      } else if (match_start != RegexSearch::kNotFound) {
+        match_start = RegexSearch::kNotFound;
+      }
     }
-    file.close();
 
-    while (std::regex_search(str, matches, regex)) {
-        result.push_back(matches.length());
-        result.push_back(current_position += matches.position());
-        str = matches.suffix().str();
+    if (curr_lexeme_ended) {
+      curr_lexeme_ended = false;
+      j++;
     }
 
-    return result;
+  }  // for
+
+  res[0] = match_start;
+  res[1] = match_end - match_start;
+
+  return res;
+}
+
+//
+// Static methods
+//
+
+void RegexSearch::MakeStringLonger(std::string& out_str, const std::string& str) {
+  out_str = str;
+  for (int i = 0; i < (kMaxTokenSize - 1); ++i) {
+    out_str.push_back(0);
+  }
+}
+
+void RegexSearch::Parse(const std::string& regex, std::vector<Lexeme>& lexemes) {
+  char char_next, temp;
+  size_t offset = 0;  // how many chars to ignore after the iteration
+  bool par_open = false;
+
+  // For quantifiers
+  TokenType curr_tok = TokenType::_ABSENT, prev_tok = TokenType::_ABSENT;
+  int q_min_repeats, q_max_repeats;
+
+  // Regex substrings
+  std::string literals_block;  // сurrent continuous sequence of literals
+  size_t lb_size = 0;  // literals_block current size
+
+  // For looking ahead
+  std::string regex_long;
+  MakeStringLonger(regex_long, regex);
+
+  for (auto i = 0; i < regex.size(); offset = 0, ++i) {
+    char_next = regex_long[i + 1];
+    prev_tok = curr_tok;
+
+    switch (regex[i]) {
+      case '\\':
+        if (char_next == 0) continue;
+        curr_tok = TokenType::TOK_WORD;
+        offset = 1;
+
+        temp = char_next;
+        switch (char_next) {
+          case 'n': temp = '\n'; break;
+          case 'r': temp = '\r'; break;
+        }
+
+        literals_block += temp;
+
+        break;
+
+      case '{':
+        if (par_open || lb_size == 0 || char_next == 0)
+          throw 1;
+        curr_tok = TokenType::TOK_Q_CUSTOM;
+        offset = 2;
+
+        if (char_next >= '1' && char_next <= '9') {
+          if (regex_long[i + 2] != '}') throw 1;
+          q_min_repeats = char_next - '0';
+
+          if (prev_tok == TokenType::TOK_PAR_CLOSE) {
+            lexemes.push_back({ literals_block, 1, 1 });
+          } else if (prev_tok == TokenType::TOK_WORD) {
+            if (lb_size > 1)
+              lexemes.push_back({ literals_block.substr(0, lb_size - 1), 1, 1 });
+            lexemes.push_back({ literals_block.substr(lb_size - 1), q_min_repeats, q_min_repeats});
+          }
+
+          literals_block = "";
+        } else if (char_next == '}') {
+          offset = 1;
+        } else {
+          throw 1;
+        }
+
+        break;
+
+      case '(':
+        if (par_open) throw 1;
+        par_open = true;
+        curr_tok = TokenType::TOK_PAR_OPEN;
+
+        if (lb_size > 0) {
+          lexemes.push_back({ literals_block, 1, 1 });
+          literals_block = "";
+        }
+
+        break;
+
+      case ')':
+        if (!par_open) throw 1;
+        par_open = false;
+        curr_tok = TokenType::TOK_PAR_CLOSE;
+
+        break;
+
+      case '*':
+        if (par_open || lb_size == 0) throw 1;
+        curr_tok = TokenType::TOK_Q_ASTERISK;
+
+        q_min_repeats = 0;
+        q_max_repeats = kInfinityRepeats;
+
+        break;
+
+      case '+':
+        if (par_open || lb_size == 0) throw 1;
+        curr_tok = TokenType::TOK_Q_PLUS;
+
+        if (prev_tok == TokenType::TOK_PAR_CLOSE) {
+          lexemes.push_back({ literals_block, 1, 1 });
+          lexemes.push_back({ literals_block, 0, kInfinityRepeats });
+        } else if (prev_tok == TokenType::TOK_WORD) {
+          if (lb_size > 1)
+            lexemes.push_back({ literals_block.substr(0, lb_size - 1), 1, 1 });
+          lexemes.push_back({ literals_block.substr(lb_size - 1), 1, 1 });
+          lexemes.push_back({ literals_block.substr(lb_size - 1), 0, kInfinityRepeats });
+        }
+        literals_block = "";
+
+        break;
+
+      case '?':
+        if (par_open || lb_size == 0) throw 1;
+        curr_tok = TokenType::TOK_Q_QUESTION;
+
+        q_min_repeats = 0;
+        q_max_repeats = 1;
+
+        break;
+
+      default:
+        curr_tok = TokenType::TOK_WORD;
+
+        literals_block += regex[i];
+        break;
+    }
+
+    // Processing quantifiers
+    if (curr_tok == TokenType::TOK_Q_ASTERISK || curr_tok == TokenType::TOK_Q_QUESTION) {
+      if (lb_size == 0)
+        throw 1;
+      if (prev_tok == TokenType::TOK_PAR_CLOSE) {
+        lexemes.push_back({ literals_block, 1, 1 });
+      } else if (prev_tok == TokenType::TOK_WORD) {
+        if (lb_size > 1)
+          lexemes.push_back({ literals_block.substr(0, lb_size - 1), 1, 1 });
+        lexemes.push_back({ literals_block.substr(lb_size - 1), q_min_repeats, q_max_repeats });
+      }
+      literals_block = "";
+    }
+
+    i += offset;
+    lb_size = literals_block.size();
+  }
+
+  if (lb_size > 0) {
+    lexemes.push_back({ literals_block, 1, 1 });
+  }
 }
