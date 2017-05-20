fcc5dee81237abda427da2a85b67dc4c474c2d3b
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-05-20 13:47:19 +0300

    Add a new module within existing module

---
+++ modules/integration-methods/application/integration-methods.cpp
@@ -0,0 +1,12 @@
+// Copyright 2017 Komarov Nikolay
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/integration_methods.h"
+
+int main(int argc, const char** argv) {
+    printf("Hello World!\n");
+    return 0;
+}
