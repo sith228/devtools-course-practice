3ccfa7906fc9708d173588c4a6337dd9363f8d9c
Nikita Grechuhin <NikitaGrechukhin@gmail.com>
2017-05-21 12:41:07 +0300

    delete white spaces

--- modules/mortgage-calculator/include/mortgage_calculator_app.h
+++ modules/mortgage-calculator/include/mortgage_calculator_app.h
@@ -9,11 +9,11 @@ class Application {
  public:
     Application();
     std::string operator()(int argc, const char** argv);
-    
+
  private:
     void help(const char *appName, const char *message = "");
     bool validateNumberOfArguments(int argc, const char **argv);
- 
+
     std::string message_;
 };
 #endif  // MODULES_MORTGAGE_CALCULATOR_INCLUDE_MORTGAGE_CALCULATOR_APP_H_
