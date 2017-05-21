8ac11d08849518c4ea06b854905c3c4f5d09693c
Vagik <simonyan.vagik@yandex.ru>
2017-05-21 23:42:26 +0300

    deleted whitespaces and empty lines

--- modules/weight_converter/application/weight_converter.cpp
+++ modules/weight_converter/application/weight_converter.cpp
@@ -3,11 +3,11 @@
 #include "include/weight_converter_app.h"
 
 #include <map>
+#include <string>
 
 int main(int argc, const char** argv) {
     Application app;
     std::string output = app(argc, argv);
     printf("%s\n", output.c_str());
     return 0;
-    
 }
