dca835410a6e99ac3d2e3faab08e4c81f84b68ac
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-24 17:54:32 +0300

    added header for class App

---
+++ modules/quadratic-equation/include/quadratic_equation_app.h
@@ -0,0 +1,29 @@
+// Copyright 2017 Kiseleva Olga
+
+#ifndef MODULES_QUADRATIC_EQUATION_CALCULATOR_INCLUDE_QUADRATIC_EQUATION_H_
+#define MODULES_QUADRATIC_EQUATION_CALCULATOR_INCLUDE_QUADRATIC_EQUATION_H_
+
+
+#include <map>
+#include <string>
+#include <vector>
+#include "quadratic_equation.h"
+
+class QuadraticEquationCalculator {
+ public:
+     QuadraticEquationCalculator();
+    std::string operator()(int argc, const char** argv);
+
+ private:
+    void Help(const char* appname, const char* message = "");
+    bool ValidateNumberOfArguments(int argc, const char** argv);
+    std::string message_;
+    typedef struct {
+        double coeffA;
+        double coeffB;
+        double coeffC;
+        char operation;
+    } Arguments;
+};
+
+#endif  // MODULES_QUADRATIC_EQUATION_CALCULATOR_INCLUDE_QUADRATIC_EQUATION_H_
