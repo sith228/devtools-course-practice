c14c2f31b3b7057f0a626e76f62a1d6cef40deb8
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2017-05-25 14:51:46 +0300

    Replace enum with map

--- modules/root-finder/include/function-factory.h
+++ modules/root-finder/include/function-factory.h
@@ -3,32 +3,31 @@
 #ifndef MODULES_ROOT_FINDER_INCLUDE_FUNCTION_FACTORY_H_
 #define MODULES_ROOT_FINDER_INCLUDE_FUNCTION_FACTORY_H_
 
+#include <string>
+#include <map>
+#include <vector>
 #include "include/functions.h"
 
-enum FunctionID
-{
-	POLINOMIAL_2_DEGREE,
-	POLINOMIAL_3_DEGREE,
-    POLINOMIAL_4_DEGREE,
-    EXPONENT
+std::map<std::string, int> FunctionId {
+	std::make_pair("POLINOMIAL_2_DEGREE", 0),
+	std::make_pair("POLINOMIAL_3_DEGREE", 1),
+	std::make_pair("POLINOMIAL_4_DEGREE", 2),
+	std::make_pair("EXPONENT", 3)
 };
 
 class FunctionFactory {
  public:
-	static Function* createFunction (FunctionID fid)
-	{
+	 static Function* createFunction (std::string functionName, std::vector<float> koefs) {
 		Function *function;
 
-		switch (fid) {
-		case 0: function = new Polynomial2Degree(); break;
-		case 1: function = new Polynomial3Degree(); break;
-		case 2: function = new Polynomial4Degree(); break;
-        case 3: function = new Exponent(); break;
+		switch (FunctionId[functionName]) {
+		case 0: function = new Polynomial2Degree(koefs); break;
+		case 1: function = new Polynomial3Degree(koefs); break;
+		case 2: function = new Polynomial4Degree(koefs); break;
+        case 3: function = new Exponent(koefs); break;
 		}
 		return function;
 	}
 };
-
-
 
 #endif  // MODULES_ROOT_FINDER_INCLUDE_FUNCTION_FACTORY_H_
