3e1c4b6f3617660a05f1562b7a209ef24c729282
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-25 15:50:46 +0300

    renamed ifndef and so on

--- modules/quadratic-equation/include/quadratic_eq_app.h
+++ modules/quadratic-equation/include/quadratic_eq_app.h
@@ -1,7 +1,7 @@
 // Copyright 2017 Kiseleva Olga
 
-#ifndef MODULES_QUADRATIC_EQUATION_INCLUDE_Quadratic_Eq_App_H_
-#define MODULES_QUADRATIC_EQUATION_INCLUDE_Quadratic_Eq_App_H_
+#ifndef MODULES_QUADRATIC_EQUATION_INCLUDE_Quadratic_EQ_APP_H_
+#define MODULES_QUADRATIC_EQUATION_INCLUDE_Quadratic_Eq_APP_H_
 
 
 #include <map>
@@ -26,4 +26,4 @@ class QuadraticEquationCalculator {
     } Arguments;
 };
 
-#endif  // MODULES_QUADRATIC_EQUATION_CALCULATOR_INCLUDE_Quadratic_Eq_App_H_
+#endif  // MODULES_QUADRATIC_EQUATION_CALCULATOR_INCLUDE_Quadratic_EQ_APP_H_
