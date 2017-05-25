3663465591134a919b5dca348418c8ecbc50a4be
Maxim <thanatormax@gmail.com>
2017-05-25 22:45:38 +0300

    Стуле

--- modules/Sort/application/Sort.cpp
+++ modules/Sort/application/Sort.cpp
@@ -8,7 +8,7 @@
 
 int main(int argc, const char** argv) {
   SortApp app;
-  std::string output = app(argc,argv);
+  std::string output = app(argc, argv);
   printf("%s\n", output.c_str());
   return 0;
 }
