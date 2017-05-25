235ce239d33ff26f4da315b0ff2b77a7c2020be9
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-25 19:58:32 +0300

    style

--- modules/quadratic-equation/src/quadratic_eq_app.cpp
+++ modules/quadratic-equation/src/quadratic_eq_app.cpp
@@ -67,7 +67,7 @@ std::string QuadrEquationCalc::operator()
         switch (countSolution) {
         case 0:
             stream << "x1 = " << quadratic_eq.GetComplexX1().getRe() <<
-                "+" << quadratic_eq.GetComplexX1().getIm() << "i"<<" ";
+                "+" << quadratic_eq.GetComplexX1().getIm() << "i" << " ";
             stream << "x2 = " << quadratic_eq.GetComplexX2().getRe() <<
                 quadratic_eq.GetComplexX2().getIm() << "i";
             break;
