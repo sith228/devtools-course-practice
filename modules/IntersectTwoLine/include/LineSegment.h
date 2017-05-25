5c833d40f2522e5bdc68a5cf05104c2b8eefbdb0
LobanovAndrey <1etodva@mail.ru>
2017-05-25 19:25:58 +0300

    coding style

--- modules/IntersectTwoLine/include/LineSegment.h
+++ modules/IntersectTwoLine/include/LineSegment.h
@@ -10,27 +10,10 @@ using std::string;
 
 class LineSegment2D {
  public:
-  LineSegment2D(double X1, double Y1, double X2, double Y2) {
-    if (X1 == X2 && Y1 == Y2) {
-      throw "Error, points can not coincide";
-    } else if (X1 > X2) {
-      X1_ = X2;
-      Y1_ = Y2;
-      X2_ = X1;
-      Y2_ = Y1;
-    } else {
-      X1_ = X1;
-      Y1_ = Y1;
-      X2_ = X2;
-      Y2_ = Y2;
-    }
-  }
-  LineSegment2D() {
-    X1_ = 0;
-    Y1_ = 0;
-    X2_ = 0;
-    Y2_ = 0;
-  }
+  LineSegment2D(double X1, double Y1, double X2, double Y2);
+
+  LineSegment2D() : X1_(0), X2_(0), Y1_(0), Y2_(0) {}
+
   void SetX1(double X) {
     X1_ = X;
   }
