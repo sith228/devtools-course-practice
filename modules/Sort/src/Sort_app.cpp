5a0150467dc82f54ece44bb2111c7683693ea5e4
Maxim <thanatormax@gmail.com>
2017-05-25 16:26:29 +0300

    Добавлен класс Application

--- modules/Sort/src/Sort_app.cpp
+++ modules/Sort/src/Sort_app.cpp
@@ -11,9 +11,102 @@
 SortApp::SortApp() : message_("") {}
 
 void SortApp::help(const char* appname, const char* message) {
-    message_ = string(message) +
+    message_ = std::string(message) +
         "This is a sort application. \n\n" +
-        "Pleass provide arguments in the following format:\n\n";
+        "Pleass provide arguments in the following format:\n\n" +
+
+        " $ " + appname + " <length_> <sort_type_> <array_elem_1> ..." +
+        " <array_elem_length>\n\n" +
+
+        "Where all arguments is a int-precision numbers, " +
+        " and <sort_type_> is a choice of the kind of sort\n";
+}
+
+double parseInt(const char* args) {
+	char* end = 0;
+	int value = strtol(args, &end, 10);
+
+	if (end[0]) {
+		throw std::string("Wrong number format!");
+	}
+
+	return value;
+}
+
+bool SortApp::validateNumberOfArguments(int argc, const char** argv)
+{
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    }
+    else if (parseInt(argv[1]) != (argc - 3)) {
+        help(argv[0], "Error: Should be <length> arguments\n");
+        return false;
+    }
+    return true;
+}
 
 
+
+
+std::string SortApp::operator()(int argc, const char** argv) {
+    Arguments args;
+
+    if (!validateNumberOfArguments(argc,argv)) {
+        return message_;
+    }
+
+    try {
+        args.length_ = parseInt(argv[1]);
+        args.sort_type_ = parseInt(argv[2]);
+        args.array_ = new int[args.length_];
+        for (int i = 0; i < args.length_; i++) {
+            args.array_[i] = parseInt(argv[3+i]);
+         }
+        }
+    catch(std::string& str) {
+        return str;
+    }
+
+    std::ostringstream stream;
+    Sort sort(args.array_, args.length_);
+    switch (args.sort_type_) {
+        case 1:
+        sort.QuickSort(0, args.length_-1);
+        stream << "Sorted array: ";
+        for (int i = 0; i < args.length_; i++)
+            stream << sort[i] << " ";
+        stream << "\n";
+
+        break;
+
+        case 2:
+        sort.PasteSort();
+        stream << "Sorted array: ";
+        for (int i = 0; i < args.length_; i++)
+            stream << sort[i] << " ";
+        stream << "\n";
+
+        break;
+
+        case 3:
+        sort.ChoiceSort();
+        stream << "Sorted array: ";
+        for (int i = 0; i < args.length_; i++)
+            stream << sort[i] << " ";
+        stream << "\n";
+
+        break;
+
+        case 4:
+        sort.MergeSort(0, args.length_-1);
+        stream << "Sorted array: ";
+        for (int i = 0; i < args.length_; i++)
+            stream << sort[i] << " ";
+        stream << "\n";
+
+        break;
+    }
+    message_ = stream.str();
+    return message_;
 }
