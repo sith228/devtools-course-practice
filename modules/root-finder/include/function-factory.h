14f93df3775cb456a9576ea6627e12c73694c0c4
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2017-05-25 14:56:15 +0300

    Add default in switch

--- modules/root-finder/include/function-factory.h
+++ modules/root-finder/include/function-factory.h
@@ -25,6 +25,7 @@ class FunctionFactory {
 		case 1: function = new Polynomial3Degree(koefs); break;
 		case 2: function = new Polynomial4Degree(koefs); break;
         case 3: function = new Exponent(koefs); break;
+		default: function = nullptr;
 		}
 		return function;
 	}
