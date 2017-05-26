db9e72122e4c894c255cfd5be38db26d3c727346
Eugene Baranov <zcbw@mail.ru>
2017-05-26 20:10:42 +0300

    fix googlestyle

--- modules/color-spaces/include/ColorConverter.h
+++ modules/color-spaces/include/ColorConverter.h
@@ -3,8 +3,8 @@
 #ifndef MODULES_COLOR_SPACES_INCLUDE_COLORCONVERTER_H_
 #define MODULES_COLOR_SPACES_INCLUDE_COLORCONVERTER_H_
 
-#include <string>
 #include <stdint.h>
+#include <string>
 
 typedef struct {
     uint16_t value1;
@@ -16,11 +16,11 @@ typedef struct {
 } Arguments;
 
 class ColorConverter {
-public:
+ public:
     ColorConverter();
     std::string operator()(int argc, const char** argv);
 
-private:
+ private:
     void help(const char* appname, const char* message = "");
     bool ValidateNumberOfArguments(int argc, const char** argv);
     std::string message_;
