48ae1184bd4c411ffe06f435de27d4daf76888e6
Maxim-Doronin <doronin.maxim.unn@gmail.com>
2017-05-26 17:43:54 +0300

    Add all tests

--- modules/root-finder/test/test_newton-method-app.cpp
+++ modules/root-finder/test/test_newton-method-app.cpp
@@ -49,8 +49,16 @@ TEST_F(NewtonMethodApplicationTest, Do_Print_Help_Without_Arguments) {
     Assert("This is a Newton Method application");
 }
 
+TEST_F(NewtonMethodApplicationTest, Is_Checking_Accuracy) {
+    vector<string> args = { "EXPONENT" };
+
+    Act(args);
+
+    Assert("ERROR: No accuracy!");
+}
+
 TEST_F(NewtonMethodApplicationTest, Is_Checking_Bounds) {
-    vector<string> args = {"EXPONENT", "0"};
+    vector<string> args = { "EXPONENT", "0.01", "0" };
 
     Act(args);
 
@@ -58,7 +66,7 @@ TEST_F(NewtonMethodApplicationTest, Is_Checking_Bounds) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Is_Checking_Coefficients) {
-    vector<string> args = { "EXPONENT", "0", "1", "1" };
+    vector<string> args = { "EXPONENT", "0.01", "0", "1", "1" };
 
     Act(args);
 
@@ -66,7 +74,7 @@ TEST_F(NewtonMethodApplicationTest, Is_Checking_Coefficients) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_Wrong_Number_Format) {
-    vector<string> args = {"EXPONENT", "pi", "phi"};
+    vector<string> args = {"EXPONENT", "0.01", "pi", "phi"};
 
     Act(args);
 
@@ -74,7 +82,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_Wrong_Number_Format) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_Wrong_Function_Format) {
-    vector<string> args = {"EXP", "0", "1", "2", "3", "4"};
+    vector<string> args = {"EXP", "0.01", "0", "1", "2", "3", "4"};
 
     Act(args);
 
@@ -82,7 +90,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_Wrong_Function_Format) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_Zero_As_First_Coeff) {
-    vector<string> args = {"EXPONENT", "0", "1", "0", "3", "4"};
+    vector<string> args = {"EXPONENT", "0.01", "0", "1", "0", "3", "4"};
 
     Act(args);
 
@@ -90,7 +98,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_Zero_As_First_Coeff) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_No_Monotone_Function) {
-    vector<string> args = { "POLINOMIAL_2_DEGREE", "-4", "4", "2", "1", "-6" };
+    vector<string> args = { "POLINOMIAL_2_DEGREE", "0.01", "-4", "4", "2", "1", "-6" };
 
     Act(args);
 
@@ -98,7 +106,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_No_Monotone_Function) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_If_Segment_Is_Point) {
-    vector<string> args = { "POLINOMIAL_2_DEGREE", "4", "4", "2", "1", "-6" };
+    vector<string> args = { "POLINOMIAL_2_DEGREE", "0.01", "4", "4", "2", "1", "-6" };
 
     Act(args);
 
@@ -106,7 +114,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_If_Segment_Is_Point) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Detect_If_Function_Hasnt_Roots) {
-    vector<string> args = { "POLINOMIAL_2_DEGREE", "3", "4", "2", "1", "-6" };
+    vector<string> args = { "POLINOMIAL_2_DEGREE", "0.01", "3", "4", "2", "1", "-6" };
 
     Act(args);
 
@@ -114,7 +122,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Detect_If_Function_Hasnt_Roots) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_2_Degree) {
-    vector<string> args = { "POLINOMIAL_2_DEGREE", "0", "2", "2", "1", "-6" };
+    vector<string> args = { "POLINOMIAL_2_DEGREE", "0.01", "0", "2", "2", "1", "-6" };
 
     Act(args);
 
@@ -122,7 +130,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_2_Degree) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_3_Degree) {
-    vector<string> args = { "POLINOMIAL_3_DEGREE", "3", "6", "1", "-1", "-16", "16" };
+    vector<string> args = { "POLINOMIAL_3_DEGREE", "0.01", "3", "6", "1", "-1", "-16", "16" };
 
     Act(args);
 
@@ -130,7 +138,7 @@ TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_3_Degree) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_4_Degree) {
-    vector<string> args = { "POLINOMIAL_4_DEGREE", "2.5", "3.5", "1", "1", "-9", "-9", "0" };
+    vector<string> args = { "POLINOMIAL_4_DEGREE", "0.01", "2.5", "3.5", "1", "1", "-9", "-9", "0" };
 
     Act(args);
 
@@ -138,9 +146,25 @@ TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Polinomial_4_Degree) {
 }
 
 TEST_F(NewtonMethodApplicationTest, Can_Find_Root_On_Exponent) {
-    vector<string> args = { "EXPONENT", "-1", "1", "1", "2", "-1" };
+    vector<string> args = { "EXPONENT", "0.01", "-1", "1", "1", "2", "-1" };
 
     Act(args);
 
     Assert("Root: 0");
+}
+
+TEST_F(NewtonMethodApplicationTest, Low_Accuracy_Affect_On_Result) {
+    vector<string> args = { "EXPONENT", "0.01", "0", "0.5", "1", "2.5", "-2" };
+
+    Act(args);
+
+    Assert("Root: 0.277272");
+}
+
+TEST_F(NewtonMethodApplicationTest, Hi_Accuracy_Affect_On_Result) {
+    vector<string> args = { "EXPONENT", "0.0001", "0", "0.5", "1", "2.5", "-2" };
+
+    Act(args);
+
+    Assert("Root: 0.277259");
 }
