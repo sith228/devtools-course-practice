7fc0a1cdae3c10db6726e3bc3a2038195bedde69
VladimirKalinin <vovakalinin1997@gmail.com>
2017-05-27 01:08:51 +0300

    fix register in class name

--- modules/dijkstra/src/Graph.cpp
+++ modules/dijkstra/src/Graph.cpp
@@ -3,51 +3,51 @@
 #include "include/Graph.h"
 #include <limits.h>
 
-edge::edge() : weight_(0), key_(0), next_(0) {}
+Edge::Edge() : weight_(0), key_(0), next_(0) {}
 
-edge::edge(const int _weight, const int _key) :
+Edge::Edge(const int _weight, const int _key) :
   weight_(_weight), key_(_key), next_(0) {}
 
-void edge::SetNext(edge* _next) { next_ = _next; }
+void Edge::SetNext(Edge* _next) { next_ = _next; }
 
-int edge::GetWeight() { return weight_; }
+int Edge::GetWeight() { return weight_; }
 
-int edge::GetKey() { return key_; }
+int Edge::GetKey() { return key_; }
 
-edge* edge::GetNext() { return next_; }
+Edge* Edge::GetNext() { return next_; }
 
-graph::graph() : size_(0), node_(0) {}
+Graph::Graph() : size_(0), node_(0) {}
 
-graph::graph(int _size) : size_(_size) {
-  node_ = new edge*[size_];
+Graph::Graph(int _size) : size_(_size) {
+  node_ = new Edge*[size_];
   for (int i = 0; i < size_; i++)
     node_[i] = 0;
 }
 
-void graph::AddEdge(const int _weight, const int key1, const int key2) {
+void Graph::AddEdge(const int _weight, const int key1, const int key2) {
   if (IsConnect(key1, key2))
     return;
   if (node_[key1] == 0) {
-    node_[key1] = new edge(_weight, key2);
+    node_[key1] = new Edge(_weight, key2);
   } else {
-    edge* tmp = node_[key1];
+    Edge* tmp = node_[key1];
     while (tmp->GetNext())
       tmp = tmp->GetNext();
-    tmp->SetNext(new edge(_weight, key2));
+    tmp->SetNext(new Edge(_weight, key2));
   }
 
   if (node_[key2] == 0) {
-    node_[key2] = new edge(_weight, key1);
+    node_[key2] = new Edge(_weight, key1);
   } else {
-    edge* tmp = node_[key2];
+    Edge* tmp = node_[key2];
     while (tmp->GetNext())
       tmp = tmp->GetNext();
-    tmp->SetNext(new edge(_weight, key1));
+    tmp->SetNext(new Edge(_weight, key1));
   }
 }
 
-bool graph::IsConnect(const int key1, const int key2) {
-  edge* tmp = node_[key1];
+bool Graph::IsConnect(const int key1, const int key2) {
+  Edge* tmp = node_[key1];
   while (tmp) {
     if (tmp->GetKey() == key2)
       return true;
@@ -56,15 +56,15 @@ bool graph::IsConnect(const int key1, const int key2) {
   return false;
 }
 
-int graph::GetSize() {
+int Graph::GetSize() {
   return size_;
 }
 
-edge* graph::GetNode(int n) {
+Edge* Graph::GetNode(int n) {
   return node_[n];
 }
 
-int* graph::Dijkstra(int node_index) {
+int* Graph::Dijkstra(int node_index) {
   int* weight = new int[size_];
   for (int i = 0; i < size_; i++)
     weight[i] = INT_MAX;
@@ -84,7 +84,7 @@ int* graph::Dijkstra(int node_index) {
         minWeightNode = i;
       }
     mark[minWeightNode] = 1;
-    edge* tmp = node_[minWeightNode];
+    Edge* tmp = node_[minWeightNode];
     while (tmp) {
       if (weight[tmp->GetKey()] > weight[minWeightNode] +
         tmp->GetWeight() && mark[tmp->GetKey()] == 0)
