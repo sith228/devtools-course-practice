c749fe3f9565d7f2ecdf8243a94eceb12b9930fc
dmitrydryakhlov <dmitrydryakhlov.mail@gmail.com>
2017-06-02 21:19:09 +0300

    final

--- modules/huffman-algoritm/test/test_huffman_app.cpp
+++ modules/huffman-algoritm/test/test_huffman_app.cpp
@@ -40,54 +40,48 @@ class HuffmanAlgAppTest : public ::testing::Test {
 
 TEST_F(HuffmanAlgAppTest, Do_Print_Help_Without_Arguments) {
     vector<string> args = {};
-
+    // Act
     Act(args);
-
+    // Assert
     Assert("This is a huffman algoritm application\\..*");
-
 }
 
 TEST_F(HuffmanAlgAppTest, Is_Checking_Number_Of_Arguments_1) {
     vector<string> args = { "encode" };
-
+    // Act
     Act(args);
-	
+    // Assert
     Assert("ERROR: Should be more than 1 arguments*");
-
 }
 
 TEST_F(HuffmanAlgAppTest, Is_Checking_Number_Of_Arguments_3) {
     vector<string> args = { "asd", "sda", "asdasd" };
-
+    // Act
     Act(args);
-
+    // Assert
     Assert("ERROR: Should be less than 3 arguments*");
-
 }
 
 TEST_F(HuffmanAlgAppTest, Can_Detect_Wrong_Comand_Format) {
     vector<string> args = { "encod", "it is my striiing" };
-    
+    // Act
     Act(args);
-
+    // Assert
     Assert("command is uncorrect");
-	
 }
 
 TEST_F(HuffmanAlgAppTest, Can_Calculate_Huffman_Cod) {
     vector<string> args = { "encode", "it is my striiing" };
-
+    // Act
     Act(args);
-
+    // Assert
     Assert("10010111100011110111000111001010110110101011000110");
-
 }
 
 TEST_F(HuffmanAlgAppTest, Can_Encode_And_Decode) {
     vector<string> args = { "encode and decode", "it is my striiing" };
-
+    // Act
     Act(args);
-
+    // Assert
     Assert("it is my striiing");
-
 }
