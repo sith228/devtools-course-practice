72b9ae9fcec507208974ff0f07d1db0f01fac679
Игорь Федоров <fedorovigor1155@gmail.com>
2017-06-20 01:45:31 +0300

    correct style

--- modules/sapper/include/field.h
+++ modules/sapper/include/field.h
@@ -1,23 +1,26 @@
 // Copyright 2017 Fedorov Igor
 
-#ifndef MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
-#define MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
 
-struct Cell
-{
+#ifndef MODULES_SAPPER_INCLUDE_FIELD_H_
+#define MODULES_SAPPER_INCLUDE_FIELD_H_
+
+struct Cell {
     bool isBomb = false;
     char viewCell = '*';
     int num = 0;
 };
 
 class Field {
+ 
  private:
+     
      Cell **field_;
-     short int Height_ ;
-     short int Width_ ;
-     short int NumBomb_ ;
+     int16_t Height_;
+     int16_t Width_;
+     int16_t NumBomb_;
  public:
-     Field(int Width = 10, int Height = 10, int bombcount = 10);
+
+     explicit Field(int Width = 10, int Height = 10, int bombcount = 10);
      ~Field();
      void PrintField();
      // feedback from touch
@@ -30,14 +33,16 @@ class Field {
      // create array[,]
      void GenerateField();
      // getter
-     short int GetBombCount() {
+     int16_t GetBombCount() {
          return NumBomb_;
      }
+
  private:
+
      // add in field bombcount bombs
      void GenerateBomb(int bombcount);
      // generate num of bomb for each cell
      void GenerateCellNum();
 };
 
-#endif  // MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_NUMBER_H_
+#endif MODULES_SAPPER_INCLUDE_FIELD_H_
--- modules/sapper/include/saper.h
+++ modules/sapper/include/saper.h
@@ -1,14 +1,14 @@
 // Copyright 2017 Fedorov Igor
 
-#ifndef MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
-#define MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
+#ifndef MODULES_SAPPER_INCLUDE_SAPER_H_  
+#define MODULES_SAPPER_INCLUDE_SAPER_H_  
 
 #include<iostream>
 #include"../include/field.h"
 
 class Saper {
  public:
-    Saper(int wth = 10, int hght = 10, int countbomb = 10);
+	 explicit Saper(int wth = 10, int hght = 10, int countbomb = 10);
     void Print();
     bool TouchResult(int x_, int y_);
     void MarkCell(int x, int y);
@@ -19,4 +19,4 @@ class Saper {
     int DeactivatedBomb_;
 };
 
-#endif  // MODULES_COMPLEX_NUMBER_INCLUDE_COMPLEX_CALCULATOR_H_
+#endif MODULES_SAPPER_INCLUDE_SAPER_H_  
