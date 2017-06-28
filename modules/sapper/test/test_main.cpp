b85f85580d56175e2bfc6380a567142cb81ce3c2
Игорь Федоров <fedorovigor1155@gmail.com>
2017-06-28 16:00:39 +0300

    correct rand

--- modules/sapper/test/test_main.cpp
+++ modules/sapper/test/test_main.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
-    getchar();
-    return 0;
+    return RUN_ALL_TESTS();
+    //  getchar();
 }
